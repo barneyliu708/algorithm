96b2c921cf290a11275beb6a0268959a3b788775
barneyliu708 <barneyliu708@gmail.com>
2021-10-07 19:40:50 -0700

    ALGORITHM-2 Add Code for find max subarray problem

---
+++ Algorithm/CH4_DivideAndConquer/Ch4-1_MaximumSubarray/FindMaximumSubarray.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH4_DivideAndConquer.Ch4_1_MaximumSubarray
+{
+    [TestFixture]
+    public class Executor
+    {
+        public Tuple<int, int, int> FindMaximumSubarray(int[] A, int low, int high)
+        {
+            return new Tuple<int, int, int>(0, 0, 0);
+        }
+
+        [Test]
+        public void Execute()
+        {
+            int[] A = new int[] { 13, -3, -25, 20, -3, -6, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7 };
+            var result = FindMaximumSubarray(A, 0, A.Length - 1);
+        }
+    }
+}
---
+++ Algorithm/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Algorithm
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
