110d167c597b14dc113d92d2f430c33a688d4b2f
barneyliu708 <barneyliu708@gmail.com>
2021-10-21 22:46:04 -0700

    ALGORITHM-12 Reading 6.2 Maintaining Heap Property
    Max-Heapify

---
+++ Algorithm/CH6_SortingAndOrderStatistics/Heaps/Heap.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH6_SortingAndOrderStatistics.Ch6_2_MaintainingTheHeapProperty
+{
+    public class Heap
+    {
+        public void MaxHeapify(int[] A, int i)
+        {
+            int maxIndex = i;
+            int rightIndex = RightChildIndex(i);
+            int leftIndex = LeftChildIndex(i);
+            if (leftIndex < A.Length && A[leftIndex] > A[i])
+            {
+                maxIndex = leftIndex;
+            }
+            if (rightIndex < A.Length && A[rightIndex] > A[maxIndex])
+            {
+                maxIndex = rightIndex;
+            }
+
+            if (i != maxIndex)
+            {
+                int temp = A[i];
+                A[i] = A[maxIndex];
+                A[maxIndex] = temp;
+
+                MaxHeapify(A, maxIndex);
+            }
+        }
+
+        public int ParentIndex(int i)
+        {
+            return (i - 1) / 2;
+        }
+
+        public int LeftChildIndex(int i)
+        {
+            return i * 2 + 1;
+        }
+
+        public int RightChildIndex(int i)
+        {
+            return i * 2 + 2;
+        }
+    }
+}
