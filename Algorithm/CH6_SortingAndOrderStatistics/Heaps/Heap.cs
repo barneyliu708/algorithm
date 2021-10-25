42cc2f0ab388c4fdf345e8661823b5c1a422c94a
barneyliu708 <barneyliu708@gmail.com>
2021-10-24 19:54:44 -0700

    ALGORITHM-37 Reading 6.5 The Priority Queue

--- Algorithm/CH6_SortingAndOrderStatistics/Heaps/Heap.cs
+++ Algorithm/CH6_SortingAndOrderStatistics/Heaps/Heap.cs
@@ -6,6 +6,64 @@ namespace Algorithm.CH6_SortingAndOrderStatistics.Ch6_2_MaintainingTheHeapProper
 {
     public class Heap
     {
+        private int[] _A;
+        private int _heapSize;
+
+        public Heap(int size)
+        {
+            _heapSize = size;
+            _A = new int[size];
+        }
+
+        public Heap(int[] A)
+        {
+            _heapSize = A.Length;
+            _A = A;
+            BuildMaxHeap(this._A);
+        }
+
+        public int this[int i]
+        {
+            get => _A[i];
+            set => _A[i] = value;
+        }
+
+        public int Maximum()
+        {
+            return _A[0];
+        }
+
+        public int ExtractMax()
+        {
+            if (_heapSize < 1)
+            {
+                throw new Exception("heap underflow");
+            }
+
+            int max = _A[0];
+            _A[0] = _A[_heapSize - 1];
+            _heapSize--;
+            MaxHeapify(_A, 0, _heapSize);
+
+            return max;
+        }
+
+        public void IncreaseKey(int i, int key)
+        {
+            if (key < _A[i])
+            {
+                throw new Exception("new key is smaller than current key!");
+            }
+
+            _A[i] = key;
+            while (i > 0 && _A[ParentIndex(i)] < _A[i]) {
+                int temp = _A[i];
+                _A[i] = _A[ParentIndex(i)];
+                _A[ParentIndex(i)] = temp;
+                i = ParentIndex(i);
+            }
+        }
+
         public void MaxHeapify(int[] A, int i, int heapSize)
         {
             int maxIndex = i;
---
+++ Algorithm/CH6_SortingAndOrderStatistics/PriorityQueue/PriorityQueue.cs
@@ -0,0 +1,25 @@
+﻿using Algorithm.CH6_SortingAndOrderStatistics.Ch6_2_MaintainingTheHeapProperty;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH6_SortingAndOrderStatistics.PriorityQueue
+{
+    public class PriorityQueue
+    {
+        private Heap A;
+
+        public PriorityQueue(int size)
+        {
+            A = new Heap(size);
+        }
+
+        public int Maximum()
+        {
+            return A[1];
+        }
+
+       
+
+    }
+}
