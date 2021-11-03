f1a978254abd6c0cb34309982c34328cd13b9bb2
barneyliu708 <barneyliu708@gmail.com>
2021-11-02 21:24:51 -0700

    ALGORITHM-39 Reading 7.1 Description of Quicksort

---
+++ Algorithm/CH7_QuickSort/Ch7-1/Exercises.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH7_QuickSort.Ch7_1
+{
+    [TestFixture]
+    public class Exercises
+    {
+        [Test]
+        public void Exercise7_1()
+        {
+            int[] A = { 13, 19, 9, 5, 12, 8, 7, 4, 21, 2, 6, 11 };
+            QuickSort.Sort(A);
+            Assert.AreEqual(2, A[0]);
+            Assert.AreEqual(4, A[1]);
+            Assert.AreEqual(5, A[2]);
+            Assert.AreEqual(6, A[3]);
+            Assert.AreEqual(7, A[4]);
+            Assert.AreEqual(8, A[5]);
+            Assert.AreEqual(9, A[6]);
+            Assert.AreEqual(11, A[7]);
+            Assert.AreEqual(12, A[8]);
+            Assert.AreEqual(13, A[9]);
+            Assert.AreEqual(19, A[10]);
+            Assert.AreEqual(21, A[11]);
+        }
+    }
+}
---
+++ Algorithm/CH7_QuickSort/QuickSort.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH7_QuickSort
+{
+    public class QuickSort
+    {
+        public static void Sort(int[] A)
+        {
+            Sort(A, 0, A.Length - 1);
+        }
+
+        private static void Sort(int[] A, int p, int r)
+        {
+            if (p < r)
+            {
+                int q = Partition(A, p, r);
+                Sort(A, p, q - 1);
+                Sort(A, q + 1, r);
+            }
+        }
+
+        private static int Partition(int[] A, int p, int r)
+        {
+            int x = A[r];
+            int i = p - 1;
+            for (int j = p; j < r; j++)
+            {
+                if (A[j] < x)
+                {
+                    i++;
+                    Swap(A, i, j);
+                }
+            }
+
+            Swap(A, i + 1, r);
+            return i + 1;
+        }
+
+        private static void Swap(int[] A, int i, int j)
+        {
+            int temp = A[i];
+            A[i] = A[j];
+            A[j] = temp;
+        }
+    }
+}
