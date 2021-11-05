93093363b1de172b5c4fb1581076589d6ac79f61
barneyliu708 <barneyliu708@gmail.com>
2021-11-04 22:40:54 -0700

    ALGORITHM-47 LC032 Longest Valid Parentheses - Stack
    Stack solution: https://leetcode.com/problems/longest-valid-parentheses/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC032LongestValidParentheses.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    [TestFixture]
+    public class LC032LongestValidParentheses
+    {
+        public int LongestValidParentheses(string s)
+        {
+
+            int maxlen = 0;
+            Stack<int> stack = new Stack<int>();
+            stack.Push(-1);
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '(')
+                {
+                    stack.Push(i);
+                }
+                else
+                {
+                    int preidx = stack.Pop();
+                    if (stack.Count == 0)
+                    {
+                        stack.Push(i);
+                    }
+                    else
+                    {
+                        maxlen = Math.Max(maxlen, i - stack.Peek());
+                    }
+                }
+            }
+            return maxlen;
+        }
+    }
+}
