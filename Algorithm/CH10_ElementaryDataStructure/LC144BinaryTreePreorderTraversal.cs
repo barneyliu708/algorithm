a7e7ae512ea52c5e0e377aca5e59ab02abb0901f
barneyliu708 <barneyliu708@gmail.com>
2021-12-28 21:12:24 -0800

    ALGORITHM-55 LC144 Binary Tree Preorder Traversal - Stack Solution
    stack solution: https://www.cnblogs.com/grandyang/p/4146981.html

---
+++ Algorithm/CH10_ElementaryDataStructure/LC144BinaryTreePreorderTraversal.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    public class LC144BinaryTreePreorderTraversal
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+        public IList<int> PreorderTraversal(TreeNode root)
+        {
+            IList<int> results = new List<int>();
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+            TreeNode p = root;
+            while (stack.Count != 0 || p != null)
+            {
+                if (p != null)
+                {
+                    stack.Push(p);
+                    results.Add(p.val);
+                    p = p.left;
+                }
+                else
+                {
+                    p = stack.Pop();
+                    p = p.right;
+                }
+            }
+            return results;
+        }
+    }
+}
