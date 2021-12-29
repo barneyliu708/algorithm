100574ac43d24fd7357301680d43b9274d661105
barneyliu708 <barneyliu708@gmail.com>
2021-12-28 19:54:17 -0800

    ALGORITHM-50 LC084 Largest Rectangle in Histogram
    LC084 Largest Rectangle in Histogram - solution https://leetcode.com/problems/largest-rectangle-in-histogram/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC084LargestRectangleInHistogram.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    public class LC084LargestRectangleInHistogram
+    {
+        public int LargestRectangleArea(int[] heights)
+        {
+            Stack<int> stack = new Stack<int>();
+            stack.Push(-1);
+            int maxArea = 0;
+            for (int i = 0; i < heights.Length; i++)
+            {
+                while (stack.Peek() != -1 && heights[stack.Peek()] >= heights[i])
+                {
+                    int currentHeight = heights[stack.Peek()];
+                    stack.Pop();
+                    int currentWidth = i - stack.Peek() - 1;
+                    maxArea = Math.Max(maxArea, currentHeight * currentWidth);
+                }
+                stack.Push(i);
+            }
+            while (stack.Peek() != -1)
+            {
+                int currentHeight = heights[stack.Peek()];
+                stack.Pop();
+                int currentWidth = heights.Length - stack.Peek() - 1;
+                maxArea = Math.Max(maxArea, currentHeight * currentWidth);
+            }
+
+            return maxArea;
+        }
+    }
+}
