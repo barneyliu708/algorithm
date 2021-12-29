c14dd682a84e75fe2e2a8721131fbcc1da5693b4
barneyliu708 <barneyliu708@gmail.com>
2021-12-28 18:52:18 -0800

    LC071 Simplify Path
    LC071 Simplify Path solution - stack

---
+++ Algorithm/CH10_ElementaryDataStructure/LC071SimplifyPath.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    public class LC071SimplifyPath
+    {
+        public string SimplifyPath(string path)
+        {
+
+            Stack<string> stack = new Stack<string>();
+
+            string[] components = path.Split("/");
+            foreach (string directory in components)
+            {
+
+                if (string.Equals(directory, ".") || string.IsNullOrEmpty(directory))
+                {
+                    continue;
+                }
+
+                else if (string.Equals(directory, ".."))
+                {
+                    if (stack.Count != 0)
+                    {
+                        stack.Pop();
+                    }
+                }
+
+                else
+                {
+                    stack.Push(directory);
+                }
+            }
+
+            StringBuilder result = new StringBuilder();
+            foreach (string directory in stack)
+            {
+                result.Insert(0, directory);
+                result.Insert(0, "/");
+            }
+
+            return result.Length > 0 ? result.ToString() : "/";
+        }
+    }
+}
