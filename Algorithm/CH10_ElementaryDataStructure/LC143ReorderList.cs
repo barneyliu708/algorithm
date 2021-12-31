cfc4e21863b8ef53b10ade463387175e10133b51
barneyliu708 <barneyliu708@gmail.com>
2021-12-30 22:37:42 -0800

    ALGORITHM-54 LC143 Reorder List - Stack Solution
    Stack Solution: solution 2 of https://www.cnblogs.com/grandyang/p/4254860.html

---
+++ Algorithm/CH10_ElementaryDataStructure/LC143ReorderList.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    public class LC143ReorderList
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+        public void ReorderList(ListNode head)
+        {
+
+            ListNode cur = head;
+            Stack<ListNode> stack = new Stack<ListNode>();
+            while (cur != null)
+            {
+                stack.Push(cur);
+                cur = cur.next;
+            }
+
+            int count = stack.Count / 2;
+            cur = head;
+            while (count-- > 0)
+            {
+                ListNode tail = stack.Pop();
+                ListNode next = cur.next;
+                cur.next = tail;
+                tail.next = next;
+                cur = next;
+            }
+            cur.next = null;
+        }
+    }
+}
