a362905fc20d79cd385d16bb49c873c115a6a563
barneyliu708 <barneyliu708@gmail.com>
2022-01-01 21:09:28 -0800

    ALGORITHM-57 LC150 Evaluate Reverse Polish Notation - Stack Solution

---
+++ Algorithm/CH10_ElementaryDataStructure/LC150EvaluateReversePolishNotation.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    public class LC150EvaluateReversePolishNotation
+    {
+        public int EvalRPN(string[] tokens)
+        {
+            Stack<int> stack = new Stack<int>();
+            foreach (string token in tokens)
+            {
+                if (!IsOperator(token))
+                {
+                    stack.Push(int.Parse(token));
+                }
+                else
+                {
+                    int rightOperand = stack.Pop();
+                    int leftOperand = stack.Pop();
+                    int result;
+                    if (token == "+")
+                    {
+                        result = leftOperand + rightOperand;
+                    }
+                    else if (token == "-")
+                    {
+                        result = leftOperand - rightOperand;
+                    }
+                    else if (token == "*")
+                    {
+                        result = leftOperand * rightOperand;
+                    }
+                    else
+                    { // division
+                        result = leftOperand / rightOperand;
+                    }
+                    stack.Push(result);
+                }
+            }
+            return stack.Peek();
+        }
+
+        private bool IsOperator(string s)
+        {
+            return s == "+" || s == "-" || s == "*" || s == "/";
+        }
+    }
+}
