400b7c99a3891b26717e22fef02eed173e8c9f4f
barneyliu708 <barneyliu708@gmail.com>
2022-01-01 22:12:54 -0800

    ALGORITHM-59 LC173 Binary Search Tree Iterator - stack solution
    Stack solution: https://leetcode.com/problems/binary-search-tree-iterator/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC173BinarySearchTreeIterator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    public class LC173BinarySearchTreeIterator
+    {
+        public class BSTIterator
+        {
+
+            private Stack<TreeNode> stack = new Stack<TreeNode>();
+
+            public BSTIterator(TreeNode root)
+            {
+                LeftMostInorder(root);
+            }
+
+            public int Next()
+            {
+
+                TreeNode curnode = stack.Pop();
+                if (curnode.right != null)
+                {
+                    LeftMostInorder(curnode.right);
+                }
+                return curnode.val;
+
+            }
+
+            public bool HasNext()
+            {
+                return stack.Count > 0;
+            }
+
+            private void LeftMostInorder(TreeNode root)
+            {
+                while (root != null)
+                {
+                    stack.Push(root);
+                    root = root.left;
+                }
+            }
+        }
+    }
+}
