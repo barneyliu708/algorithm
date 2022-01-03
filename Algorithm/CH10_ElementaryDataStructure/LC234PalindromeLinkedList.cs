91afc97b74b2e259070b0b81225872cbc18fd2e2
barneyliu708 <barneyliu708@gmail.com>
2022-01-02 21:32:05 -0800

    ALGORITHM-63 LC234 Palindrome Linked List - Stack Solution
    Stack Solution: https://leetcode.com/problems/palindrome-linked-list/solution/
    And similar to the stack solution of ALGORITHM-54 LC143 Reorder List

---
+++ Algorithm/CH10_ElementaryDataStructure/LC234PalindromeLinkedList.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    public class LC234PalindromeLinkedList
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+        public bool IsPalindrome(ListNode head)
+        {
+
+            Stack<ListNode> stack = new Stack<ListNode>();
+            ListNode cur = head;
+            while (cur != null)
+            {
+                stack.Push(cur);
+                cur = cur.next;
+            }
+
+            int count = stack.Count / 2;
+            cur = head;
+            while (count-- > 0)
+            {
+                if (cur.val != stack.Pop().val)
+                {
+                    return false;
+                }
+                cur = cur.next;
+            }
+            return true;
+        }
+    }
+}
