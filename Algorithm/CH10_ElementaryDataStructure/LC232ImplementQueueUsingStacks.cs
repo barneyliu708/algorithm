e06494368e98286125b664553edd4bd08ce1cbb1
barneyliu708 <barneyliu708@gmail.com>
2022-01-02 21:08:37 -0800

    ALGORITHM-62 LC232 Implement Queue using Stacks
    Solution approach 2: https://leetcode.com/problems/implement-queue-using-stacks/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC232ImplementQueueUsingStacks.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    public class LC232ImplementQueueUsingStacks
+    {
+        public class MyQueue
+        {
+
+            private Stack<int> stack1;
+            private Stack<int> stack2;
+            private int front;
+
+            public MyQueue()
+            {
+                stack1 = new Stack<int>();
+                stack2 = new Stack<int>();
+            }
+
+            public void Push(int x)
+            {
+                if (stack1.Count == 0)
+                {
+                    front = x;
+                }
+                stack1.Push(x);
+            }
+
+            public int Pop()
+            {
+                if (stack2.Count == 0)
+                {
+                    while (stack1.Count != 0)
+                    {
+                        stack2.Push(stack1.Pop());
+                    }
+                }
+                return stack2.Pop();
+            }
+
+            public int Peek()
+            {
+                if (stack2.Count != 0)
+                {
+                    return stack2.Peek();
+                }
+                return front;
+            }
+
+            public bool Empty()
+            {
+                return stack1.Count == 0 && stack2.Count == 0;
+            }
+        }
+
+    }
+}
