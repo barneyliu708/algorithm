26230dcdce46c8d44a157e4d8db50c4380a63b91
barneyliu708 <barneyliu708@gmail.com>
2022-01-02 20:53:08 -0800

    ALGORITHM-61 LC225 Implement Stack using Queues
    Solution: https://leetcode.com/problems/implement-stack-using-queues/solution/

--- Algorithm/CH10_ElementaryDataStructure/LC173BinarySearchTreeIterator.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC173BinarySearchTreeIterator.cs
@@ -6,6 +6,18 @@ namespace Algorithm.CH10_ElementaryDataStructure
 {
     public class LC173BinarySearchTreeIterator
     {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
         public class BSTIterator
         {
 
---
+++ Algorithm/CH10_ElementaryDataStructure/LC225ImplementStackUsingQueues.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    public class LC225ImplementStackUsingQueues
+    {
+        public class MyStack
+        {
+
+            private Queue<int> queue;
+
+            public MyStack()
+            {
+                queue = new Queue<int>();
+            }
+
+            public void Push(int x)
+            {
+                queue.Enqueue(x);
+                int count = queue.Count;
+                while (count > 1)
+                {
+                    queue.Enqueue(queue.Dequeue());
+                    count--;
+                }
+            }
+
+            public int Pop()
+            {
+                return queue.Dequeue();
+            }
+
+            public int Top()
+            {
+                return queue.Peek();
+            }
+
+            public bool Empty()
+            {
+                return queue.Count == 0;
+            }
+        }
+    }
+}
