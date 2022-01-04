c9e113fcc9dc61de80eb424e2f16e0fbbfe000ef
barneyliu708 <barneyliu708@gmail.com>
2022-01-03 20:22:42 -0800

    ALGORITHM-64 LC1249 Minimum Remove to Make Valid Parentheses - Stack solution
    stack solution: https://leetcode.com/problems/minimum-remove-to-make-valid-parentheses/solution/
    
    Time Complexity: O(n)
    Space Complexity: O(n)

---
+++ Algorithm/CH10_ElementaryDataStructure/LC1249MinimumRemoveToMakeValidParentheses.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    public class LC1249MinimumRemoveToMakeValidParentheses
+    {
+        public string MinRemoveToMakeValid(string s)
+        {
+
+            Stack<int> stack = new Stack<int>();
+            HashSet<int> indexesToRemoved = new HashSet<int>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '(')
+                {
+                    stack.Push(i);
+                }
+                else if (s[i] == ')')
+                {
+                    if (stack.Count > 0)
+                    {
+                        stack.Pop();
+                    }
+                    else
+                    {
+                        indexesToRemoved.Add(i);
+                    }
+                }
+            }
+
+            while (stack.Count != 0)
+            {
+                indexesToRemoved.Add(stack.Pop());
+            }
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (!indexesToRemoved.Contains(i))
+                {
+                    sb.Append(s[i]);
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
