5816690754002256f46a4047cb4093ab15d7546c
barneyliu708 <barneyliu708@gmail.com>
2022-01-03 21:04:28 -0800

    ALGORITHM-65 LC1762 Buildings With an Ocean View - monotonic stack
    monotonic stack solution: https://leetcode.com/problems/buildings-with-an-ocean-view/solution/
    
    monotonic stack explanation:
    https://leetcode.com/problems/sum-of-subarray-minimums/discuss/178876/stack-solution-with-very-detailed-explanation-step-by-step
    https://labuladong.gitbook.io/algo-en/ii.-data-structure/monotonicstack

---
+++ Algorithm/CH10_ElementaryDataStructure/LC1762BuildingsWithAnOceanView.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    public class LC1762BuildingsWithAnOceanView
+    {
+        public int[] FindBuildings(int[] heights)
+        {
+
+            int curMax = -1;
+            List<int> indices = new List<int>();
+            for (int i = heights.Length - 1; i >= 0; i--)
+            {
+                if (heights[i] > curMax)
+                {
+                    indices.Add(i);
+                    curMax = heights[i];
+                }
+            }
+
+            // swap the indices in the array
+            for (int i = 0; i < indices.Count / 2; i++)
+            {
+                int temp = indices[i];
+                indices[i] = indices[indices.Count - 1 - i];
+                indices[indices.Count - 1 - i] = temp;
+            }
+
+            return indices.ToArray();
+        }
+
+        public int[] FindBuildings_MonotonicStack(int[] heights)
+        {
+
+            Stack<int> stack = new Stack<int>();
+            List<int> indices = new List<int>();
+            for (int i = heights.Length - 1; i >= 0; i--)
+            {
+                while (stack.Count != 0 && heights[i] > heights[stack.Peek()])
+                {
+                    stack.Pop();
+                }
+
+                if (stack.Count == 0)
+                {
+                    indices.Add(i);
+                }
+
+                stack.Push(i);
+            }
+
+            // reverse the indices in the list
+            indices.Reverse();
+
+            return indices.ToArray();
+        }
+    }
+}
