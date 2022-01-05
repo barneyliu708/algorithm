a3ef1dcbc2408294daa5e035a374de9831ad58fd
barneyliu708 <barneyliu708@gmail.com>
2022-01-04 21:53:54 -0800

    ALGORITHM-68 LC098 Validate Binary Search Tree - recursion solution
    recursion solution - approach 1: https://leetcode.com/problems/validate-binary-search-tree/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC098ValidateBinarySearchTree.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    public class LC098ValidateBinarySearchTree
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+        public bool IsValidBST(TreeNode root)
+        {
+            return IsValidBSTUti(root, null, null);
+        }
+
+        private bool IsValidBSTUti(TreeNode curNode, TreeNode lower, TreeNode upper)
+        {
+
+            if (curNode == null)
+            {
+                return true;
+            }
+
+            if (lower != null && curNode.val <= lower.val ||
+                upper != null && curNode.val >= upper.val)
+            {
+                return false;
+            }
+
+            return IsValidBSTUti(curNode.left, lower, curNode) &&
+                   IsValidBSTUti(curNode.right, curNode, upper);
+        }
+    }
+}
