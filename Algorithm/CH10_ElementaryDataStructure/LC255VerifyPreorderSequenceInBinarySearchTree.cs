4f4ee225d1b719e1d457c281b8376bb5977cac1a
barneyliu708 <barneyliu708@gmail.com>
2022-01-04 21:10:53 -0800

    ALGORITHM-67 LC255 Verify Preorder Sequence in Binary Search Tree - Stack solution
    stack solution - first approach: https://www.cnblogs.com/grandyang/p/5327635.html

---
+++ Algorithm/CH10_ElementaryDataStructure/LC255VerifyPreorderSequenceInBinarySearchTree.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    public class LC255VerifyPreorderSequenceInBinarySearchTree
+    {
+        public bool VerifyPreorder(int[] preorder)
+        {
+
+            int low = int.MinValue;
+            Stack<int> stack = new Stack<int>();
+
+            foreach (int cur in preorder)
+            {
+
+                if (cur < low)
+                {
+                    return false;
+                }
+
+                while (stack.Count != 0 && cur > stack.Peek())
+                {
+                    low = stack.Pop();
+                }
+
+                stack.Push(cur);
+
+            }
+
+            return true;
+        }
+    }
+}
