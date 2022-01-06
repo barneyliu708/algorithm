c8050bd9b2a7017995a61e4f0b23234453829449
barneyliu708 <barneyliu708@gmail.com>
2022-01-05 22:20:22 -0800

    ALGORITHM-72 LC445 Add Two Numbers II - stack method
    stack solution - approach 1: https://www.cnblogs.com/grandyang/p/6216480.html

---
+++ Algorithm/CH10_ElementaryDataStructure/LC445AddTwoNumbersII.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    public class LC445AddTwoNumbersII
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            Stack<int> stack1 = new Stack<int>();
+            Stack<int> stack2 = new Stack<int>();
+
+            ListNode p = l1;
+            while (p != null)
+            {
+                stack1.Push(p.val);
+                p = p.next;
+            }
+            p = l2;
+            while (p != null)
+            {
+                stack2.Push(p.val);
+                p = p.next;
+            }
+
+            ListNode ans = new ListNode(0, null);
+            int sum = 0;
+            while (stack1.Count != 0 || stack2.Count != 0)
+            {
+
+                if (stack1.Count != 0)
+                {
+                    sum += stack1.Pop();
+                }
+                if (stack2.Count != 0)
+                {
+                    sum += stack2.Pop();
+                }
+                ans.val = sum % 10;
+                ListNode head = new ListNode(sum / 10);
+                head.next = ans;
+                ans = head;
+                sum /= 10;
+            }
+
+            return ans.val == 0 ? ans.next : ans;
+        }
+    }
+}
