37b169266680b0d13cbae9445d213880f1e8d570
barneyliu708 <barneyliu708@gmail.com>
2022-01-06 21:52:47 -0800

    ALGORITHM-75 LC503. Next Greater Element II - stack solution
    stack solution - approach 2: https://www.cnblogs.com/grandyang/p/6442861.html

---
+++ Algorithm/CH10_ElementaryDataStructure/LC503NextGreaterElementII.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    public class LC503NextGreaterElementII
+    {
+        public int[] NextGreaterElements(int[] nums)
+        {
+
+            Stack<int> stack = new Stack<int>();
+            int[] ans = new int[nums.Length];
+
+            for (int i = 0; i < nums.Length * 2; i++)
+            {
+                int cur = nums[i % nums.Length];
+                while (stack.Count != 0 && nums[stack.Peek()] < cur)
+                {
+                    ans[stack.Pop()] = cur;
+                }
+                if (i < nums.Length)
+                {
+                    stack.Push(i);
+                }
+            }
+
+            while (stack.Count != 0)
+            {
+                ans[stack.Pop()] = -1;
+            }
+
+            return ans;
+        }
+    }
+}
