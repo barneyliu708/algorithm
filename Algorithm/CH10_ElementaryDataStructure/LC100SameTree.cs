51826366b646d6340ddd2a114179b08be03af34a
barneyliu708 <barneyliu708@gmail.com>
2022-01-07 17:23:40 -0800

    ALGORITHM-77 LC100. Same Tree - dft solution
    dft solution - approach 1: https://leetcode.com/problems/same-tree/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC100SameTree.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    public class LC100SameTree
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+        public bool IsSameTree(TreeNode p, TreeNode q)
+        {
+
+            if (p == null && q == null)
+            {
+                return true;
+            }
+
+            if (p == null || q == null)
+            {
+                return false;
+            }
+
+            if (p.val != q.val)
+            {
+                return false;
+            }
+
+            return IsSameTree(p.left, q.left) &&
+                   IsSameTree(p.right, q.right);
+        }
+    }
+}
