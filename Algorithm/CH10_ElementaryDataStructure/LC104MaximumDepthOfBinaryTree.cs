ccec192fddc72d8553d06dc2e3caec754c18c280
barneyliu708 <barneyliu708@gmail.com>
2022-01-07 20:34:36 -0800

    ALGORITHM-79 LC104. Maximum Depth of Binary Tree - Recursion solution
    recursion solution - approach 1: https://leetcode.com/problems/maximum-depth-of-binary-tree/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC104MaximumDepthOfBinaryTree.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    public class LC104MaximumDepthOfBinaryTree
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+        public int MaxDepth(TreeNode root)
+        {
+
+            if (root == null)
+            {
+                return 0;
+            }
+
+            return Math.Max(MaxDepth(root.left), MaxDepth(root.right)) + 1;
+        }
+    }
+}
