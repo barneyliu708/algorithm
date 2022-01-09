e2ef3443442d1ad9a24073942efd49e2c4044158
barneyliu708 <barneyliu708@gmail.com>
2022-01-08 19:40:05 -0800

    ALGORITHM-82 LC112. Path Sum - recursion solution - dft
    dft solution - approach 1: https://leetcode.com/problems/path-sum/solution/
    
    Traversal the tree and maintain the target sum on each node

---
+++ Algorithm/CH10_ElementaryDataStructure/LC112PathSum.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    public class LC112PathSum
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+        public bool HasPathSum(TreeNode root, int targetSum)
+        {
+
+            if (root == null)
+            {
+                return false;
+            }
+
+            if (root.left == null && root.right == null) // this is a leaf, which has no children
+            {
+                return root.val == targetSum;
+            }
+
+            bool hasPathSum = false;
+            if (root.left != null)
+            {
+                hasPathSum = hasPathSum || HasPathSum(root.left, targetSum - root.val);
+            }
+
+            if (root.right != null)
+            {
+                hasPathSum = hasPathSum || HasPathSum(root.right, targetSum - root.val);
+            }
+
+            return hasPathSum;
+        }
+    }
+}
