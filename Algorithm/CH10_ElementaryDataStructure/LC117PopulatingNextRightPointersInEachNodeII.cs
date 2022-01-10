b618635e5534b459cbc16c86ee3c9acb413ca517
barneyliu708 <barneyliu708@gmail.com>
2022-01-09 20:28:20 -0800

    ALGORITHM-85 LC117. Populating Next Right Pointers in Each Node II - queue + bft
    queue + bft - approach 1: https://leetcode.com/problems/populating-next-right-pointers-in-each-node-ii/solution/
    
    same solution to the story ALGORITHM-84 LC116. Populating Next Right Pointers in Each Node

---
+++ Algorithm/CH10_ElementaryDataStructure/LC117PopulatingNextRightPointersInEachNodeII.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    public class LC117PopulatingNextRightPointersInEachNodeII
+    {
+        public class Node
+        {
+            public int val;
+            public Node left;
+            public Node right;
+            public Node next;
+
+            public Node() { }
+
+            public Node(int _val)
+            {
+                val = _val;
+            }
+
+            public Node(int _val, Node _left, Node _right, Node _next)
+            {
+                val = _val;
+                left = _left;
+                right = _right;
+                next = _next;
+            }
+        }
+        public Node Connect(Node root)
+        {
+
+            if (root == null)
+            {
+                return root;
+            }
+
+            Queue<Node> queue = new Queue<Node>();
+            queue.Enqueue(root);
+
+            while (queue.Count != 0)
+            {
+
+                int count = queue.Count;
+
+                for (int i = 0; i < count; i++)
+                {
+
+                    Node curNode = queue.Dequeue();
+
+                    if (i < count - 1)
+                    { // if curNode is not the rightmost node in current level.
+                        curNode.next = queue.Peek();
+                    }
+
+                    if (curNode.left != null)
+                    {
+                        queue.Enqueue(curNode.left);
+                    }
+
+                    if (curNode.right != null)
+                    {
+                        queue.Enqueue(curNode.right);
+                    }
+                }
+            }
+
+            return root;
+        }
+    }
+}
