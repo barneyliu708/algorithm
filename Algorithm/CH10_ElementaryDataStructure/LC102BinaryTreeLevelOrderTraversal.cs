0bf6f58cf22e4434631bae187001304a04d78c4e
barneyliu708 <barneyliu708@gmail.com>
2022-01-10 21:34:27 -0800

    ALGORITHM-90 LC102. Binary Tree Level Order Traversal - bft + queue solution
    bft + queue solution - approach 2: https://leetcode.com/problems/binary-tree-level-order-traversal/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC102BinaryTreeLevelOrderTraversal.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    public class LC102BinaryTreeLevelOrderTraversal
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+        public IList<IList<int>> LevelOrder(TreeNode root)
+        {
+
+            if (root == null)
+            {
+                return new List<IList<int>>();
+            }
+
+
+            IList<IList<int>> ans = new List<IList<int>>();
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+
+            while (queue.Count != 0)
+            {
+
+                int count = queue.Count;
+                IList<int> levelValues = new List<int>();
+
+                for (int i = 0; i < count; i++)
+                {
+
+                    TreeNode curNode = queue.Dequeue();
+                    levelValues.Add(curNode.val);
+
+                    if (curNode.left != null)
+                    {
+                        queue.Enqueue(curNode.left);
+                    }
+                    if (curNode.right != null)
+                    {
+                        queue.Enqueue(curNode.right);
+                    }
+                }
+
+                ans.Add(levelValues);
+            }
+
+            return ans;
+        }
+    }
+}
