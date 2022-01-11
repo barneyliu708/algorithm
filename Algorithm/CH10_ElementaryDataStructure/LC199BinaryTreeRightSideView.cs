8ebcc42c40046f6016b5177d92850dd1205e0feb
barneyliu708 <barneyliu708@gmail.com>
2022-01-10 20:24:47 -0800

    ALGORITHM-88 LC199. Binary Tree Right Side View - bft solution
    bft with queue - approach 1: https://leetcode.com/problems/binary-tree-right-side-view/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC199BinaryTreeRightSideView.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    public class LC199BinaryTreeRightSideView
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+        public IList<int> RightSideView(TreeNode root)
+        {
+
+            if (root == null)
+            {
+                return new List<int>();
+            }
+
+            IList<int> ans = new List<int>();
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+
+            while (queue.Count != 0)
+            {
+
+                int count = queue.Count;
+                for (int i = 0; i < count; i++)
+                {
+                    TreeNode curNode = queue.Dequeue();
+
+                    if (i == count - 1)
+                    { // the rightmost node in current level
+                        ans.Add(curNode.val);
+                    }
+
+                    if (curNode.left != null)
+                    {
+                        queue.Enqueue(curNode.left);
+                    }
+
+                    if (curNode.right != null)
+                    {
+                        queue.Enqueue(curNode.right);
+                    }
+                }
+            }
+
+            return ans;
+        }
+    }
+}
