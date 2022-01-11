b07ab3886cc219db02272197bc78d4aebc410c65
barneyliu708 <barneyliu708@gmail.com>
2022-01-10 21:46:52 -0800

    ALGORITHM-91 LC103. Binary Tree Zigzag Level Order Traversal - bft + queue
    bft + queue - approach 1: https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC103BinaryTreeZigzagLevelOrderTraversal.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    public class LC103BinaryTreeZigzagLevelOrderTraversal
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+        public IList<IList<int>> ZigzagLevelOrder(TreeNode root)
+        {
+            if (root == null)
+            {
+                return new List<IList<int>>();
+            }
+
+
+            IList<IList<int>> ans = new List<IList<int>>();
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+            int level = 0;
+
+            while (queue.Count != 0)
+            {
+
+                level++;
+                int count = queue.Count;
+                IList<int> levelValues = new List<int>();
+
+                for (int i = 0; i < count; i++)
+                {
+
+                    TreeNode curNode = queue.Dequeue();
+                    levelValues.Add(curNode.val);
+
+                    if (curNode.left != null)
+                    {
+                        queue.Enqueue(curNode.left);
+                    }
+                    if (curNode.right != null)
+                    {
+                        queue.Enqueue(curNode.right);
+                    }
+                }
+
+                if (level % 2 == 0)
+                {
+                    levelValues = levelValues.Reverse().ToList();
+                }
+                ans.Add(levelValues);
+            }
+
+            return ans;
+        }
+    }
+}
