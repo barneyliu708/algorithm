7132748155d0302ff9eaabd320aebf0641ca12f6
barneyliu708 <barneyliu708@gmail.com>
2022-01-11 22:10:14 -0800

    ALGORITHM-94 LC022. Generate Parentheses - backtracking / recursion
    recursion solution: https://www.cnblogs.com/grandyang/p/4444160.html

---
+++ Algorithm/CH10_ElementaryDataStructure/LC022GenerateParentheses.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC022GenerateParentheses
+    {
+        public IList<string> GenerateParenthesis(int n)
+        {
+            IList<string> ans = new List<string>();
+            utility(n, n, "", ans);
+            return ans;
+        }
+
+        private void utility(int left, int right, string output, IList<string> ans)
+        {
+            if (left > right)
+            {
+                return;
+            }
+            if (left == 0 && right == 0)
+            {
+                ans.Add(output);
+                return;
+            }
+
+            if (left > 0)
+            {
+                utility(left - 1, right, output + "(", ans);
+            }
+
+            if (right > 0)
+            {
+                utility(left, right - 1, output + ")", ans);
+            }
+        }
+    }
+}
