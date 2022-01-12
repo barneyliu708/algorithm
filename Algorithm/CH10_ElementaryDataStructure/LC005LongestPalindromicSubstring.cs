35ca4728eddfab82d011370d378ffb723dda2ea3
barneyliu708 <barneyliu708@gmail.com>
2022-01-11 21:51:59 -0800

    ALGORITHM-93 LC005. Longest Palindromic Substring - dp solution
    dp solution - approach 3: https://www.cnblogs.com/grandyang/p/4464476.html

---
+++ Algorithm/CH10_ElementaryDataStructure/LC005LongestPalindromicSubstring.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC005LongestPalindromicSubstring
+    {
+        public string LongestPalindrome(string s)
+        {
+
+            bool[,] dp = new bool[s.Length, s.Length];
+            int maxLen = 1; int left = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                dp[i, i] = true;
+
+                for (int l = 0; l < i; l++)
+                {
+                    if (l == i - 1)
+                    {
+                        dp[l, i] = s[l] == s[i];
+                    }
+                    else
+                    {
+                        dp[l, i] = s[l] == s[i] && dp[l + 1, i - 1];
+                    }
+
+                    if (dp[l, i] && maxLen < i - l + 1)
+                    { // update the current max lengh info
+                        maxLen = i - l + 1;
+                        left = l;
+                    }
+                }
+            }
+
+            return s.Substring(left, maxLen);
+        }
+    }
+}
