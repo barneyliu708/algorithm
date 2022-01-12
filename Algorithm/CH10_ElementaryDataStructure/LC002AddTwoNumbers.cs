73db545c1b80df9cc4bcd9a27d7e9dde31faa6a5
barneyliu708 <barneyliu708@gmail.com>
2022-01-11 21:07:28 -0800

    ALGORITHM-92 LC002. Add Two Numbers
    solution: https://www.cnblogs.com/grandyang/p/4129891.html

---
+++ Algorithm/CH10_ElementaryDataStructure/LC002AddTwoNumbers.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC002AddTwoNumbers
+    {
+
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+
+            ListNode dummy = new ListNode();
+            ListNode cur = dummy;
+            int carry = 0;
+
+            while (l1 != null || l2 != null || carry != 0)
+            {
+                int curSum = carry;
+                if (l1 != null)
+                {
+                    curSum += l1.val;
+                    l1 = l1.next;
+                }
+                if (l2 != null)
+                {
+                    curSum += l2.val;
+                    l2 = l2.next;
+                }
+
+                cur.next = new ListNode(curSum % 10);
+                carry = curSum / 10;
+
+                cur = cur.next;
+            }
+
+            return dummy.next;
+        }
+    }
+}
