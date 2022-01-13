addefd1fbd77b2bad2d96bf95497bb2209e6770d
barneyliu708 <barneyliu708@gmail.com>
2022-01-12 21:00:15 -0800

    ALGORITHM-95 LC055. Jump Game - backtracking solution
    backtracking solution - approach 1: https://leetcode.com/problems/jump-game/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC055JumpGame.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    public class LC055JumpGame
+    {
+        public class BacktrackingMethod
+        {
+            public bool CanJump(int[] nums)
+            {
+                return backtracking(0, nums);
+            }
+
+            private bool backtracking(int position, int[] nums)
+            {
+                if (position == nums.Length - 1)
+                {
+                    return true;
+                }
+
+                int maxJump = Math.Min(position + nums[position], nums.Length - 1);
+                for (int nextPosition = position + 1; nextPosition <= maxJump; nextPosition++)
+                {
+                    if (backtracking(nextPosition, nums))
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+        }
+
+
+    }
+}
