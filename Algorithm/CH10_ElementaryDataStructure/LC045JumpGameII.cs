6361966d21f5357ea6011be3d1e53885102e0665
barneyliu708 <barneyliu708@gmail.com>
2022-01-12 22:29:43 -0800

    ALGORITHM-96 LC045. Jump Game II - dp solution
    dp solution: https://leetcode.com/problems/jump-game-ii/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC045JumpGameII.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    [TestFixture]
+    public class LC045JumpGameII
+    {
+        public int Jump(int[] nums)
+        {
+            int[] dp = new int[nums.Length];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                dp[i] = int.MaxValue;
+            }
+
+            dp[dp.Length - 1] = 0; // the last position can always take 0 step to the end
+
+            for (int i = nums.Length - 2; i >= 0; i--)
+            {
+                int maxJump = Math.Min(i + nums[i], nums.Length - 1);
+
+                int minSteps = nums.Length; // use nums.Length rather that int.MaxValue as the default minsteps to void overflow if the maxJump at i is 0;
+                for (int j = i + 1; j <= maxJump; j++)
+                {
+                    if (dp[j] < minSteps)
+                    {
+                        minSteps = dp[j];
+                    }
+                }
+                dp[i] = minSteps + 1;
+
+            }
+
+            return dp[0];
+        }
+
+        [Test]
+        public void TestJump()
+        {
+            int[] nums = new int[] { 2, 3, 1, 1, 4 };
+            var test = Jump(nums);
+        }
+    }
+}
--- Algorithm/CH10_ElementaryDataStructure/LC111MinimumDepthOfBinaryTree.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC111MinimumDepthOfBinaryTree.cs
@@ -6,6 +6,18 @@ namespace Algorithm.CH10_ElementaryDataStructure
 {
     public class LC111MinimumDepthOfBinaryTree
     {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
         public int MinDepth(TreeNode root)
         {
 
