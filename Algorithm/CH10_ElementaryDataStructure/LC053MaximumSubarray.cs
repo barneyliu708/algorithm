e865a3337bf9f9a5efd250e62202b7bf6d250ff1
barneyliu708 <barneyliu708@gmail.com>
2022-01-12 22:53:52 -0800

    ALGORITHM-97 LC053. Maximum Subarray - dp solution
    dp solution - approach 2: https://leetcode.com/problems/maximum-subarray/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC053MaximumSubarray.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC053MaximumSubarray
+    {
+        public int MaxSubArray(int[] nums)
+        {
+
+            int curMax = nums[0];
+            int totalMax = nums[0];
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                int num = nums[i];
+                curMax = Math.Max(num, curMax + num);
+                totalMax = Math.Max(curMax, totalMax);
+            }
+
+            return totalMax;
+        }
+    }
+}
