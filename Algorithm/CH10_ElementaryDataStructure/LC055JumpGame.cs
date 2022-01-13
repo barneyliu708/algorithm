fe7f1817a77a220a38a5f8df810544eeb672d6a4
barneyliu708 <barneyliu708@gmail.com>
2022-01-12 21:39:10 -0800

    ALGORITHM-95 LC055. Jump Game - dp bottom-up method
    dp bottom-up - approach 3: https://leetcode.com/problems/jump-game/solution/

--- Algorithm/CH10_ElementaryDataStructure/LC055JumpGame.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC055JumpGame.cs
@@ -66,5 +66,37 @@ namespace Algorithm.CH10_ElementaryDataStructure
                 return false;
             }
         }
+
+        public class DynamicProgrammingBottomUpMethod
+        {
+            private int UNKNOWN = 0;
+            private int BAD = -1;
+            private int GOOD = 1;
+
+            public bool CanJump(int[] nums)
+            {
+
+                int[] dp = new int[nums.Length];
+
+                dp[dp.Length - 1] = GOOD; // the last position can always reach out to itself
+
+                for (int i = nums.Length - 2; i >= 0; i--)
+                {
+                    int maxJump = Math.Min(i + nums[i], nums.Length - 1);
+
+                    for (int j = i + 1; j <= maxJump; j++)
+                    {
+                        if (dp[j] == GOOD)
+                        {
+                            dp[i] = GOOD;
+                            break;
+                        }
+                    }
+
+                }
+
+                return dp[0] == GOOD;
+            }
+        }
     }
 }
