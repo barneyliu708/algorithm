633248bf8acf3510a7781e8ebfab4245dfdbaeec
barneyliu708 <barneyliu708@gmail.com>
2022-01-13 21:01:23 -0800

    ALGORITHM-99 LC063. Unique Paths II - dp solution
    dp solution:  very similar to the solution of story ALGORITHM-98 LC062. Unique Paths

---
+++ Algorithm/CH10_ElementaryDataStructure/LC063UniquePathsII.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC063UniquePathsII
+    {
+        public int UniquePathsWithObstacles(int[][] obstacleGrid)
+        {
+            int m = obstacleGrid.Length;
+            int n = obstacleGrid[0].Length;
+            int[,] dp = new int[m, n];
+
+            if (obstacleGrid[0][0] == 0)
+            {
+                dp[0, 0] = 1;
+            }
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i == 0 && j == 0)
+                    {
+                        continue;
+                    }
+                    if (obstacleGrid[i][j] == 0)
+                    {
+                        dp[i, j] = UniquePaths(obstacleGrid, dp, i, j - 1) + UniquePaths(obstacleGrid, dp, i - 1, j);
+                    }
+                }
+            }
+
+            return dp[m - 1, n - 1];
+        }
+
+        private int UniquePaths(int[][] obstacleGrid, int[,] dp, int i, int j)
+        {
+
+            if (i < 0 || i >= dp.GetLength(0) ||
+                j < 0 || j >= dp.GetLength(1) ||
+                obstacleGrid[i][j] == 1)
+            {
+                return 0;
+            }
+
+            return dp[i, j];
+        }
+    }
+}
