9ae6ae6a3e49a9971519cec3e7a839992ae56928
barneyliu708 <barneyliu708@gmail.com>
2022-01-14 14:47:13 -0800

    ALGORITHM-101 LC070. Climbing Stairs - dp solution
    dp solution - approach 3: https://leetcode.com/problems/climbing-stairs/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC070ClimbingStairs.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC070ClimbingStairs
+    {
+        public int ClimbStairs(int n)
+        {
+            int[] dp = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                if (i == 0)
+                {
+                    dp[i] = 1;
+                }
+                else if (i == 1)
+                {
+                    dp[i] = 2;
+                }
+                else
+                {
+                    dp[i] = dp[i - 1] + dp[i - 2];
+                }
+            }
+
+            return dp[n - 1];
+        }
+    }
+}
