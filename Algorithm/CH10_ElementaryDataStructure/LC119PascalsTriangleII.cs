ea8c8510ecf3a384268f2c56de5705fd6fc4b177
barneyliu708 <barneyliu708@gmail.com>
2022-01-14 20:25:14 -0800

    ALGORITHM-105 LC119. Pascal's Triangle II - dynamic programming
    dp solution - approach 3: https://leetcode.com/problems/pascals-triangle-ii/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC119PascalsTriangleII.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC119PascalsTriangleII
+    {
+        public IList<int> GetRow(int rowIndex)
+        {
+            int[] ans = new int[rowIndex + 1];
+
+            for (int i = 0; i <= rowIndex; i++)
+            {
+                for (int j = i; j >= 0; j--)
+                {
+                    if (j == 0)
+                    {
+                        ans[j] = 1;
+                    }
+                    else
+                    {
+                        ans[j] = ans[j - 1] + ans[j];
+                    }
+                }
+            }
+
+            return ans;
+        }
+    }
+}
