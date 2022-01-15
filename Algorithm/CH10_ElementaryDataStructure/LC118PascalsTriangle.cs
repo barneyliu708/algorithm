15884f1d7368791c0d8058afffe2b95259513ef8
barneyliu708 <barneyliu708@gmail.com>
2022-01-14 20:16:08 -0800

    ALGORITHM-104 LC118. Pascal's Triangle - dynamic programming
    dp solution: https://leetcode.com/problems/pascals-triangle/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC118PascalsTriangle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC118PascalsTriangle
+    {
+        public IList<IList<int>> Generate(int numRows)
+        {
+
+            IList<IList<int>> ans = new List<IList<int>>();
+
+            for (int i = 0; i < numRows; i++)
+            {
+                ans.Add(new List<int>());
+                for (int j = 0; j <= i; j++)
+                {
+                    if (j == 0 || j == i)
+                    {
+                        ans[i].Add(1);
+                    }
+                    else
+                    {
+                        ans[i].Add(ans[i - 1][j - 1] + ans[i - 1][j]);
+                    }
+                }
+            }
+
+            return ans;
+        }
+    }
+}
