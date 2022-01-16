40ff04673dd6efe18cdf0dcd586dabb885fedff1
barneyliu708 <barneyliu708@gmail.com>
2022-01-15 21:24:29 -0800

    ALGORITHM-108 LC121. Best Time to Buy and Sell Stock - dp
    dp: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC121BestTimeToBuyAndSellStock.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC121BestTimeToBuyAndSellStock
+    {
+        public int MaxProfit(int[] prices)
+        {
+
+            int curMinPrice = prices[0];
+            int curMaxProfit = 0;
+
+            for (int i = 1; i < prices.Length; i++)
+            {
+                curMaxProfit = Math.Max(curMaxProfit, prices[i] - curMinPrice);
+                curMinPrice = Math.Min(curMinPrice, prices[i]);
+            }
+
+            return curMaxProfit;
+        }
+    }
+}
+}
