da4d12d5f8259a4876fa7493941fa1e5b6358c87
barneyliu708 <barneyliu708@gmail.com>
2022-01-15 20:22:12 -0800

    ALGORITHM-106 LC097. Interleaving String - Recursion with memoization
    approach 2: https://leetcode.com/problems/interleaving-string/solution/

--- Algorithm/CH10_ElementaryDataStructure/LC097InterleavingString.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC097InterleavingString.cs
@@ -43,6 +43,54 @@ namespace Algorithm.CH10_ElementaryDataStructure
                 return ans;
             }
         }
+
+        public class RecursionWithMemoization_DynamicProgramming
+        {
+            public bool IsInterleave(string s1, string s2, string s3)
+            {
+                if (s1.Length + s2.Length != s3.Length)
+                {
+                    return false;
+                }
+
+                int[,] dp = new int[s1.Length, s2.Length];
+                for (int i = 0; i < dp.GetLength(0); i++)
+                {
+                    for (int j = 0; j < dp.GetLength(1); j++)
+                    {
+                        dp[i, j] = -1;
+                    }
+                }
+
+                return IsInterleave(s1, 0, s2, 0, dp, s3);
+            }
+
+            public bool IsInterleave(string s1, int i, string s2, int j, int[,] dp, string s3)
+            {
+
+                if (i == s1.Length)
+                {
+                    return s2.Substring(j) == s3.Substring(i + j);
+                }
+
+                if (j == s2.Length)
+                {
+                    return s1.Substring(i) == s3.Substring(i + j);
+                }
+
+                if (dp[i, j] != -1)
+                {
+                    return dp[i, j] == 1;
+                }
+
+                bool ans = (s3[i + j] == s1[i] && IsInterleave(s1, i + 1, s2, j, dp, s3)) ||
+                           (s3[i + j] == s2[j] && IsInterleave(s1, i, s2, j + 1, dp, s3));
+
+                dp[i, j] = ans ? 1 : 0;
+
+                return ans;
+            }
+        }
         
     }
 }
