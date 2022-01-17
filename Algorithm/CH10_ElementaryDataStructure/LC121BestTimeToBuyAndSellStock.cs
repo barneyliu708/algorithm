513820c5ec285ec9aaee3548e610bb63701bfd9d
barneyliu708 <barneyliu708@gmail.com>
2022-01-16 19:52:56 -0800

    ALGORITHM-109 LC122. Best Time to Buy and Sell Stock II
    solution: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/solution/

--- Algorithm/CH10_ElementaryDataStructure/LC121BestTimeToBuyAndSellStock.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC121BestTimeToBuyAndSellStock.cs
@@ -22,4 +22,3 @@ namespace Algorithm.CH10_ElementaryDataStructure
         }
     }
 }
-}
---
+++ Algorithm/CH10_ElementaryDataStructure/LC122BestTimeToBuyAndSellStockII.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    public class LC122BestTimeToBuyAndSellStockII
+    {
+        public int MaxProfit(int[] prices)
+        {
+
+            int ans = 0;
+            for (int i = 1; i < prices.Length; i++)
+            {
+                if (prices[i] > prices[i - 1])
+                {
+                    ans += prices[i] - prices[i - 1];
+                }
+            }
+
+            return ans;
+        }
+    }
+}
