7368a72bb442db156db42062ffc5aa3154fe9e94
barneyliu708 <barneyliu708@gmail.com>
2022-01-17 20:13:12 -0800

    ALGORITHM-113 LC198. House Robber - dynamic programming
    dynamic programming - approach 2: https://leetcode.com/problems/house-robber/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC198HouseRobber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC198HouseRobber
+    {
+        public int Rob(int[] nums)
+        {
+
+            if (nums == null || nums.Length == 0)
+            {
+                return 0;
+            }
+
+            if (nums.Length == 1)
+            {
+                return nums[0];
+            }
+
+            int[] dp = new int[nums.Length];
+            dp[0] = nums[0];
+            dp[1] = Math.Max(dp[0], nums[1]);
+
+            for (int i = 2; i < nums.Length; i++)
+            {
+                dp[i] = Math.Max(dp[i - 2] + nums[i], dp[i - 1]);
+            }
+
+            return dp[nums.Length - 1];
+        }
+    }
+}
