363dd13f8b0c60d0f1e975367216daf95dd02c69
barneyliu708 <barneyliu708@gmail.com>
2022-01-19 20:09:40 -0800

    ALGORITHM-118 LC263. Ugly Number - recursion

---
+++ Algorithm/CH10_ElementaryDataStructure/LC263UglyNumber.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC263UglyNumber
+    {
+        public bool IsUgly(int n)
+        {
+
+            if (n == 0)
+            {
+                return false;
+            }
+
+            if (n == 1)
+            {
+                return true;
+            }
+
+            if (n % 2 == 0)
+            {
+                return IsUgly(n / 2);
+            }
+            if (n % 3 == 0)
+            {
+                return IsUgly(n / 3);
+            }
+            if (n % 5 == 0)
+            {
+                return IsUgly(n / 5);
+            }
+
+            return false;
+        }
+    }
+}
