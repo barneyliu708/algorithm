07ac909d8402be0816350ec15de54403979ac7dc
barneyliu708 <barneyliu708@gmail.com>
2022-01-19 21:37:17 -0800

    ALGORITHM-120 LC279. Perfect Squares - dp solution
    dp solution - approach 2: https://leetcode.com/problems/perfect-squares/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC279PerfectSquares.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC279PerfectSquares
+    {
+        public int NumSquares(int n)
+        {
+
+            int[] dp = new int[n + 1];
+            for (int i = 0; i < dp.Length; i++)
+            {
+                dp[i] = int.MaxValue;
+            }
+            dp[0] = 0;
+
+            int maxSqrIndx = (int)Math.Sqrt(n) + 1;
+            int[] squares = new int[maxSqrIndx];
+            for (int i = 1; i < maxSqrIndx; i++)
+            {
+                squares[i] = i * i;
+            }
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int s = 1; s < maxSqrIndx; s++)
+                {
+                    if (i < squares[s])
+                    {
+                        break;
+                    }
+
+                    dp[i] = Math.Min(dp[i], dp[i - squares[s]] + 1);
+                }
+            }
+
+            return dp[n];
+        }
+    }
+}
