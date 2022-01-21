7b302d6556c0994d3ffcbe97a0e132b6433826ba
barneyliu708 <barneyliu708@gmail.com>
2022-01-20 22:11:16 -0800

    ALGORITHM-122 LC309. Best Time to Buy and Sell Stock with Cooldown - dp solution
    dp solution: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC309BestTimeToBuyAndSellStockWithCooldown.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC309BestTimeToBuyAndSellStockWithCooldown
+    {
+        public int MaxProfit(int[] prices)
+        {
+            int n = prices.Length;
+            int[] buy = new int[n];
+            int[] sell = new int[n];
+            int[] rest = new int[n];
+
+            buy[0] = -prices[0];
+            for (int i = 1; i < n; i++)
+            {
+                buy[i] = Math.Max(rest[i - 1] - prices[i], buy[i - 1]);
+                sell[i] = Math.Max(buy[i - 1] + prices[i], sell[i - 1]);
+                rest[i] = Math.Max(Math.Max(buy[i - 1], sell[i - 1]), rest[i - 1]);
+            }
+
+
+            return Math.Max(Math.Max(buy[n - 1], sell[n - 1]), rest[n - 1]);
+        }
+    }
+}
