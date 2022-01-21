9f6c9983338efae6b5b894ffd2fde7964a0f262f
barneyliu708 <barneyliu708@gmail.com>
2022-01-20 21:27:30 -0800

    ALGORITHM-121 LC300. Longest Increasing Subsequence - dp solution
    dp solution - approach 1: https://leetcode.com/problems/longest-increasing-subsequence/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC300LongestIncreasingSubsequence.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC300LongestIncreasingSubsequence
+    {
+        public int LengthOfLIS(int[] nums)
+        {
+
+            int[] dp = new int[nums.Length];
+            for (int i = 0; i < dp.Length; i++)
+            {
+                dp[i] = 1;
+            }
+
+            int ans = 1;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (nums[i] > nums[j])
+                    {
+                        dp[i] = Math.Max(dp[i], dp[j] + 1);
+                    }
+                }
+                ans = Math.Max(ans, dp[i]);
+            }
+
+            return ans;
+        }
+    }
+}
