c3a5223598d644870f86bd09db62ff1e8d2aa94c
barneyliu708 <barneyliu708@gmail.com>
2022-01-21 20:18:08 -0800

    ALGORITHM-124 LC338. Counting Bits - dp
    dp solution  - approach 3: https://leetcode.com/problems/counting-bits/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC338CountingBits.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC338CountingBits
+    {
+        public int[] CountBits(int n)
+        {
+
+            int[] ans = new int[n + 1];
+            for (int x = 1; x <= n; x++)
+            {
+                // x / 2 is x >> 1, and x % 2 is x & 1
+                ans[x] = ans[x >> 1] + (x & 1);
+            }
+
+            return ans;
+        }
+    }
+}
