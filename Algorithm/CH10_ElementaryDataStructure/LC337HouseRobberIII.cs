d9c242da17a9f812dcdd7029629baac1c4987189
barneyliu708 <barneyliu708@gmail.com>
2022-01-21 22:37:00 -0800

    ALGORITHM-126 LC337. House Robber III - Recursion + Memoization
    recursion + memoization - approach 2: https://leetcode.com/problems/house-robber-iii/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC337HouseRobberIII.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC337HouseRobberIII
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        Dictionary<TreeNode, int> robResults = new Dictionary<TreeNode, int>();
+        Dictionary<TreeNode, int> notRobResults = new Dictionary<TreeNode, int>();
+
+        public int Rob(TreeNode root)
+        {
+            return Rob(root, false);
+        }
+
+        private int Rob(TreeNode root, bool isParentRobbed)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            if (isParentRobbed)
+            {
+                if (robResults.ContainsKey(root))
+                {
+                    return robResults[root];
+                }
+                int result = Rob(root.left, false) + Rob(root.right, false);
+                robResults[root] = result;
+                return result;
+            }
+            else
+            {
+                if (notRobResults.ContainsKey(root))
+                {
+                    return notRobResults[root];
+                }
+                int rob = root.val + Rob(root.left, true) + Rob(root.right, true);
+                int notRob = Rob(root.left, false) + Rob(root.right, false);
+                int result = Math.Max(rob, notRob);
+                notRobResults[root] = result;
+                return result;
+            }
+        }
+    }
+}
