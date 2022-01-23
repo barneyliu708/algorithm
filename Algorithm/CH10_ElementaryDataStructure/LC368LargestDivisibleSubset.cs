273b63ae6362a0bd02a8af66deddae0035f269bc
barneyliu708 <barneyliu708@gmail.com>
2022-01-22 21:07:18 -0800

    ALGORITHM-129 LC392. Is Subsequence - dp solution
    dp solution - approach 1: https://leetcode.com/problems/largest-divisible-subset/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC368LargestDivisibleSubset.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC368LargestDivisibleSubset
+    {
+        public IList<int> LargestDivisibleSubset(int[] nums)
+        {
+
+            if (nums.Length == 0)
+            {
+                return new List<int>();
+            }
+
+            Array.Sort(nums);
+            IList<int> ans = new List<int>();
+            IList<IList<int>> EDS = new List<IList<int>>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                EDS.Add(new List<int>());
+            }
+            for (int i = 0; i < nums.Length; i++)
+            {
+                IList<int> maxSubset = new List<int>();
+
+                for (int k = 0; k < i; k++)
+                {
+                    if (nums[i] % nums[k] == 0 && maxSubset.Count < EDS[k].Count)
+                    {
+                        maxSubset = EDS[k];
+                    }
+                }
+
+                // EDS[i].AddRange(maxSubset);
+                foreach (var el in maxSubset)
+                {
+                    EDS[i].Add(el);
+                }
+                EDS[i].Add(nums[i]);
+
+                if (ans.Count < EDS[i].Count)
+                {
+                    ans = EDS[i];
+                }
+            }
+
+            return ans;
+        }
+    }
+}
