672129dee448dddf27c6458a1b4bd6a3be31cccf
barneyliu708 <barneyliu708@gmail.com>
2022-01-22 20:39:21 -0800

    ALGORITHM-128 LC392. Is Subsequence - 2 pointers
    2 pointers - approach 2: https://leetcode.com/problems/is-subsequence/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC392IsSubsequence.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC392IsSubsequence
+    {
+        public bool IsSubsequence(string s, string t)
+        {
+
+            int l = 0;
+            int r = 0;
+
+            while (l < s.Length && r < t.Length)
+            {
+                if (s[l] == t[r])
+                {
+                    l++;
+                }
+                r++;
+            }
+
+            return l == s.Length;
+        }
+    }
+}
