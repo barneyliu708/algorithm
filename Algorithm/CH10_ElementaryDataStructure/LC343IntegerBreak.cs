f720a513457b1637f9422d457b05920d9b432aec
barneyliu708 <barneyliu708@gmail.com>
2022-01-22 20:30:39 -0800

    ALGORITHM-127 LC343. Integer Break - dp bottom up
    dp bottom up: https://www.cnblogs.com/grandyang/p/5411919.html

---
+++ Algorithm/CH10_ElementaryDataStructure/LC343IntegerBreak.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC343IntegerBreak
+    {
+        public int IntegerBreak(int n)
+        {
+
+            int[] dp = new int[n + 1];
+            dp[0] = 0;
+            dp[1] = 1;
+            dp[2] = 1;
+            for (int i = 3; i <= n; i++)
+            {
+                for (int j = 1; j < i; j++)
+                {
+                    dp[i] = Math.Max(dp[i], Math.Max(dp[j] * (i - j), j * (i - j)));
+
+                }
+            }
+
+            return dp[n];
+        }
+    }
+}
