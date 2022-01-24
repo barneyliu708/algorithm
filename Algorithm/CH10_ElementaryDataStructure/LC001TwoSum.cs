338f20b73e6a1a0cf8324f5df9e84387594c8060
barneyliu708 <barneyliu708@gmail.com>
2022-01-23 21:16:18 -0800

    ALGORITHM-130 LC001. Two Sum - hash table
    hash table - approach 3: https://leetcode.com/problems/two-sum/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC001TwoSum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC001TwoSum
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+
+            Dictionary<int, int> map = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (map.ContainsKey(target - nums[i]))
+                {
+                    return new int[] { map[target - nums[i]], i };
+                }
+                map[nums[i]] = i;
+            }
+
+            return new int[2];
+        }
+    }
+}
