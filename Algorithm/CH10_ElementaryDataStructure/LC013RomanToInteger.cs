e2a25da39f42f31c58a5eb539f68f07d0c9fc012
barneyliu708 <barneyliu708@gmail.com>
2022-01-24 15:33:38 -0800

    ALGORITHM-133 LC013. Roman to Integer - hash map
    hash map - approach 1: https://leetcode.com/problems/roman-to-integer/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC013RomanToInteger.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC013RomanToInteger
+    {
+        public int RomanToInt(string s)
+        {
+
+            Dictionary<char, int> map = new Dictionary<char, int>() {
+                { 'M', 1000 },
+                { 'D', 500 },
+                { 'C', 100 },
+                { 'L', 50 },
+                { 'X', 10 },
+                { 'V', 5 },
+                { 'I', 1 },
+            };
+
+            int ans = 0;
+            for (int i = 0; i < s.Length;)
+            {
+                if (i + 1 < s.Length && map[s[i]] < map[s[i + 1]])
+                {
+                    ans += map[s[i + 1]] - map[s[i]];
+                    i += 2;
+                }
+                else
+                {
+                    ans += map[s[i]];
+                    i++;
+                }
+            }
+
+            return ans;
+        }
+    }
+}
