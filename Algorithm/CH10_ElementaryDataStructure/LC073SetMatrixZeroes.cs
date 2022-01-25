95ec6021e5590cdff125661d6e3088af941069c4
barneyliu708 <barneyliu708@gmail.com>
2022-01-24 20:40:00 -0800

    ALGORITHM-137 LC073. Set Matrix Zeroes - hash table
     hash table - approach 1: https://leetcode.com/problems/set-matrix-zeroes/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC073SetMatrixZeroes.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC073SetMatrixZeroes
+    {
+        public void SetZeroes(int[][] matrix)
+        {
+
+            HashSet<int> rows = new HashSet<int>();
+            HashSet<int> cols = new HashSet<int>();
+
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                for (int j = 0; j < matrix[0].Length; j++)
+                {
+                    if (matrix[i][j] == 0)
+                    {
+                        rows.Add(i);
+                        cols.Add(j);
+                    }
+                }
+            }
+
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                for (int j = 0; j < matrix[0].Length; j++)
+                {
+                    if (rows.Contains(i) || cols.Contains(j))
+                    {
+                        matrix[i][j] = 0;
+                    }
+                }
+            }
+        }
+    }
+}
