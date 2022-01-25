6728427c32abd90faa62347acc979d65308c8de7
barneyliu708 <barneyliu708@gmail.com>
2022-01-24 20:24:29 -0800

    ALGORITHM-136 LC049. Group Anagrams - hash table
    hash table - approach 2: https://leetcode.com/problems/group-anagrams/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC049GroupAnagrams.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC049GroupAnagrams
+    {
+        public IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+
+            Dictionary<string, IList<string>> map = new Dictionary<string, IList<string>>();
+
+            foreach (string str in strs)
+            {
+                string hash = Hash(str);
+                if (map.ContainsKey(hash))
+                {
+                    map[hash].Add(str);
+                }
+                else
+                {
+                    map[hash] = new List<string>();
+                    map[hash].Add(str);
+                }
+            }
+
+            return map.Values.ToList();
+        }
+
+        private string Hash(string str)
+        {
+            int[] count = new int[26];
+            for (int i = 0; i < str.Length; i++)
+            {
+                count[str[i] - 'a']++;
+            }
+
+            StringBuilder sb = new StringBuilder();
+            foreach (int c in count)
+            {
+                sb.Append("#" + c.ToString());
+            }
+
+            return sb.ToString();
+        }
+    }
+}
