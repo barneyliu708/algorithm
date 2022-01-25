82c0a6ee317fbb1357ec4b5396a9bdfd7e672218
barneyliu708 <barneyliu708@gmail.com>
2022-01-24 17:17:02 -0800

    ALGORITHM-135 LC036. Valid Sudoku - hash table
    hash table - approach 1: https://leetcode.com/problems/valid-sudoku/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC036ValidSudoku.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC036ValidSudoku
+    {
+        public bool IsValidSudoku(char[][] board)
+        {
+
+            int N = 9;
+
+            HashSet<char>[] rows = new HashSet<char>[N];
+            HashSet<char>[] cols = new HashSet<char>[N];
+            HashSet<char>[] boxes = new HashSet<char>[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                rows[i] = new HashSet<char>();
+                cols[i] = new HashSet<char>();
+                boxes[i] = new HashSet<char>();
+            }
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    char val = board[i][j];
+
+                    if (val == '.')
+                    {
+                        continue;
+                    }
+
+                    if (rows[i].Contains(val))
+                    {
+                        return false;
+                    }
+                    rows[i].Add(val);
+
+                    if (cols[j].Contains(val))
+                    {
+                        return false;
+                    }
+                    cols[j].Add(val);
+
+                    int idx = (i / 3) * 3 + j / 3;
+                    if (boxes[idx].Contains(val))
+                    {
+                        return false;
+                    }
+                    boxes[idx].Add(val);
+
+                }
+            }
+
+            return true;
+        }
+    }
+}
