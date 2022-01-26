ff172be7010810532da61929aef789941cc33f9f
barneyliu708 <barneyliu708@gmail.com>
2022-01-25 21:10:48 -0800

    ALGORITHM-140 LC138. Copy List with Random Pointer - dft + hash table
    dft + hash table - approach 1: https://leetcode.com/problems/copy-list-with-random-pointer/solution/
    
    similar to the solution of story ALGORITHM-139 LC133. Clone Graph

---
+++ Algorithm/CH10_ElementaryDataStructure/LC138CopyListWithRandomPointer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC138CopyListWithRandomPointer
+    {
+
+        // Definition for a Node.
+        public class Node
+        {
+            public int val;
+            public Node next;
+            public Node random;
+
+            public Node(int _val)
+            {
+                val = _val;
+                next = null;
+                random = null;
+            }
+        }
+
+        public Node CopyRandomList(Node head)
+        {
+            Dictionary<Node, Node> visited = new Dictionary<Node, Node>();
+            return CopyRandomList(head, visited);
+        }
+
+        private Node CopyRandomList(Node head, Dictionary<Node, Node> visited)
+        {
+            if (head == null)
+            {
+                return head;
+            }
+
+            if (visited.ContainsKey(head))
+            {
+                return visited[head];
+            }
+
+            Node clone = new Node(head.val);
+            visited[head] = clone;
+
+            clone.next = CopyRandomList(head.next, visited);
+            clone.random = CopyRandomList(head.random, visited);
+
+            return clone;
+        }
+    }
+}
