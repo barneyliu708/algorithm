f3011444fd0392ba8fb57cc215303b6d821cbc1d
barneyliu708 <barneyliu708@gmail.com>
2022-01-25 20:32:00 -0800

    ALGORITHM-138 LC128. Longest Consecutive Sequence - hash table
    hash table - approach 3: https://leetcode.com/problems/longest-consecutive-sequence/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC128LongestConsecutiveSequence.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC128LongestConsecutiveSequence
+    {
+        public int LongestConsecutive(int[] nums)
+        {
+
+            HashSet<int> hashset = new HashSet<int>();
+            foreach (int num in nums)
+            {
+                hashset.Add(num);
+            }
+
+            int ans = 0;
+            foreach (int num in hashset)
+            {
+                if (!hashset.Contains(num - 1))
+                {
+                    int curNum = num;
+                    int curLength = 1;
+
+                    while (hashset.Contains(curNum + 1))
+                    {
+                        curNum++;
+                        curLength++;
+                    }
+
+                    ans = Math.Max(ans, curLength);
+                }
+            }
+
+            return ans;
+        }
+    }
+}
