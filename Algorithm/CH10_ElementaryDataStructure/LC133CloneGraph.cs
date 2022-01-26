2a2c92611bfdb321aee69f669f9886932dd2c0fb
barneyliu708 <barneyliu708@gmail.com>
2022-01-25 20:53:12 -0800

    ALGORITHM-139 LC133. Clone Graph - dft + hash table
    dft + hash table - approach 1: https://leetcode.com/problems/clone-graph/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC133CloneGraph.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+
+    // Definition for a Node.
+    public class Node
+    {
+        public int val;
+        public IList<Node> neighbors;
+
+        public Node()
+        {
+            val = 0;
+            neighbors = new List<Node>();
+        }
+
+        public Node(int _val)
+        {
+            val = _val;
+            neighbors = new List<Node>();
+        }
+
+        public Node(int _val, List<Node> _neighbors)
+        {
+            val = _val;
+            neighbors = _neighbors;
+        }
+    }
+
+    class LC133CloneGraph
+    {
+        public Node CloneGraph(Node node)
+        {
+            Dictionary<Node, Node> visited = new Dictionary<Node, Node>();
+            return CloneGraph(node, visited);
+        }
+
+        private Node CloneGraph(Node node, Dictionary<Node, Node> visited)
+        {
+
+            if (node == null)
+            {
+                return node;
+            }
+
+            if (visited.ContainsKey(node))
+            {
+                return visited[node];
+            }
+
+            Node cloneNode = new Node(node.val);
+            visited[node] = cloneNode;
+
+            foreach (Node neighbor in node.neighbors)
+            {
+                cloneNode.neighbors.Add(CloneGraph(neighbor, visited));
+            }
+
+            return cloneNode;
+        }
+    }
+}
