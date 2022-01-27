f18f4bebb5946ddfc3c532920d05e7c8aa67881b
barneyliu708 <barneyliu708@gmail.com>
2022-01-26 21:02:00 -0800

    ALGORITHM-144 LC160. Intersection of Two Linked Lists - Hash Table
    Hash Table - approach 2: https://leetcode.com/problems/intersection-of-two-linked-lists/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC160IntersectionOfTwoLinkedLists.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC160IntersectionOfTwoLinkedLists
+    {
+
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x) { val = x; }
+        }
+
+        public ListNode GetIntersectionNode(ListNode headA, ListNode headB)
+        {
+
+            HashSet<ListNode> hashset = new HashSet<ListNode>();
+
+            while (headA != null)
+            {
+                hashset.Add(headA);
+                headA = headA.next;
+            }
+
+            while (headB != null)
+            {
+                if (hashset.Contains(headB))
+                {
+                    return headB;
+                }
+                headB = headB.next;
+            }
+
+            return null;
+        }
+    }
+}
