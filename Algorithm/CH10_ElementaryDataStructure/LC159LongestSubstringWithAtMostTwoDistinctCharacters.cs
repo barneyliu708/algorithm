2c38372b23efcfa9684a6676246470d000889994
barneyliu708 <barneyliu708@gmail.com>
2022-01-26 21:52:05 -0800

    ALGORITHM-145 LC159. Longest Substring with At Most Two Distinct Characters - hash table
    hash table + sliding window - approach 2: https://leetcode.com/problems/longest-substring-without-repeating-characters/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC159LongestSubstringWithAtMostTwoDistinctCharacters.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC159LongestSubstringWithAtMostTwoDistinctCharacters
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+
+            int[] chars = new int[128];
+
+            int left = 0;
+            int right = 0;
+
+            int ans = 0;
+            while (right < s.Length)
+            {
+                char r = s[right];
+                chars[r]++;
+
+                while (chars[r] > 1)
+                {
+                    char l = s[left];
+                    chars[l]--;
+                    left++;
+                }
+
+                ans = Math.Max(ans, right - left + 1);
+
+                right++;
+            }
+
+            return ans;
+        }
+    }
+}
