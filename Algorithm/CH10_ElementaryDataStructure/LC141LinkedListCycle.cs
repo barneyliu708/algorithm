9a329795b8640f3cf6d8f7733347e640b34c31ad
barneyliu708 <barneyliu708@gmail.com>
2022-01-26 17:31:21 -0800

    ALGORITHM-141 LC141. Linked List Cycle - hash table
    hash table - approach 1: https://leetcode.com/problems/linked-list-cycle/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC141LinkedListCycle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC141LinkedListCycle
+    {
+        public bool HasCycle(ListNode head)
+        {
+
+            HashSet<ListNode> hashset = new HashSet<ListNode>();
+
+            while (head != null)
+            {
+                if (hashset.Contains(head))
+                {
+                    return true;
+                }
+                hashset.Add(head);
+                head = head.next;
+            }
+
+            return false;
+        }
+    }
+}
