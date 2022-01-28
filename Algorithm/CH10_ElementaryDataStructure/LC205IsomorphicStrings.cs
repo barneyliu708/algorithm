df3b08cb46e86325b10767ff880e8cf9067207b6
barneyliu708 <barneyliu708@gmail.com>
2022-01-28 14:21:39 -0800

    ALGORITHM-149 LC205. Isomorphic Strings - hash table
    hash table - approach 1: https://leetcode.com/problems/isomorphic-strings/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC205IsomorphicStrings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC205IsomorphicStrings
+    {
+        public bool IsIsomorphic(string s, string t)
+        {
+
+            if (s.Length != t.Length)
+            {
+                return false;
+            }
+
+            Dictionary<char, char> mapstot = new Dictionary<char, char>();
+            Dictionary<char, char> mapttos = new Dictionary<char, char>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if ((mapstot.ContainsKey(s[i]) && mapstot[s[i]] != t[i]) ||
+                    (mapttos.ContainsKey(t[i]) && mapttos[t[i]] != s[i]))
+                {
+                    return false;
+                }
+
+                mapstot[s[i]] = t[i];
+                mapttos[t[i]] = s[i];
+            }
+
+            return true;
+        }
+    }
+}
