f811261013832ce62a4b8cd010a084552c62e8c3
barneyliu708 <barneyliu708@gmail.com>
2022-01-27 22:44:47 -0800

    ALGORITHM-147 LC187. Repeated DNA Sequences - hash table
    hash table - approach 1: https://leetcode.com/problems/repeated-dna-sequences/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC187RepeatedDNASequences.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC187RepeatedDNASequences
+    {
+        public IList<string> FindRepeatedDnaSequences(string s)
+        {
+
+            HashSet<string> hashset = new HashSet<string>();
+            HashSet<string> ans = new HashSet<string>();
+            for (int i = 0; i < s.Length - 9; i++)
+            {
+                string substr = s.Substring(i, 10);
+                if (hashset.Contains(substr))
+                {
+                    ans.Add(substr);
+                }
+                else
+                {
+                    hashset.Add(substr);
+                }
+            }
+            return ans.ToList();
+        }
+    }
+}
