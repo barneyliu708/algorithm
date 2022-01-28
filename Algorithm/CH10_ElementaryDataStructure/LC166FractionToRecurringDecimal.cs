a37e861904d592a7ad0e3223947e379646acef44
barneyliu708 <barneyliu708@gmail.com>
2022-01-27 22:30:07 -0800

    ALGORITHM-146 LC166. Fraction to Recurring Decimal - hash table
    hash table - approach 1: https://leetcode.com/problems/fraction-to-recurring-decimal/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC166FractionToRecurringDecimal.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC166FractionToRecurringDecimal
+    {
+        public string FractionToDecimal(int numerator, int denominator)
+        {
+
+            if (numerator == 0)
+            {
+                return "0";
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            if (numerator < 0 ^ denominator < 0)
+            {
+                sb.Append("-");
+            }
+            long dividend = Math.Abs((long)numerator);
+            long dividor = Math.Abs((long)denominator);
+            sb.Append((dividend / dividor).ToString());
+
+            long remainder = dividend % dividor;
+            if (remainder == 0)
+            {
+                return sb.ToString();
+            }
+
+            sb.Append(".");
+            Dictionary<long, int> map = new Dictionary<long, int>();
+            while (remainder != 0)
+            {
+                if (map.ContainsKey(remainder))
+                {
+                    sb.Insert(map[remainder], "(");
+                    sb.Append(")");
+                    break;
+                }
+
+                map[remainder] = sb.Length;
+                remainder *= 10;
+                sb.Append((remainder / dividor).ToString());
+                remainder = remainder % dividor;
+            }
+
+            return sb.ToString();
+        }
+    }
+}
