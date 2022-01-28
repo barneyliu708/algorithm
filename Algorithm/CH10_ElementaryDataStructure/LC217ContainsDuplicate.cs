c04ce6a19b7e4cfe7bc8e403347a885fcdc0db59
barneyliu708 <barneyliu708@gmail.com>
2022-01-28 14:47:03 -0800

    ALGORITHM-150 LC217. Contains Duplicate - hash table
    hash table - approach 3: https://leetcode.com/problems/contains-duplicate/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC217ContainsDuplicate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC217ContainsDuplicate
+    {
+        public bool ContainsDuplicate(int[] nums)
+        {
+            HashSet<int> hashset = new HashSet<int>();
+
+            foreach (int num in nums)
+            {
+                if (hashset.Contains(num))
+                {
+                    return true;
+                }
+                hashset.Add(num);
+            }
+
+            return false;
+        }
+    }
+}
