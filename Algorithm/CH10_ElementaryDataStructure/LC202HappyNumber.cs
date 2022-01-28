d00424012dd8a484430543893a77869b8c8d565e
barneyliu708 <barneyliu708@gmail.com>
2022-01-27 23:00:30 -0800

    ALGORITHM-148 LC202. Happy Number - hash table
    hash table - approach 1: https://leetcode.com/problems/happy-number/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC202HappyNumber.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC202HappyNumber
+    {
+        public bool IsHappy(int n)
+        {
+
+            if (n < 0)
+            {
+                return false;
+            }
+
+            return IsHappy(n, new HashSet<int>());
+        }
+
+        private bool IsHappy(int n, HashSet<int> hashset)
+        {
+
+            int sum = 0;
+            while (n != 0)
+            {
+                int remainder = n % 10;
+                sum += remainder * remainder;
+                n = n / 10;
+            }
+
+            if (sum == 1)
+            {
+                return true;
+            }
+
+            if (hashset.Contains(sum))
+            {
+                return false;
+            }
+
+            hashset.Add(sum);
+
+            return IsHappy(sum, hashset);
+        }
+    }
+}
