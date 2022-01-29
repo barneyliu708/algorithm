22736660ba4571b79e1baefa8046e1deadbb7224
barneyliu708 <barneyliu708@gmail.com>
2022-01-28 20:28:10 -0800

    ALGORITHM-152 LC229. Majority Element II - hash table
    hash table: https://leetcode.com/problems/majority-element-ii/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC229MajorityElementII.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC229MajorityElementII
+    {
+        public IList<int> MajorityElement(int[] nums)
+        {
+
+            int floor = nums.Length / 3;
+            Dictionary<int, int> map = new Dictionary<int, int>();
+            HashSet<int> ans = new HashSet<int>();
+
+            foreach (int num in nums)
+            {
+                if (!map.ContainsKey(num))
+                {
+                    map[num] = 0;
+                }
+                map[num]++;
+
+                if (map[num] > floor)
+                {
+                    ans.Add(num);
+                }
+            }
+
+            return ans.ToList();
+        }
+    }
+}
