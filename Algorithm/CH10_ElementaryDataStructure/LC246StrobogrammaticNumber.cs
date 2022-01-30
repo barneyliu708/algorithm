7763269056b17e3198d9a2fc2f9975bf101bbfb2
barneyliu708 <barneyliu708@gmail.com>
2022-01-29 21:27:11 -0800

    ALGORITHM-156 LC246. Strobogrammatic Number - hash table
    hash table: https://leetcode.com/problems/strobogrammatic-number/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC246StrobogrammaticNumber.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC246StrobogrammaticNumber
+    {
+        public bool IsStrobogrammatic(string num)
+        {
+
+            Dictionary<char, char> map = new Dictionary<char, char>() {
+                { '0', '0' },
+                { '1', '1' },
+                { '6', '9' },
+                { '8', '8' },
+                { '9', '6' }
+            };
+
+            for (int i = 0; i <= num.Length / 2; i++)
+            {
+                if (!map.ContainsKey(num[i]))
+                {
+                    return false;
+                }
+
+                if (map[num[i]] != num[num.Length - i - 1])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
