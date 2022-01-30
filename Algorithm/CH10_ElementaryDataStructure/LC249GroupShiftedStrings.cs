d0813005e7159720d5c89f9244c05e6c704d4998
barneyliu708 <barneyliu708@gmail.com>
2022-01-29 21:51:29 -0800

    ALGORITHM-157 LC249. Group Shifted Strings - hash table
    hash table: https://leetcode.com/problems/group-shifted-strings/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC249GroupShiftedStrings.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC249GroupShiftedStrings
+    {
+        public IList<IList<string>> GroupStrings(string[] strings)
+        {
+
+            Dictionary<string, IList<string>> map = new Dictionary<string, IList<string>>();
+
+            foreach (string str in strings)
+            {
+                string hashstr = HashStr(str);
+                if (!map.ContainsKey(hashstr))
+                {
+                    map[hashstr] = new List<string>();
+                }
+                map[hashstr].Add(str);
+            }
+
+            return map.Values.ToList();
+        }
+
+        private string HashStr(string str)
+        {
+
+            int shift = str[0] - 'a';
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < str.Length; i++)
+            {
+                char ch = (char)((str[i] - shift + 26) % 26 + 'a');
+
+                sb.Append(ch);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
