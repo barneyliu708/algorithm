e891b9042ef257efcd9ecd39e8c96a4c7702b7a8
barneyliu708 <barneyliu708@gmail.com>
2022-01-30 19:02:35 -0800

    ALGORITHM-160 LC288. Unique Word Abbreviation - Hash table
    hash table: https://www.cnblogs.com/grandyang/p/5220589.html

---
+++ Algorithm/CH10_ElementaryDataStructure/LC288UniqueWordAbbreviation.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC288UniqueWordAbbreviation
+    {
+        public class ValidWordAbbr
+        {
+
+            private Dictionary<string, HashSet<string>> map;
+
+            public ValidWordAbbr(string[] dictionary)
+            {
+                map = new Dictionary<string, HashSet<string>>();
+
+                foreach (string word in dictionary)
+                {
+                    string abbreviation = Abbreviate(word);
+                    if (!map.ContainsKey(abbreviation))
+                    {
+                        map[abbreviation] = new HashSet<string>();
+                    }
+                    map[abbreviation].Add(word);
+                }
+
+            }
+
+            public bool IsUnique(string word)
+            {
+                string abbreviation = Abbreviate(word);
+                if (!map.ContainsKey(abbreviation))
+                {
+                    return true;
+                }
+                return map[abbreviation].Count == 1 && map[abbreviation].Contains(word);
+            }
+
+            private string Abbreviate(string word)
+            {
+                if (word.Length <= 2)
+                {
+                    return word;
+                }
+
+                StringBuilder sb = new StringBuilder();
+                sb.Append(word[0]);
+                sb.Append((word.Length - 2).ToString());
+                sb.Append(word[word.Length - 1]);
+
+                return sb.ToString();
+            }
+        }
+
+    }
+}
