db8b89adabdc82afadb5b47101ae79976293ba79
barneyliu708 <barneyliu708@gmail.com>
2022-01-30 18:47:18 -0800

    ALGORITHM-159 LC268. Missing Number - Gauss Sum
    Gauss Sum - approach 4: https://leetcode.com/problems/missing-number/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC268MissingNumber.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC268MissingNumber
+    {
+        public int MissingNumber(int[] nums)
+        {
+
+            int n = nums.Length;
+            int sum = 0;
+            foreach (int num in nums)
+            {
+                sum += num;
+            }
+
+            return (0 + n) * (n + 1) / 2 - sum;
+        }
+    }
+}
