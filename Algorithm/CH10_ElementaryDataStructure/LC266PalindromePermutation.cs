55e7027cdeb9fabc5a21a636e76f55ec38edfc58
barneyliu708 <barneyliu708@gmail.com>
2022-01-30 18:23:05 -0800

    ALGORITHM-158 LC266. Palindrome Permutation - hash table
    hash table - approach 2: https://leetcode.com/problems/palindrome-permutation/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC266PalindromePermutation.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC266PalindromePermutation
+    {
+        public bool CanPermutePalindrome(string s)
+        {
+
+            Dictionary<char, int> map = new Dictionary<char, int>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (!map.ContainsKey(s[i]))
+                {
+                    map[s[i]] = 0;
+                }
+                map[s[i]]++;
+            }
+
+            int oddCount = 0;
+            foreach (char key in map.Keys)
+            {
+                oddCount += map[key] % 2;
+            }
+
+            return oddCount <= 1;
+        }
+    }
+}
