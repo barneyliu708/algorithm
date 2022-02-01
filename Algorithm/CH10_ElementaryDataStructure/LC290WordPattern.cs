e060641a8c1527632889f5fd18c9b3f88475fdcb
barneyliu708 <barneyliu708@gmail.com>
2022-01-31 21:29:08 -0800

    ALGORITHM-162 LC290. Word Pattern - hash table
    hash table - approach 1: https://leetcode.com/problems/word-pattern/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC290WordPattern.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC290WordPattern
+    {
+        public bool WordPattern(string pattern, string s)
+        {
+
+            string[] sl = s.Split(' ');
+
+            if (pattern.Length != sl.Length)
+            {
+                return false;
+            }
+
+            Dictionary<char, string> mapptos = new Dictionary<char, string>();
+            Dictionary<string, char> mapstop = new Dictionary<string, char>();
+
+            for (int i = 0; i < pattern.Length; i++)
+            {
+                if (mapptos.ContainsKey(pattern[i]) && mapptos[pattern[i]] != sl[i])
+                {
+                    return false;
+                }
+
+                if (mapstop.ContainsKey(sl[i]) && mapstop[sl[i]] != pattern[i])
+                {
+                    return false;
+                }
+
+                mapptos[pattern[i]] = sl[i];
+                mapstop[sl[i]] = pattern[i];
+            }
+
+            return true;
+        }
+    }
+}
