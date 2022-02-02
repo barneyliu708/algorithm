7f7e4486c8daf83d38e29db24ab2548edd0e81b5
barneyliu708 <barneyliu708@gmail.com>
2022-02-01 17:26:56 -0800

    ALGORITHM-22 LC190 Reverse Bits - bit manipulation
    bit manipulation - approach 1: https://leetcode.com/problems/reverse-bits/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC190ReverseBits.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC190ReverseBits
+    {
+        public uint reverseBits(uint n)
+        {
+
+            uint ans = 0;
+            int power = 31;
+
+            while (n != 0)
+            {
+                ans += (n & 1) << power;
+                n = n >> 1;
+                power--;
+            }
+
+            return ans;
+        }
+    }
+}
