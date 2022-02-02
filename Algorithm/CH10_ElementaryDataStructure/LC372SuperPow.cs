eebd2259611c0f789c21df6cdd5b56b1f69388a2
barneyliu708 <barneyliu708@gmail.com>
2022-02-01 19:36:01 -0800

    ALGORITHM-30 LC372 Super Pow - divide and conquer
    divide and conquer: https://www.cnblogs.com/grandyang/p/5651982.html

---
+++ Algorithm/CH10_ElementaryDataStructure/LC372SuperPow.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC372SuperPow
+    {
+        public int SuperPow(int a, int[] b)
+        {
+
+            int ans = 1;
+            for (int i = 0; i < b.Length; i++)
+            {
+                ans = Pow(ans, 10) * Pow(a, b[i]) % 1337;
+            }
+
+            return ans;
+        }
+
+        private int Pow(int x, int n)
+        {
+
+            if (n == 0)
+            {
+                return 1;
+            }
+
+            if (n == 1)
+            {
+                return x % 1337;
+            }
+
+            return Pow(x % 1337, n / 2) * Pow(x % 1337, n - n / 2) % 1337;
+        }
+    }
+}
