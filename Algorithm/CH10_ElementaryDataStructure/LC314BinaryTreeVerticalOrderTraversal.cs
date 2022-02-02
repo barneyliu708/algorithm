dff2a7cb3d5a6b99a0afcb3c04a7cebb94cf65fe
barneyliu708 <barneyliu708@gmail.com>
2022-02-01 20:27:02 -0800

    ALGORITHM-166 LC314. Binary Tree Vertical Order Traversal - hash table + bft
    bft / dft + hash table - approach 2: https://leetcode.com/problems/binary-tree-vertical-order-traversal/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC314BinaryTreeVerticalOrderTraversal.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC314BinaryTreeVerticalOrderTraversal
+    {
+
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        public IList<IList<int>> VerticalOrder(TreeNode root)
+        {
+
+            if (root == null)
+            {
+                return new List<IList<int>>();
+            }
+
+            Dictionary<int, List<int>> map = new Dictionary<int, List<int>>();
+
+            Queue<KeyValuePair<TreeNode, int>> queue = new Queue<KeyValuePair<TreeNode, int>>();
+            queue.Enqueue(new KeyValuePair<TreeNode, int>(root, 0));
+
+            while (queue.Count != 0)
+            {
+
+                TreeNode curNode = queue.Peek().Key;
+                int curCol = queue.Peek().Value;
+
+                queue.Dequeue();
+
+                if (!map.ContainsKey(curCol))
+                {
+                    map[curCol] = new List<int>();
+                }
+                map[curCol].Add(curNode.val);
+
+                if (curNode.left != null)
+                {
+                    queue.Enqueue(new KeyValuePair<TreeNode, int>(curNode.left, curCol - 1));
+                }
+
+                if (curNode.right != null)
+                {
+                    queue.Enqueue(new KeyValuePair<TreeNode, int>(curNode.right, curCol + 1));
+                }
+            }
+
+            IList<IList<int>> ans = new List<IList<int>>();
+            List<int> colList = map.Keys.ToList();
+            colList.Sort();
+            foreach (int col in colList)
+            {
+                ans.Add(map[col]);
+            }
+
+            return ans;
+        }
+    }
+}
