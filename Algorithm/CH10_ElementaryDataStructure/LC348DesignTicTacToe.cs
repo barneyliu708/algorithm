3e0487af804e3b10f3cdb1e8e323c88aad17d8e6
barneyliu708 <barneyliu708@gmail.com>
2022-02-02 21:41:23 -0800

    ALGORITHM-169 LC348. Design Tic-Tac-Toe - hash table
    hash table - approach 2: https://leetcode.com/problems/design-tic-tac-toe/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC348DesignTicTacToe.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC348DesignTicTacToe
+    {
+        public class TicTacToe
+        {
+
+            int[] rows;
+            int[] cols;
+            int diagonal;
+            int antidiagonal;
+
+            int size;
+
+            public TicTacToe(int n)
+            {
+                size = n;
+                rows = new int[n];
+                cols = new int[n];
+                diagonal = 0;
+                antidiagonal = 0;
+            }
+
+            public int Move(int row, int col, int player)
+            {
+
+                int curplayer = player == 1 ? 1 : -1;
+
+                rows[row] += curplayer;
+                cols[col] += curplayer;
+                if (row == col)
+                {
+                    diagonal += curplayer;
+                }
+                if (row + col == size - 1)
+                {
+                    antidiagonal += curplayer;
+                }
+
+                if (Math.Abs(rows[row]) == size ||
+                    Math.Abs(cols[col]) == size ||
+                    Math.Abs(diagonal) == size ||
+                    Math.Abs(antidiagonal) == size)
+                {
+                    return player;
+                }
+
+                return 0;
+            }
+        }
+    }
+}
