ad2421a6fb3b2c45f96d89220a01828d2d4a002a
barneyliu708 <barneyliu708@gmail.com>
2022-02-02 20:41:17 -0800

    ALGORITHM-167 LC325. Maximum Size Subarray Sum Equals k - prefix sum + hash table
    hash table - approach 1: https://leetcode.com/problems/maximum-size-subarray-sum-equals-k/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC325MaximumSizeSubarraySumEqualsK.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC325MaximumSizeSubarraySumEqualsK
+    {
+        public int MaxSubArrayLen(int[] nums, int k)
+        {
+
+            int[] prefix = new int[nums.Length + 1];
+            prefix[0] = 0;
+            for (int i = 1; i < prefix.Length; i++)
+            {
+                prefix[i] = prefix[i - 1] + nums[i - 1];
+            }
+
+            int ans = 0;
+            Dictionary<int, int> map = new Dictionary<int, int>();
+            for (int i = 0; i < prefix.Length; i++)
+            {
+                if (map.ContainsKey(prefix[i] - k))
+                {
+                    ans = Math.Max(ans, i - map[prefix[i] - k]);
+                }
+                // only add kv pair when it is not exist to get the longest result
+                if (!map.ContainsKey(prefix[i]))
+                {
+                    map[prefix[i]] = i;
+                }
+            }
+
+            return ans;
+        }
+    }
+}
