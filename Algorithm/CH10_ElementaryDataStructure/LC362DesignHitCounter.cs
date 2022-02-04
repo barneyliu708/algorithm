9b03e6a4942b799ece4e8c02a456a2afa65355ae
barneyliu708 <barneyliu708@gmail.com>
2022-02-03 21:37:11 -0800

    ALGORITHM-173 LC362. Design Hit Counter - queue
    queue - approach 1: https://leetcode.com/problems/design-hit-counter/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC362DesignHitCounter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC362DesignHitCounter
+    {
+        public class HitCounter
+        {
+
+            private Queue<int> hits;
+
+            public HitCounter()
+            {
+                hits = new Queue<int>();
+            }
+
+            public void Hit(int timestamp)
+            {
+                hits.Enqueue(timestamp);
+            }
+
+            public int GetHits(int timestamp)
+            {
+                while (hits.Count > 0 && timestamp - hits.Peek() >= 300)
+                {
+                    hits.Dequeue();
+                }
+
+                return hits.Count;
+            }
+        }
+    }
+}
