3778032f778e9c479c289e4618678e1a88446e8e
barneyliu708 <barneyliu708@gmail.com>
2022-02-03 20:00:23 -0800

    ALGORITHM-171 LC350. Intersection of Two Arrays II - hash table
    hash table - approach 1: https://leetcode.com/problems/intersection-of-two-arrays-ii/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC350IntersectionOfTwoArraysII.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC350IntersectionOfTwoArraysII
+    {
+        public int[] Intersect(int[] nums1, int[] nums2)
+        {
+            if (nums1.Length > nums2.Length)
+            {
+                return Intersect(nums2, nums1);
+            }
+
+            Dictionary<int, int> map1 = new Dictionary<int, int>();
+            foreach (int num in nums1)
+            {
+                if (!map1.ContainsKey(num))
+                {
+                    map1[num] = 0;
+                }
+                map1[num]++;
+            }
+
+            IList<int> ans = new List<int>();
+            foreach (int num in nums2)
+            {
+                if (map1.ContainsKey(num) && map1[num] > 0)
+                {
+                    ans.Add(num);
+                    map1[num]--;
+                }
+            }
+
+            return ans.ToArray();
+        }
+    }
+}
