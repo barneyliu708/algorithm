3451e761523ae716128107784cfaccb0582cad9f
barneyliu708 <barneyliu708@gmail.com>
2022-02-03 21:27:48 -0800

    ALGORITHM-172 LC356. Line Reflection - hash table
    hash table - approach 1: https://www.cnblogs.com/grandyang/p/5579271.html

---
+++ Algorithm/CH10_ElementaryDataStructure/LC356LineReflection.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC356LineReflection
+    {
+        public bool IsReflected(int[][] points)
+        {
+
+            Dictionary<int, HashSet<int>> map = new Dictionary<int, HashSet<int>>(); // x - y[];
+            int xmax = points[0][0];
+            int xmin = points[0][0];
+            foreach (int[] point in points)
+            {
+                int x = point[0];
+                int y = point[1];
+
+                xmax = Math.Max(xmax, x);
+                xmin = Math.Min(xmin, x);
+
+                if (!map.ContainsKey(x))
+                {
+                    map[x] = new HashSet<int>();
+                }
+                map[x].Add(y);
+            }
+
+            foreach (int[] point in points)
+            {
+                int x = point[0];
+                int x_ = xmax + xmin - x;
+                int y = point[1];
+
+                if (!map.ContainsKey(x_) || !map[x_].Contains(y))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
