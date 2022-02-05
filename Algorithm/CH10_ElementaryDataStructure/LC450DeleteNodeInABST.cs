876216a73dc04026f1e9006d3b45f18319ccc180
barneyliu708 <barneyliu708@gmail.com>
2022-02-04 17:54:00 -0800

    ALGORITHM-174 LC450. Delete Node in a BST - binary search tree + dft
    binary search tree: https://leetcode.com/problems/delete-node-in-a-bst/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC450DeleteNodeInABST.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC450DeleteNodeInABST
+    {
+        public TreeNode Successor(TreeNode root)
+        {
+            if (root.right == null)
+            {
+                return null;
+            }
+            TreeNode cur = root.right;
+
+            while (cur.left != null)
+            {
+                cur = cur.left;
+            }
+
+            return cur;
+        }
+
+        public TreeNode Predecessor(TreeNode root)
+        {
+            if (root.left == null)
+            {
+                return null;
+            }
+            TreeNode cur = root.left;
+
+            while (cur.right != null)
+            {
+                cur = cur.right;
+            }
+
+            return cur;
+        }
+
+        public TreeNode DeleteNode(TreeNode root, int key)
+        {
+
+            if (root == null)
+            {
+                return null;
+            }
+
+            if (key > root.val)
+            {
+                root.right = DeleteNode(root.right, key);
+            }
+            else if (key < root.val)
+            {
+                root.left = DeleteNode(root.left, key);
+            }
+            else
+            {
+                if (root.left == null && root.right == null)
+                {
+                    return null;
+                }
+                else if (root.right != null)
+                {
+                    TreeNode successor = Successor(root);
+                    root.val = successor.val;
+                    root.right = DeleteNode(root.right, successor.val);
+                }
+                else
+                {
+                    TreeNode predecessor = Predecessor(root);
+                    root.val = predecessor.val;
+                    root.left = DeleteNode(root.left, predecessor.val);
+                }
+            }
+
+            return root;
+        }
+    }
+}
