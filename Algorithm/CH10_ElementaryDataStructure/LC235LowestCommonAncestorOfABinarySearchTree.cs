fb278c1a16850d95dd5a11b1bc04fa13176ee79e
barneyliu708 <barneyliu708@gmail.com>
2022-02-05 20:35:42 -0800

    ALGORITHM-179 LC235. Lowest Common Ancestor of a Binary Search Tree - recursion
    recursion - approach 1: https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC235LowestCommonAncestorOfABinarySearchTree.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC235LowestCommonAncestorOfABinarySearchTree
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+        {
+
+            if (p.val >= q.val)
+            {
+                return LowestCommonAncestor(root, q, p);
+            }
+            // where p.val <= q.val
+
+            // root.val < p.val <= q.val
+            if (root.val < p.val)
+            {
+                return LowestCommonAncestor(root.right, p, q);
+            }
+
+            // p.val <= q.val < root.val
+            if (q.val < root.val)
+            {
+                return LowestCommonAncestor(root.left, p, q);
+            }
+
+            // root.val = p.val <= q.val || p.val <= q.val = root.val || p.val <= root.val <= q.val
+            return root;
+        }
+    }
+}
