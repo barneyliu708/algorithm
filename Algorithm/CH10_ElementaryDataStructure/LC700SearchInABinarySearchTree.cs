5fac0d14a510fb49a32eb35f8d65534e71eb4efd
barneyliu708 <barneyliu708@gmail.com>
2022-02-05 20:45:21 -0800

    https://leetcode.com/problems/search-in-a-binary-search-tree/solution/ - iteration
    iteration - approach 2: https://leetcode.com/problems/search-in-a-binary-search-tree/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC700SearchInABinarySearchTree.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC700SearchInABinarySearchTree
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        public TreeNode SearchBST(TreeNode root, int val)
+        {
+
+            TreeNode cur = root;
+            while (cur != null)
+            {
+                if (val > cur.val)
+                {
+                    cur = cur.right;
+                }
+                else if (val < cur.val)
+                {
+                    cur = cur.left;
+                }
+                else
+                {
+                    return cur;
+                }
+            }
+            return null;
+        }
+    }
+}
