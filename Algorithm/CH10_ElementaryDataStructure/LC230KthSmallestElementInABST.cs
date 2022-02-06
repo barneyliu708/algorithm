8cc720e5071fdbba7f6fd57f5d58959deae3db7d
barneyliu708 <barneyliu708@gmail.com>
2022-02-05 19:52:41 -0800

    ALGORITHM-177 LC230. Kth Smallest Element in a BST - bst + iterative dft
    bst + iterative dft - approach 2: https://leetcode.com/problems/kth-smallest-element-in-a-bst/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC230KthSmallestElementInABST.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC230KthSmallestElementInABST
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        public int KthSmallest(TreeNode root, int k)
+        {
+
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+
+            stack.Push(root);
+            TreeNode cur = root;
+
+            while (cur != null || stack.Count != 0)
+            {
+
+                // navigatet to the leftmost child of the current node
+                while (cur != null)
+                {
+                    stack.Push(cur);
+                    cur = cur.left;
+                }
+                cur = stack.Pop();
+                if (--k == 0)
+                {
+                    return cur.val;
+                }
+                cur = cur.right;
+            }
+
+            throw new Exception("Invalid k!");
+        }
+    }
+}
