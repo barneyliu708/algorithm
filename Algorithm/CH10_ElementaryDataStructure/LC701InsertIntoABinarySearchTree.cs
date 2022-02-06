8ff11e0ab05cf321730a9439afbc9e734799a7b7
barneyliu708 <barneyliu708@gmail.com>
2022-02-05 20:08:36 -0800

    ALGORITHM-178 LC701. Insert into a Binary Search Tree - bst + recursion
    recursion: https://leetcode.com/problems/insert-into-a-binary-search-tree/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC701InsertIntoABinarySearchTree.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC701InsertIntoABinarySearchTree
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        public TreeNode InsertIntoBST(TreeNode root, int val)
+        {
+
+            if (root == null)
+            {
+                return new TreeNode(val);
+            }
+
+            if (val > root.val)
+            {
+                root.right = InsertIntoBST(root.right, val);
+            }
+            else
+            {
+                root.left = InsertIntoBST(root.left, val);
+            }
+
+            return root;
+        }
+    }
+}
