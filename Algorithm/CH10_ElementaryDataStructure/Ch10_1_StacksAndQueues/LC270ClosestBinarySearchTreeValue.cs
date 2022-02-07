34fd611105cf732563d6cb5e4e521cf2b344e49d
barneyliu708 <barneyliu708@gmail.com>
2022-02-06 21:30:22 -0800

    ALGORITHM-182 LC270. Closest Binary Search Tree Value - iterative dft
    iterative dft: https://leetcode.com/problems/closest-binary-search-tree-value/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/Ch10_1_StacksAndQueues/LC270ClosestBinarySearchTreeValue.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure.Ch10_1_StacksAndQueues
+{
+    class LC270ClosestBinarySearchTreeValue
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+        public int ClosestValue(TreeNode root, double target)
+        {
+
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+
+            TreeNode cur = root;
+            int ans = root.val;
+            while (cur != null || stack.Count != 0)
+            {
+                while (cur != null)
+                {
+                    stack.Push(cur);
+                    cur = cur.left;
+                }
+                cur = stack.Pop();
+
+                // do the comparision
+                if (Math.Abs(ans - target) > Math.Abs(cur.val - target))
+                {
+                    ans = cur.val;
+                }
+
+                // navigate to the next
+                cur = cur.right;
+            }
+
+            return ans;
+        }
+    }
+}
