6f7bf0bf278fc346fae7ac62a7093425ffee72ae
barneyliu708 <barneyliu708@gmail.com>
2022-02-06 22:32:46 -0800

    ALGORITHM-184 LC510. Inorder Successor in BST II - Iteration
    Iteration: https://leetcode.com/problems/inorder-successor-in-bst-ii/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC510InorderSuccessorInBSTII.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC510InorderSuccessorInBSTII
+    {
+        public Node InorderSuccessor(Node x)
+        {
+
+            if (x.right != null)
+            { // the successor is the leftmost node of x's right subtree
+                Node cur = x.right;
+                while (cur.left != null)
+                {
+                    cur = cur.left;
+                }
+                return cur;
+            }
+
+            while (x.parent != null && x.parent.right == x)
+            {
+                x = x.parent;
+            }
+
+            return x.parent;
+        }
+    }
+}
