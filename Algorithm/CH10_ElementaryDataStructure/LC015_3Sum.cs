f166b5083bc507897fa2f2054507d26b7111b654
barneyliu708 <barneyliu708@gmail.com>
2022-02-07 20:48:50 -0800

    ALGORITHM-185 LC015. 3Sum - sorting
    sorting - approach 2: https://leetcode.com/problems/3sum/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC015_3Sum.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC015_3Sum
+    {
+        public IList<IList<int>> ThreeSum(int[] nums)
+        {
+            Array.Sort(nums);
+            List<IList<int>> ans = new List<IList<int>>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (i == 0 || nums[i] != nums[i - 1])
+                {
+                    TwoSum(nums, i, ans);
+                }
+            }
+
+            return ans;
+        }
+
+        private void TwoSum(int[] nums, int i, List<IList<int>> ans)
+        {
+            HashSet<int> hashset = new HashSet<int>();
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                int complement = -nums[i] - nums[j];
+                if (hashset.Contains(complement))
+                {
+                    ans.Add(new List<int>() { nums[i], nums[j], complement });
+                    while (j + 1 < nums.Length && nums[j] == nums[j + 1])
+                    {
+                        j++;
+                    }
+                }
+                hashset.Add(nums[j]);
+            }
+        }
+    }
+}
