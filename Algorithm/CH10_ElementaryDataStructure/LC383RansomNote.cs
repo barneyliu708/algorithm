1e5a8c677ff29d971c8b0e00263d31d52bd456cd
barneyliu708 <barneyliu708@gmail.com>
2022-02-07 22:20:10 -0800

    ALGORITHM-187 LC383. Ransom Note - hash table
    hash table - approach 3: https://leetcode.com/problems/ransom-note/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC383RansomNote.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC383RansomNote
+    {
+        public bool CanConstruct(string ransomNote, string magazine)
+        {
+
+            Dictionary<char, int> countMap = new Dictionary<char, int>();
+            for (int i = 0; i < magazine.Length; i++)
+            {
+                if (!countMap.ContainsKey(magazine[i]))
+                {
+                    countMap[magazine[i]] = 0;
+                }
+                countMap[magazine[i]]++;
+            }
+
+            for (int i = 0; i < ransomNote.Length; i++)
+            {
+                char ch = ransomNote[i];
+                if (!countMap.ContainsKey(ch))
+                {
+                    return false;
+                }
+                if (countMap[ch] <= 0)
+                {
+                    return false;
+                }
+                countMap[ch]--;
+            }
+
+            return true;
+        }
+    }
+}
