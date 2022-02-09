7643a56818915d2f9d4d9a16546ddcf165f1a38f
barneyliu708 <barneyliu708@gmail.com>
2022-02-08 20:51:55 -0800

    ALGORITHM-189 LC034. Find First and Last Position of Element in Sorted Array - Binary search
    binary search: https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC034FindFirstAndLastPositionOfElementInSortedArray.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC034FindFirstAndLastPositionOfElementInSortedArray
+    {
+        public int[] SearchRange(int[] nums, int target)
+        {
+
+            int[] ans = new int[] { -1, -1 };
+
+            if (nums.Length == 0)
+            {
+                return ans;
+            }
+
+            ans[0] = SearchLowerBoundary(nums, target);
+            ans[1] = SearchUpperBoundary(nums, target);
+
+            return ans;
+        }
+
+        private int SearchLowerBoundary(int[] nums, int target)
+        {
+
+            int start = 0, end = nums.Length - 1;
+            while (start < end)
+            {
+                int mid = (start + end) / 2;
+                if (nums[mid] == target)
+                {
+                    end = mid;
+                }
+                else if (nums[mid] > target)
+                {
+                    end = mid - 1;
+                }
+                else
+                {
+                    start = mid + 1;
+                }
+            }
+
+            if (nums[start] != target)
+            {
+                return -1;
+            }
+
+            return start;
+        }
+
+        private int SearchUpperBoundary(int[] nums, int target)
+        {
+
+            int start = 0, end = nums.Length - 1;
+            while (start < end)
+            {
+                int mid = (start + end + 1) / 2;
+                if (nums[mid] == target)
+                {
+                    start = mid;
+                }
+                else if (nums[mid] > target)
+                {
+                    end = mid - 1;
+                }
+                else
+                {
+                    start = mid + 1;
+                }
+            }
+
+            if (nums[end] != target)
+            {
+                return -1;
+            }
+
+            return end;
+        }
+    }
+}
