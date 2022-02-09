78f9e739756d4ac4b0b3d1c820068c889915bcba
barneyliu708 <barneyliu708@gmail.com>
2022-02-08 21:47:27 -0800

    ALGORITHM-189 LC034. Find First and Last Position of Element in Sorted Array
    the second approach - a more consist way for binary search

--- Algorithm/CH10_ElementaryDataStructure/LC034FindFirstAndLastPositionOfElementInSortedArray.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC034FindFirstAndLastPositionOfElementInSortedArray.cs
@@ -4,80 +4,162 @@ using System.Text;
 
 namespace Algorithm.CH10_ElementaryDataStructure
 {
-    class LC034FindFirstAndLastPositionOfElementInSortedArray
+    public class LC034FindFirstAndLastPositionOfElementInSortedArray
     {
-        public int[] SearchRange(int[] nums, int target)
+              
+        public class Approach1
         {
-
-            int[] ans = new int[] { -1, -1 };
-
-            if (nums.Length == 0)
+            public int[] SearchRange(int[] nums, int target)
             {
+
+                int[] ans = new int[] { -1, -1 };
+
+                if (nums.Length == 0)
+                {
+                    return ans;
+                }
+
+                ans[0] = SearchLowerBoundary(nums, target);
+                ans[1] = SearchUpperBoundary(nums, target);
+
                 return ans;
             }
 
-            ans[0] = SearchLowerBoundary(nums, target);
-            ans[1] = SearchUpperBoundary(nums, target);
+            private int SearchLowerBoundary(int[] nums, int target)
+            {
 
-            return ans;
+                int start = 0, end = nums.Length - 1;
+                while (start < end)
+                {
+                    int mid = (start + end) / 2;
+                    if (nums[mid] == target)
+                    {
+                        end = mid;
+                    }
+                    else if (nums[mid] > target)
+                    {
+                        end = mid - 1;
+                    }
+                    else
+                    {
+                        start = mid + 1;
+                    }
+                }
+
+                if (nums[start] != target)
+                {
+                    return -1;
+                }
+
+                return start;
+            }
+
+            private int SearchUpperBoundary(int[] nums, int target)
+            {
+
+                int start = 0, end = nums.Length - 1;
+                while (start < end)
+                {
+                    int mid = (start + end + 1) / 2;
+                    if (nums[mid] == target)
+                    {
+                        start = mid;
+                    }
+                    else if (nums[mid] > target)
+                    {
+                        end = mid - 1;
+                    }
+                    else
+                    {
+                        start = mid + 1;
+                    }
+                }
+
+                if (nums[end] != target)
+                {
+                    return -1;
+                }
+
+                return end;
+            }
         }
 
-        private int SearchLowerBoundary(int[] nums, int target)
+        public class Approach2 // the second binary search approach: a more consist way - always use "end = mid - 1" and "start = mid + 1"
         {
-
-            int start = 0, end = nums.Length - 1;
-            while (start < end)
+            public int[] SearchRange(int[] nums, int target)
             {
-                int mid = (start + end) / 2;
-                if (nums[mid] == target)
+
+                int[] ans = new int[] { -1, -1 };
+
+                if (nums.Length == 0)
                 {
-                    end = mid;
+                    return ans;
                 }
-                else if (nums[mid] > target)
-                {
-                    end = mid - 1;
-                }
-                else
-                {
-                    start = mid + 1;
-                }
+
+                ans[0] = SearchLowerBoundary(nums, target);
+                ans[1] = SearchUpperBoundary(nums, target);
+
+                return ans;
             }
 
-            if (nums[start] != target)
+            private int SearchLowerBoundary(int[] nums, int target)
             {
-                return -1;
+
+                int start = 0, end = nums.Length - 1;
+                while (start <= end)
+                {
+                    int mid = (start + end) / 2;
+                    if (nums[mid] == target)
+                    {
+                        end = mid - 1;
+                    }
+                    else if (nums[mid] > target)
+                    {
+                        end = mid - 1;
+                    }
+                    else
+                    {
+                        start = mid + 1;
+                    }
+                }
+
+                if (start >= nums.Length || nums[start] != target)
+                {
+                    return -1;
+                }
+
+                return start;
             }
 
-            return start;
+            private int SearchUpperBoundary(int[] nums, int target)
+            {
+
+                int start = 0, end = nums.Length - 1;
+                while (start <= end)
+                {
+                    int mid = (start + end) / 2;
+                    if (nums[mid] == target)
+                    {
+                        start = mid + 1;
+                    }
+                    else if (nums[mid] > target)
+                    {
+                        end = mid - 1;
+                    }
+                    else
+                    {
+                        start = mid + 1;
+                    }
+                }
+
+                if (end < 0 || nums[end] != target)
+                {
+                    return -1;
+                }
+
+                return end;
+            }
         }
 
-        private int SearchUpperBoundary(int[] nums, int target)
-        {
-
-            int start = 0, end = nums.Length - 1;
-            while (start < end)
-            {
-                int mid = (start + end + 1) / 2;
-                if (nums[mid] == target)
-                {
-                    start = mid;
-                }
-                else if (nums[mid] > target)
-                {
-                    end = mid - 1;
-                }
-                else
-                {
-                    start = mid + 1;
-                }
-            }
-
-            if (nums[end] != target)
-            {
-                return -1;
-            }
-
-            return end;
-        }
     }
 }
