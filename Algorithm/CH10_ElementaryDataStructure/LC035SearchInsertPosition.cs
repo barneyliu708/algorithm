3ef32ae643b92e696c74ff53fdfccdd2aee83738
barneyliu708 <barneyliu708@gmail.com>
2022-02-08 21:18:35 -0800

    ALGORITHM-190 LC035. Search Insert Position - binary search
    binary search: https://leetcode.com/problems/search-insert-position/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC035SearchInsertPosition.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC035SearchInsertPosition
+    {
+        public int SearchInsert(int[] nums, int target)
+        {
+            int start = 0, end = nums.Length - 1;
+            while (start <= end)
+            {
+                int mid = (start + end) / 2;
+                if (nums[mid] == target)
+                {
+                    return mid;
+                }
+                else if (nums[mid] > target)
+                {
+                    end = mid - 1;
+                }
+                else
+                {
+                    start = mid + 1;
+                }
+            }
+
+            return start;
+        }
+    }
+}
