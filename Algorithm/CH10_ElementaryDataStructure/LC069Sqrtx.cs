b4fc7780ca618c9e3556c31a295d8a103a4f0b4c
barneyliu708 <barneyliu708@gmail.com>
2022-02-08 21:34:59 -0800

    ALGORITHM-191 LC069. Sqrt(x) - binary search
    binary search: https://leetcode.com/problems/sqrtx/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC069Sqrtx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC069Sqrtx
+    {
+        public int MySqrt(int x)
+        {
+
+            int start = 0, end = x / 2 + 1;
+            while (start <= end)
+            {
+                int mid = start + (end - start) / 2;
+                long sqr = (long)mid * mid;
+                if (sqr == x)
+                {
+                    return mid;
+                }
+                else if (sqr > x)
+                {
+                    end = mid - 1;
+                }
+                else
+                {
+                    start = mid + 1;
+                }
+            }
+
+            return end;
+        }
+    }
+}
