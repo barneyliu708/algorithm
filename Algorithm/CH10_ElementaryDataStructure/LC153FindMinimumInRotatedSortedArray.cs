25b04ea8adf05446aa32ba6ceb42b67c22e5781e
barneyliu708 <barneyliu708@gmail.com>
2022-02-09 21:04:23 -0800

    ALGORITHM-194 LC153. Find Minimum in Rotated Sorted Array
    https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC153FindMinimumInRotatedSortedArray.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC153FindMinimumInRotatedSortedArray
+    {
+        public int FindMin(int[] nums)
+        {
+
+            int start = 0;
+            int end = nums.Length - 1;
+
+            while (start <= end)
+            {
+                int mid = start + (end - start) / 2;
+
+                if (nums[mid] > nums[(mid + 1) % nums.Length])
+                {
+                    return nums[(mid + 1) % nums.Length];
+                }
+                else if (nums[mid] < nums[start])
+                {
+                    end = mid - 1;
+                }
+                else
+                {
+                    start = mid + 1;
+                }
+            }
+
+            return nums[start % nums.Length];
+        }
+    }
+}
