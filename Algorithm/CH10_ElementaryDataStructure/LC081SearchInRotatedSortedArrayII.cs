605e4ef9fff6b8a755c3d266c0fd45b5a15167c8
barneyliu708 <barneyliu708@gmail.com>
2022-02-09 20:27:43 -0800

    ALGORITHM-193 LC081. Search in Rotated Sorted Array II
    https://leetcode.com/problems/search-in-rotated-sorted-array-ii/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC081SearchInRotatedSortedArrayII.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC081SearchInRotatedSortedArrayII
+    {
+        public bool Search(int[] nums, int target)
+        {
+
+            int pivot = 0;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] < nums[i - 1])
+                {
+                    pivot = i;
+                }
+            }
+
+            int start = -1;
+            int end = -1;
+            if (target >= nums[pivot] && target <= nums[nums.Length - 1])
+            {
+                start = pivot;
+                end = nums.Length - 1;
+            }
+            else
+            {
+                start = 0;
+                end = pivot - 1;
+            }
+
+            while (start <= end)
+            {
+                int mid = start + (end - start) / 2;
+                if (nums[mid] == target)
+                {
+                    return true;
+                }
+                else if (nums[mid] > target)
+                {
+                    end = mid - 1;
+                }
+                else
+                {
+                    start = mid + 1;
+                }
+            }
+
+            return false;
+        }
+    }
+}
