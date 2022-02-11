82e8a3f8b498fda410b9c0b36ab1718bbe34cad2
barneyliu708 <barneyliu708@gmail.com>
2022-02-10 19:06:26 -0800

    ALGORITHM-196 LC167. Two Sum II - Input Array Is Sorted - 2 points
    2 points: https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC167TwoSumIIInputArrayIsSorted.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC167TwoSumIIInputArrayIsSorted
+    {
+        public int[] TwoSum(int[] numbers, int target)
+        {
+
+            int start = 0;
+            int end = numbers.Length - 1;
+            int[] ans = new int[2];
+            while (start < end)
+            {
+                int sum = numbers[start] + numbers[end];
+
+                if (sum == target)
+                {
+                    return new int[] { start + 1, end + 1 };
+                }
+                else if (sum > target)
+                {
+                    end--;
+                }
+                else
+                {
+                    start++;
+                }
+            }
+
+            return new int[] { -1, -1 };
+        }
+    }
+}
