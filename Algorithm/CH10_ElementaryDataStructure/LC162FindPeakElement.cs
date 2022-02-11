d15b20404f9d5501bc9aa0ac9afd86cc774200e9
barneyliu708 <barneyliu708@gmail.com>
2022-02-10 18:53:49 -0800

    ALGORITHM-195 LC162. Find Peak Element
    binary search: https://leetcode.com/problems/find-peak-element/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC162FindPeakElement.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC162FindPeakElement
+    {
+        public int FindPeakElement(int[] nums)
+        {
+
+            int start = 0;
+            int end = nums.Length - 1;
+
+            while (start < end)
+            {
+                int mid = start + (end - start) / 2;
+                if (nums[mid] > nums[mid + 1])
+                {
+                    end = mid;
+                }
+                else
+                {
+                    start = mid + 1;
+                }
+            }
+
+            return end;
+        }
+    }
+}
