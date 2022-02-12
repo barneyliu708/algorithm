07c3f7e3705610110f87bfdcbfd942fb068ebd24
barneyliu708 <barneyliu708@gmail.com>
2022-02-11 19:39:04 -0800

    ALGORITHM-199 LC278. First Bad Version
    binary search - approach 2: https://leetcode.com/problems/first-bad-version/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC278FirstBadVersion.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC278FirstBadVersion
+    {
+        public bool IsBadVersion (int version)
+        {
+            return true;
+        }
+
+        public int FirstBadVersion(int n)
+        {
+
+            int start = 1;
+            int end = n;
+
+            while (start <= end)
+            {
+                int mid = start + (end - start) / 2;
+                if (IsBadVersion(mid))
+                {
+                    end = mid - 1;
+                }
+                else
+                {
+                    start = mid + 1;
+                }
+            }
+
+            return start;
+        }
+    }
+}
