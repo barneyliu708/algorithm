3470ad5e41559b145425ab283211e7c349b16890
barneyliu708 <barneyliu708@gmail.com>
2022-02-11 20:53:36 -0800

    287. Find the Duplicate Number - hash table
    hash table - approach 3: https://leetcode.com/problems/find-the-duplicate-number/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC287FindTheDuplicateNumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC287FindTheDuplicateNumber
+    {
+        public int FindDuplicate(int[] nums)
+        {
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int index = Math.Abs(nums[i]) - 1;
+                if (nums[index] < 0)
+                {
+                    return index + 1;
+                }
+                nums[index] *= -1;
+            }
+
+            return int.MinValue;
+        }
+    }
+}
