de86799f74a4ed0505b2bb56858c5ad7ff1c6a76
barneyliu708 <barneyliu708@gmail.com>
2022-02-12 15:21:52 -0800

    ALGORITHM-203 LC011. Container With Most Water - 2 pointers
    2 pointers: https://leetcode.com/problems/container-with-most-water/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC011ContainerWithMostWater.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC011ContainerWithMostWater
+    {
+        public int MaxArea(int[] height)
+        {
+
+            int ans = 0;
+            int l = 0;
+            int r = height.Length - 1;
+            while (l < r)
+            {
+                ans = Math.Max(ans, (r - l) * Math.Min(height[l], height[r]));
+                if (height[l] < height[r])
+                {
+                    l++;
+                }
+                else
+                {
+                    r--;
+                }
+            }
+            return ans;
+        }
+    }
+}
