60b6b6066e7cfaaaa0759f0e3ce2ac5a81c663ef
barneyliu708 <barneyliu708@gmail.com>
2022-02-11 19:58:00 -0800

    ALGORITHM-200 LC259. 3Sum Smaller - 2 pointers
    2 pointers - approach 3: https://leetcode.com/problems/3sum-smaller/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC259_3SumSmaller.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC259_3SumSmaller
+    {
+        public int ThreeSumSmaller(int[] nums, int target)
+        {
+
+            Array.Sort(nums);
+            int sum = 0;
+            for (int i = 0; i < nums.Length - 2; i++)
+            {
+                sum += TwoSumSmaller(nums, i + 1, target - nums[i]);
+            }
+            return sum;
+        }
+
+        public int TwoSumSmaller(int[] nums, int start, int target)
+        {
+            int sum = 0;
+            int end = nums.Length - 1;
+            while (start < end)
+            {
+                if (nums[start] + nums[end] < target)
+                {
+                    sum += end - start;
+                    start++;
+                }
+                else
+                {
+                    end--;
+                }
+            }
+            return sum;
+        }
+    }
+}
