93f7eade5f4028e6d5da822048d818575dd9bfca
barneyliu708 <barneyliu708@gmail.com>
2022-02-12 23:50:37 -0800

    ALGORITHM-206 LC019. Remove Nth Node From End of List
    https://leetcode.com/problems/remove-nth-node-from-end-of-list/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC019RemoveNthNodeFromEndOfList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC019RemoveNthNodeFromEndOfList
+    {
+        public ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+
+            ListNode dummy = new ListNode();
+            dummy.next = head;
+            int length = 0;
+            ListNode cur = head;
+            while (cur != null)
+            {
+                length++;
+                cur = cur.next;
+            }
+
+            length = length - n;
+            cur = dummy;
+            while (length > 0)
+            {
+                length--;
+                cur = cur.next;
+            }
+            cur.next = cur.next.next;
+
+            return dummy.next;
+        }
+    }
+}
