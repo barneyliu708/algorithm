76ec50810046393cc99916ae9d2bc4a371289f89
barneyliu708 <barneyliu708@gmail.com>
2022-02-13 19:13:48 -0800

    ALGORITHM-207 LC026. Remove Duplicates from Sorted Array - 2 pointers
    2 pointers - approach 1: https://leetcode.com/problems/remove-duplicates-from-sorted-array/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC026RemoveDuplicatesFromSortedArray.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC026RemoveDuplicatesFromSortedArray
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            if (nums.Length == 0 || nums.Length == 1)
+            {
+                return nums.Length;
+            }
+
+            int pointer = 1;
+            int insert = 1;
+
+            while (pointer < nums.Length)
+            {
+                if (nums[pointer] != nums[pointer - 1])
+                {
+                    nums[insert] = nums[pointer];
+                    insert++;
+                }
+                pointer++;
+            }
+
+            return insert;
+        }
+    }
+}
