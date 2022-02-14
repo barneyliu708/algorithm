af24ce738c990a56d025786a68b2ee1af3322618
barneyliu708 <barneyliu708@gmail.com>
2022-02-13 20:27:22 -0800

    ALGORITHM-210 LC061. Rotate List - 2 pointers
    solutions: https://leetcode.com/problems/rotate-list/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC061RotateList.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC061RotateList
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+
+        public ListNode RotateRight(ListNode head, int k)
+        {
+
+            if (head == null)
+            {
+                return head;
+            }
+
+            ListNode dummy = new ListNode();
+            dummy.next = head;
+
+            // count length
+            int length = 0;
+            while (head != null)
+            {
+                length++;
+                head = head.next;
+            }
+            k = k % length;
+
+            if (k == 0)
+            {
+                return dummy.next;
+            }
+
+            ListNode fast = dummy;
+            ListNode slow = dummy;
+            while (k > 0)
+            {
+                fast = fast.next;
+                k--;
+            }
+            while (fast.next != null)
+            {
+                fast = fast.next;
+                slow = slow.next;
+            }
+
+            // swap
+            fast.next = dummy.next;
+            dummy.next = slow.next;
+            slow.next = null;
+
+            return dummy.next;
+        }
+    }
+}
