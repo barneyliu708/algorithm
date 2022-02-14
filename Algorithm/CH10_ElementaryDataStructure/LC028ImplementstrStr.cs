4b909318c0c497f32fef204fd4be8e3a12911c68
barneyliu708 <barneyliu708@gmail.com>
2022-02-13 19:34:07 -0800

    ALGORITHM-205 LC028. Implement strStr() - 2 pointers
    2 pointers - approach 1: https://www.cnblogs.com/grandyang/p/4606696.html

---
+++ Algorithm/CH10_ElementaryDataStructure/LC028ImplementstrStr.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC028ImplementstrStr
+    {
+        public int StrStr(string haystack, string needle)
+        {
+
+            if (needle.Length == 0)
+            {
+                return 0;
+            }
+
+            for (int i = 0; i <= haystack.Length - needle.Length; i++)
+            {
+                int j = 0;
+                for (; j < needle.Length; j++)
+                {
+                    if (haystack[i + j] != needle[j])
+                    {
+                        break;
+                    }
+                }
+                if (j == needle.Length)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
