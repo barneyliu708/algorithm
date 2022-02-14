bb1e8064e34d31307080e9ffdad56bb0cf575b82
barneyliu708 <barneyliu708@gmail.com>
2022-02-13 21:07:45 -0800

    ALGORITHM-211 LC367. Valid Perfect Square - binary search
    binary search: https://leetcode.com/problems/valid-perfect-square/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC367ValidPerfectSquare.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC367ValidPerfectSquare
+    {
+        public bool IsPerfectSquare(int num)
+        {
+
+            if (num == 1 || num == 4 || num == 9 || num == 16)
+            {
+                return true;
+            }
+
+            int lo = 0;
+            int hi = num / 4;
+
+            while (lo <= hi)
+            {
+                int mid = lo + (hi - lo) / 2;
+                long sqr = (long)mid * mid;
+                if (sqr == num)
+                {
+                    return true;
+                }
+                else if (sqr < num)
+                {
+                    lo = mid + 1;
+                }
+                else
+                {
+                    hi = mid - 1;
+                }
+            }
+
+            return false;
+        }
+    }
+}
