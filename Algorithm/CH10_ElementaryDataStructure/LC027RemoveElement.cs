f73d5eb8b24690e677dd23a6b410488395793bd3
barneyliu708 <barneyliu708@gmail.com>
2022-02-13 19:19:31 -0800

    ALGORITHM-208 LC027. Remove Element - 2 pointers
    2 pointers - approach 1: https://leetcode.com/problems/remove-element/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC027RemoveElement.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC027RemoveElement
+    {
+        public int RemoveElement(int[] nums, int val)
+        {
+
+            int p = 0;
+            int i = 0;
+            while (p < nums.Length)
+            {
+                if (nums[p] != val)
+                {
+                    nums[i] = nums[p];
+                    i++;
+                }
+                p++;
+            }
+
+            return i;
+        }
+    }
+}
