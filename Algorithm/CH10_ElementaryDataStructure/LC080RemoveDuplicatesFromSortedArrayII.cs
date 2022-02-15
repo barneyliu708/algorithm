137dee3c15f2c7b95e9ee57b9a100efb6bafcf32
barneyliu708 <barneyliu708@gmail.com>
2022-02-14 20:33:40 -0800

    ALGORITHM-213 LC080. Remove Duplicates from Sorted Array II
    2 pointers: https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC080RemoveDuplicatesFromSortedArrayII.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC080RemoveDuplicatesFromSortedArrayII
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+
+            int l = 0;
+            int r = 0;
+
+            int pre = int.MinValue;
+            int count = 0;
+            while (r < nums.Length)
+            {
+                if (r == 0 || nums[r - 1] != nums[r])
+                {
+                    count = 1;
+                    nums[l++] = nums[r];
+                }
+                else
+                {
+                    count++;
+                    if (count <= 2)
+                    {
+                        nums[l++] = nums[r];
+                    }
+                }
+                r++;
+            }
+
+            return l;
+        }
+    }
+}
