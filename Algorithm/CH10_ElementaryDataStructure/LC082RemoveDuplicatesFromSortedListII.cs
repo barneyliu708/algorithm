f2cbed6fd79b624d16b017eebc187e42acd6ab22
barneyliu708 <barneyliu708@gmail.com>
2022-02-14 21:09:32 -0800

    ALGORITHM-214 LC082. Remove Duplicates from Sorted List II
    2 pointers: https://www.cnblogs.com/grandyang/p/4069003.html

---
+++ Algorithm/CH10_ElementaryDataStructure/LC082RemoveDuplicatesFromSortedListII.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC082RemoveDuplicatesFromSortedListII
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+
+        public ListNode DeleteDuplicates(ListNode head)
+        {
+            ListNode dummy = new ListNode();
+            dummy.next = head;
+
+            ListNode l = dummy;
+            ListNode r = head;
+            while (r != null)
+            {
+                if (r.next != null && r.val == r.next.val)
+                {
+                    int val = r.val;
+                    while (r != null && r.val == val)
+                    {
+                        r = r.next;
+                    }
+                }
+                else // r.next == null || r.val != r.next.val
+                { 
+                    l.next = r;
+                    l = l.next;
+                    r = r.next;
+                }
+            }
+
+            l.next = null; // break the chain after the left node
+
+            return dummy.next;
+        }
+    }
+}
