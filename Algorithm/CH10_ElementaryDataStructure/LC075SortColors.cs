4f3abe0cc683356f1317ec6c0c2f9b2cc37a1828
barneyliu708 <barneyliu708@gmail.com>
2022-02-14 20:30:54 -0800

    ALGORITHM-212 LC075. Sort Colors
    solution: https://leetcode.com/problems/sort-colors/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC075SortColors.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC075SortColors
+    {
+        public void SortColors(int[] nums)
+        {
+
+            int l = 0;
+            int r = nums.Length - 1;
+            int cur = 0;
+            while (cur <= r)
+            {
+                if (nums[cur] == 0)
+                {
+                    Swap(nums, cur, l);
+                    l++;
+                    cur++; // there could be only 0s or 1s on the left side of cur
+                }
+                else if (nums[cur] == 2)
+                {
+                    Swap(nums, cur, r);
+                    r--;
+                }
+                else
+                {
+                    cur++;
+                }
+            }
+        }
+
+        private void Swap(int[] nums, int i, int j)
+        {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+    }
+}
