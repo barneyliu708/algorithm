bbd2538ba405e35990a3fefbe92711196b3b27bc
barneyliu708 <barneyliu708@gmail.com>
2022-02-15 20:32:48 -0800

    ALGORITHM-217 LC088. Merge Sorted Array - 2 pointers
    2 pointers - approach 3: https://leetcode.com/problems/merge-sorted-array/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC088MergeSortedArray.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC088MergeSortedArray
+    {
+        public void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            int i = m - 1;
+            int j = n - 1;
+            int p = m + n - 1;
+
+            while (i >= 0 && j >= 0)
+            {
+                if (nums1[i] > nums2[j])
+                {
+                    nums1[p--] = nums1[i--];
+                }
+                else
+                {
+                    nums1[p--] = nums2[j--];
+                }
+            }
+
+            while (j >= 0)
+            {
+                nums1[p--] = nums2[j--];
+            }
+        }
+    }
+}
