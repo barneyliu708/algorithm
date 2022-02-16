ec1534c741066c010a1b4c621261b2e07fe9aab8
barneyliu708 <barneyliu708@gmail.com>
2022-02-15 17:00:40 -0800

    ALGORITHM-215 LC125. Valid Palindrome - 2 pointers
    2 pointers: https://leetcode.com/problems/valid-palindrome/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC125ValidPalindrome.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC125ValidPalindrome
+    {
+        public bool IsPalindrome(string s)
+        {
+
+            int l = 0;
+            int r = s.Length - 1;
+
+            while (l < r)
+            {
+                while (l < r && !Char.IsLetterOrDigit(s[l]))
+                {
+                    l++;
+                }
+                while (l < r && !Char.IsLetterOrDigit(s[r]))
+                {
+                    r--;
+                }
+                if (Char.ToLower(s[l]) != Char.ToLower(s[r]))
+                {
+                    return false;
+                }
+                l++;
+                r--;
+            }
+
+            return true;
+        }
+    }
+}
