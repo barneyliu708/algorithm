cf9039cf02838c2aa1ae6fb09a477c66dfeaaa80
barneyliu708 <barneyliu708@gmail.com>
2022-02-16 21:48:11 -0800

    ALGORITHM-222 LC253. Meeting Rooms II
    approach 2: https://leetcode.com/problems/meeting-rooms-ii/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC253MeetingRoomsII.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC253MeetingRoomsII
+    {
+        public int MinMeetingRooms(int[][] intervals)
+        {
+
+            int[] starts = new int[intervals.Length];
+            int[] ends = new int[intervals.Length];
+
+            for (int i = 0; i < intervals.Length; i++)
+            {
+                starts[i] = intervals[i][0];
+                ends[i] = intervals[i][1];
+            }
+
+            Array.Sort(starts);
+            Array.Sort(ends);
+
+            int si = 0;
+            int ei = 0;
+            int rooms = 0;
+            int ans = 0;
+            while (si < starts.Length)
+            {
+                if (starts[si] < ends[ei])
+                {
+                    rooms++;
+                    si++;
+                }
+                else if (starts[si] > ends[ei])
+                {
+                    rooms--;
+                    ei++;
+                }
+                else
+                {
+                    si++;
+                    ei++;
+                }
+                ans = Math.Max(ans, rooms);
+            }
+            return ans;
+        }
+    }
+}
