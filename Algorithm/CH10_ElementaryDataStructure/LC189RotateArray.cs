578672855ede8e07f244d8ca7f2f484ec606f7f3
barneyliu708 <barneyliu708@gmail.com>
2022-02-16 21:09:25 -0800

    ALGORITHM-221 LC189. Rotate Array
    approach 3: https://leetcode.com/problems/rotate-array/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC189RotateArray.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC189RotateArray
+    {
+        public void Rotate(int[] nums, int k)
+        {
+
+            k %= nums.Length;
+
+            int count = 0;
+            for (int start = 0; count < nums.Length; start++)
+            {
+                int curIndex = start;
+                int curValue = nums[curIndex];
+                do
+                {
+                    int nextIndex = (curIndex + k) % nums.Length;
+                    int nextValue = nums[nextIndex];
+
+                    nums[nextIndex] = curValue;
+                    curIndex = nextIndex;
+                    curValue = nextValue;
+
+                    count++;
+                }
+                while (curIndex != start);
+            }
+        }
+    }
+}
