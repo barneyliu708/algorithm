2269e4e109462c712d30c0c5974dbb3ed53cf774
barneyliu708 <barneyliu708@gmail.com>
2022-02-16 20:03:25 -0800

    ALGORITHM-219 LC161. One Edit Distance
    https://leetcode.com/problems/one-edit-distance/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC161OneEditDistance.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC161OneEditDistance
+    {
+        public bool IsOneEditDistance(string s, string t)
+        {
+
+            if (s.Length > t.Length)
+            {
+                return IsOneEditDistance(t, s);
+            }
+
+            if (t.Length - s.Length > 1)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] != t[i])
+                {
+                    if (s.Length == t.Length)
+                    {
+                        return s.Substring(i + 1) == t.Substring(i + 1);
+                    }
+                    else
+                    {
+                        return s.Substring(i) == t.Substring(i + 1);
+                    }
+                }
+            }
+
+            return s.Length + 1 == t.Length;
+        }
+    }
+}
