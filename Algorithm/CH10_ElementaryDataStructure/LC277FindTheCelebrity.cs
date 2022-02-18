c8e8ad7b310d4287268b803181ad8104f04edf22
barneyliu708 <barneyliu708@gmail.com>
2022-02-17 20:32:56 -0800

    ALGORITHM-224 LC277. Find the Celebrity
    brute force - approach 1: https://leetcode.com/problems/find-the-celebrity/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC277FindTheCelebrity.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    public class LC277FindTheCelebrity
+    {
+        
+        public class BruteForce
+        {
+            public bool Knows(int i, int j)
+            {
+                return i == j;
+            }
+            public int FindCelebrity(int n)
+            {
+
+                for (int i = 0; i < n; i++)
+                {
+                    int j;
+                    for (j = 0; j < n; j++)
+                    {
+                        if (i != j && (!Knows(j, i) || Knows(i, j)))
+                        {
+                            break;
+                        }
+                    }
+                    if (j == n) // all n - 1 people knows i, and i don't know any of n - 1 people
+                    { 
+                        return i;
+                    }
+                }
+
+                return -1;
+            }
+        }
+
+    }
+}
