24fc62200622ab0b77669ab821f639b251096d1a
barneyliu708 <barneyliu708@gmail.com>
2022-02-17 19:57:01 -0800

    ALGORITHM-223 LC251. Flatten 2D Vector
    2 pointers - approach 2: https://leetcode.com/problems/flatten-2d-vector/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC251Flatten2DVector.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC251Flatten2DVector
+    {
+        public class Vector2D
+        {
+
+            private int[][] vectors;
+            private int i;
+            private int j;
+
+            public Vector2D(int[][] vec)
+            {
+                vectors = vec;
+                i = 0;
+                j = 0;
+            }
+
+            public int Next()
+            {
+                while (i < vectors.Length && j == vectors[i].Length)
+                {
+                    i++;
+                    j = 0;
+                }
+                return vectors[i][j++];
+            }
+
+            public bool HasNext()
+            {
+                while (i < vectors.Length && j == vectors[i].Length)
+                {
+                    i++;
+                    j = 0;
+                }
+                return i < vectors.Length;
+            }
+        }
+
+    }
+}
