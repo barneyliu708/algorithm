1ee8951d0e4ddcf15701af118f7771631e5a055f
barneyliu708 <barneyliu708@gmail.com>
2022-02-17 20:40:10 -0800

    ALGORITHM-225 LC374. Guess Number Higher or Lower
    https://leetcode.com/problems/guess-number-higher-or-lower/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC374GuessNumberHigherOrLower.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC374GuessNumberHigherOrLower
+    {
+        int guess (int guess)
+        {
+            return 0;
+        }
+        public int GuessNumber(int n)
+        {
+            int hi = n;
+            int lo = 1;
+            while (lo <= hi)
+            {
+                int mid = lo + (hi - lo) / 2;
+                if (guess(mid) == 0)
+                {
+                    return mid;
+                }
+                else if (guess(mid) == 1)
+                {
+                    lo = mid + 1;
+                }
+                else
+                {
+                    hi = mid - 1;
+                }
+            }
+
+            return 0;
+        }
+    }
+}
