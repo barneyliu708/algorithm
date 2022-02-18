002fe719eaac133a5e01f6a432092a8d6087a93d
barneyliu708 <barneyliu708@gmail.com>
2022-02-17 20:49:39 -0800

    ALGORITHM-226 LC283. Move Zeroes
    https://leetcode.com/problems/move-zeroes/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC283MoveZeroes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC283MoveZeroes
+    {
+        public void MoveZeroes(int[] nums)
+        {
+            int l = 0;
+            int r = 0;
+            while (r < nums.Length)
+            {
+                if (nums[r] != 0)
+                {
+                    nums[l++] = nums[r];
+                }
+                r++;
+            }
+
+            while (l < nums.Length)
+            {
+                nums[l++] = 0;
+            }
+        }
+    }
+}
