77f22a8749e40e8d355a31b826ad4371f01dfc33
barneyliu708 <barneyliu708@gmail.com>
2022-02-19 14:40:03 -0800

    ALGORITHM-28 LC347 Top K Frequent Elements - priority queue
    priority queue - approach 1: https://leetcode.com/problems/top-k-frequent-elements/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC347TopKFrequentElements.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC347TopKFrequentElements
+    {
+        public int[] TopKFrequent(int[] nums, int k)
+        {
+
+            Dictionary<int, int> map = new Dictionary<int, int>(); // num - count;
+            foreach (int num in nums)
+            {
+                if (!map.ContainsKey(num))
+                {
+                    map[num] = 0;
+                }
+                map[num]++;
+            }
+
+            PriorityQueue<int, int> heap = new PriorityQueue<int, int>();
+            foreach (var kv in map)
+            {
+                heap.Enqueue(kv.Key, kv.Value);
+                if (heap.Count > k)
+                {
+                    heap.Dequeue(); // dequeue the element with the mininal count
+                }
+            }
+
+            int[] ans = new int[k];
+            for (int i = k - 1; i >= 0; i--)
+            {
+                ans[i] = heap.Dequeue();
+            }
+
+            return ans;
+        }
+    }
+}
