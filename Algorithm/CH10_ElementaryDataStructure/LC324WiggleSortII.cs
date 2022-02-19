3f1921645d9c8554869f833e4b34af24e9e86977
barneyliu708 <barneyliu708@gmail.com>
2022-02-19 13:50:02 -0800

    ALGORITHM-27 LC324 Wiggle Sort II - sorting
    sorting - approach 1: https://www.cnblogs.com/grandyang/p/5139057.html

---
+++ Algorithm/CH10_ElementaryDataStructure/LC324WiggleSortII.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC324WiggleSortII
+    {
+        public void WiggleSort(int[] nums)
+        {
+            int[] temp = new int[nums.Length];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                temp[i] = nums[i];
+            }
+            Array.Sort(temp);
+
+            int s = (nums.Length - 1) / 2;
+            int b = nums.Length - 1;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = i % 2 == 1 ? temp[b--] : temp[s--];
+            }
+        }
+    }
+}
