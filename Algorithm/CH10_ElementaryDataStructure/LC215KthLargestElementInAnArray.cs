ab667037c37c85f3e7fa55a5e5fe7be283d773a5
barneyliu708 <barneyliu708@gmail.com>
2022-02-19 13:24:59 -0800

    ALGORITHM-23 LC215 Kth Largest Element in an Array - priority queue
    priority queue - approach 1: https://leetcode.com/problems/kth-largest-element-in-an-array/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC215KthLargestElementInAnArray.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC215KthLargestElementInAnArray
+    {
+        public int FindKthLargest(int[] nums, int k)
+        {
+
+            PriorityQueue<int, int> queue = new PriorityQueue<int, int>(); // element - priority
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                queue.Enqueue(nums[i], nums[i]);
+                if (queue.Count > k)
+                {
+                    queue.Dequeue();
+                }
+            }
+
+            return queue.Dequeue();
+        }
+    }
+}
