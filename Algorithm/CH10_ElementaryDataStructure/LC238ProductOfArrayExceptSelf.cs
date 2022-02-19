52c438b29037c66dee8e08aa66f9f3aa2db3d9a9
barneyliu708 <barneyliu708@gmail.com>
2022-02-18 19:07:25 -0800

    ALGORITHM-228 LC238. Product of Array Except Self
    https://leetcode.com/problems/product-of-array-except-self/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC238ProductOfArrayExceptSelf.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC238ProductOfArrayExceptSelf
+    {
+        public int[] ProductExceptSelf(int[] nums)
+        {
+            int[] prefix = new int[nums.Length];
+            int[] suffix = new int[nums.Length];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (i == 0)
+                {
+                    prefix[i] = nums[i];
+                }
+                else
+                {
+                    prefix[i] = prefix[i - 1] * nums[i];
+                }
+            }
+
+            for (int i = nums.Length - 1; i >= 0; i--)
+            {
+                if (i == nums.Length - 1)
+                {
+                    suffix[i] = nums[i];
+                }
+                else
+                {
+                    suffix[i] = suffix[i + 1] * nums[i];
+                }
+            }
+
+            int[] ans = new int[nums.Length];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (i == 0)
+                {
+                    ans[i] = suffix[i + 1];
+                }
+                else if (i == nums.Length - 1)
+                {
+                    ans[i] = prefix[i - 1];
+                }
+                else
+                {
+                    ans[i] = prefix[i - 1] * suffix[i + 1];
+                }
+            }
+
+            return ans;
+        }
+    }
+}
