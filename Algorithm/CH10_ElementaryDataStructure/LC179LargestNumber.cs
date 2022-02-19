f1a8a6073cc30688ec7952e1a1021d6b406eb283
barneyliu708 <barneyliu708@gmail.com>
2022-02-18 19:28:57 -0800

    ALGORITHM-229 LC179. Largest Number - greedy
    greedy: https://leetcode.com/problems/largest-number/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC179LargestNumber.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC179LargestNumber
+    {
+        public string LargestNumber(int[] nums)
+        {
+            List<int> sortedNums = nums.ToList();
+            sortedNums.Sort((x, y) => {
+                string xs = x.ToString();
+                string ys = y.ToString();
+                return -1 * (xs + ys).CompareTo(ys + xs);
+            });
+
+            if (sortedNums[0] == 0)
+            {
+                return "0";
+            }
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < sortedNums.Count; i++)
+            {
+                sb.Append(sortedNums[i].ToString());
+            }
+            return sb.ToString();
+        }
+    }
+}
