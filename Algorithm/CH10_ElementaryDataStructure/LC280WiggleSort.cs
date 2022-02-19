8a504ce926e47dd9f6cec23292ae98687a53ca8a
barneyliu708 <barneyliu708@gmail.com>
2022-02-18 19:38:24 -0800

    ALGORITHM-230 LC280. Wiggle Sort - greedy
    greedy - approach 2: https://leetcode.com/problems/wiggle-sort/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC280WiggleSort.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    class LC280WiggleSort
+    {
+        public void WiggleSort(int[] nums)
+        {
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                if ((i % 2 == 0 && nums[i] > nums[i + 1]) || (i % 2 == 1 && nums[i] < nums[i + 1]))
+                {
+                    Swap(nums, i, i + 1);
+                }
+            }
+        }
+
+        public void Swap(int[] nums, int i, int j)
+        {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+    }
+}
