37faaa83e668bb5f26f332212018a109a97ffede
barneyliu708 <barneyliu708@gmail.com>
2022-02-21 00:24:39 -0800

    ALGORITHM-231 LC009. Palindrome Number
    https://leetcode.com/problems/palindrome-number/solution/

---
+++ Algorithm/LC009PalindromeNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    internal class LC009PalindromeNumber
+    {
+        public bool IsPalindrome(int x)
+        {
+            if (x < 0 || (x % 10 == 0 && x != 0))
+            {
+                return false;
+            }
+
+            int revert = 0;
+            while (x > revert)
+            {
+                revert = revert * 10 + x % 10;
+                x /= 10;
+            }
+
+            return x == revert || x == revert / 10;
+        }
+    }
+}
