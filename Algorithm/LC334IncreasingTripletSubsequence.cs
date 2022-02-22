9f3028272c13846b0e4cfb034d331d745fb693c3
barneyliu708 <barneyliu708@gmail.com>
2022-02-21 19:32:43 -0800

    ALGORITHM-234 LC334. Increasing Triplet Subsequence - greedy
    greedy - approach 1: https://leetcode.com/problems/increasing-triplet-subsequence/solution/

---
+++ Algorithm/LC334IncreasingTripletSubsequence.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    internal class LC334IncreasingTripletSubsequence
+    {
+        public bool IncreasingTriplet(int[] nums)
+        {
+
+            int first = int.MaxValue;
+            int second = int.MaxValue;
+
+            foreach (int num in nums)
+            {
+                if (num <= first)
+                {
+                    first = num;
+                }
+                else if (num <= second)
+                {
+                    second = num;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
