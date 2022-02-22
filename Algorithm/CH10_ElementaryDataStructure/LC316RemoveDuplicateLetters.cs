d8b116383e9860bd5402ebfee6b1831390da8d0a
barneyliu708 <barneyliu708@gmail.com>
2022-02-21 16:13:23 -0800

    ALGORITHM-60 LC316 Remove Duplicate Letters - greedy
    greedy - approach 2: https://leetcode.com/problems/remove-duplicate-letters/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC316RemoveDuplicateLetters.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC316RemoveDuplicateLetters
+    {
+        public string RemoveDuplicateLetters(string s)
+        {
+
+            Stack<char> stack = new Stack<char>();
+            HashSet<char> seen = new HashSet<char>();
+            Dictionary<char, int> lastOccurs = new Dictionary<char, int>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                lastOccurs[s[i]] = i;
+            }
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                char ch = s[i];
+                if (!seen.Contains(ch))
+                {
+                    while (stack.Count > 0 && ch < stack.Peek() && lastOccurs[stack.Peek()] > i)
+                    {
+                        seen.Remove(stack.Pop());
+                    }
+                    seen.Add(ch);
+                    stack.Push(ch);
+                }
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            while (stack.Count > 0)
+            {
+                sb.Insert(0, stack.Pop());
+            }
+
+            return sb.ToString();
+        }
+    }
+}
