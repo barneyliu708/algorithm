5c0ba5c50a587c67a0b720515f57fedc526e634c
barneyliu708 <barneyliu708@gmail.com>
2022-02-22 21:07:34 -0800

    ALGORITHM-238 LC1937. Maximum Number of Points with Cost - dp
    dp: https://leetcode.com/problems/maximum-number-of-points-with-cost/discuss/1758037/C%2B%2B-99-or-O(MN)-time-O(N)-Space-or

---
+++ Algorithm/CH10_ElementaryDataStructure/LC1937MaximumNumberOfPointsWithCost.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC1937MaximumNumberOfPointsWithCost
+    {
+        public long MaxPoints(int[][] points)
+        {
+
+            int m = points.Length;
+            int n = points[0].Length;
+
+            long[] lmax = new long[n];
+            long[] rmax = new long[n];
+            long[] dp = new long[n];
+            for (int i = 0; i < m; i++)
+            {
+
+                lmax[0] = dp[0];
+                for (int j = 1; j < n; j++)
+                {
+                    lmax[j] = Math.Max(lmax[j - 1], dp[j] + j); // when looking towards left, the closer, the bigger
+                }
+
+                rmax[n - 1] = dp[n - 1] - (n - 1);
+                for (int j = n - 2; j >= 0; j--)
+                {
+                    rmax[j] = Math.Max(rmax[j + 1], dp[j] - j); // when looking towards right, the closer, the bigger
+                }
+
+                for (int j = 0; j < n; j++)
+                {
+                    dp[j] = points[i][j] + Math.Max(lmax[j] - j, rmax[j] + j);
+                }
+            }
+
+            long ans = 0;
+            for (int j = 0; j < n; j++)
+            {
+                ans = Math.Max(ans, dp[j]);
+            }
+            return ans;
+        }
+    }
+}
