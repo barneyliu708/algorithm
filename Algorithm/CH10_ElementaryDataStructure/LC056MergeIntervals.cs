2495ed47602955de7618dcb688f67249a13f24bc
barneyliu708 <barneyliu708@gmail.com>
2022-02-23 21:22:47 -0800

    ALGORITHM-241 LC056. Merge Intervals - sorting
    sorting solution - approach 2: https://leetcode.com/problems/merge-intervals/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC056MergeIntervals.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC056MergeIntervals
+    {
+        public int[][] Merge(int[][] intervals)
+        {
+
+            if (intervals.Length == 0)
+            {
+                return new int[0][];
+            }
+
+            Array.Sort(intervals, (int[] x, int[] y) => { return x[0].CompareTo(y[0]); });
+
+            List<int[]> ans = new List<int[]>();
+            ans.Add(intervals[0]);
+
+            for (int i = 1; i < intervals.Length; i++)
+            {
+                int[] curr = intervals[i];
+                int[] last = ans[ans.Count - 1];
+                if (curr[0] <= last[1])
+                { // merge the two interval
+                    last[1] = Math.Max(last[1], curr[1]);
+                }
+                else
+                {
+                    ans.Add(curr);
+                }
+            }
+
+            return ans.ToArray();
+        }
+    }
+}
