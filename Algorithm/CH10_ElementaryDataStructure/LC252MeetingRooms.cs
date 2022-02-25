3eb431b2ccdb841698c50b4fd2c9c1079597a1a7
barneyliu708 <barneyliu708@gmail.com>
2022-02-25 14:46:30 -0800

    ALGORITHM-246 LC252. Meeting Rooms - sorting
    sorting: https://leetcode.com/problems/meeting-rooms/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC252MeetingRooms.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC252MeetingRooms
+    {
+        public bool CanAttendMeetings(int[][] intervals)
+        {
+            Array.Sort(intervals, (int[] x, int[] y) => { return x[0].CompareTo(y[0]); });
+            for (int i = 1; i < intervals.Length; i++)
+            {
+                int[] cur = intervals[i];
+                int[] pre = intervals[i - 1];
+                if (cur[0] < pre[1])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
