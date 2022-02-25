009b4a8900f01a70544c635b677f67b6febaa068
barneyliu708 <barneyliu708@gmail.com>
2022-02-24 22:17:13 -0800

    ALGORITHM-245 LC236. Lowest Common Ancestor of a Binary Tree - dft
    dft - approach 1: https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC236LowestCommonAncestorOfABinaryTree.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC236LowestCommonAncestorOfABinaryTree
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        private TreeNode ans;
+
+        public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+        {
+
+            DftUti(root, p, q);
+
+            return ans;
+        }
+
+        private int DftUti(TreeNode root, TreeNode p, TreeNode q)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            int left = DftUti(root.left, p, q);
+            int right = DftUti(root.right, p, q);
+            int mid = (root == p || root == q) ? 1 : 0;
+
+            if (mid + left + right >= 2 && this.ans == null) // check ans == null to ensure ans is the first node which is greater than 2
+            { 
+                this.ans = root;
+            }
+
+            return mid + left + right;
+        }
+    }
+}
