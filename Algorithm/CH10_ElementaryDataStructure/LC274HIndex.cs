cac9cf0391ddbd168c37d3f0876a26b028aa0698
barneyliu708 <barneyliu708@gmail.com>
2022-02-25 14:57:09 -0800

    ALGORITHM-247 LC274. H-Index
    solution: https://leetcode.com/problems/h-index/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC274HIndex.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC274HIndex
+    {
+        public int HIndex(int[] citations)
+        {
+
+            Array.Sort(citations, (int x, int y) => {
+                return y.CompareTo(x); // decending order
+            });
+
+            int h = 0;
+            while (h < citations.Length && citations[h] > h)
+            {
+                h++;
+            }
+
+            return h;
+        }
+    }
+}
