b417b5b7d29925640675b38534b6eee25de1dfac
barneyliu708 <barneyliu708@gmail.com>
2022-02-24 21:45:52 -0800

    ALGORITHM-244 LC147. Insertion Sort List
    solution: https://leetcode.com/problems/insertion-sort-list/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC147InsertionSortList.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC147InsertionSortList
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+        public ListNode InsertionSortList(ListNode head)
+        {
+            ListNode ans = new ListNode();
+
+            ListNode cur;
+            while (head != null)
+            {
+                cur = head;
+                head = head.next;
+                cur.next = null; // break the link to the original list
+
+                ListNode insert = ans;
+                while (insert.next != null && insert.next.val < cur.val)
+                {
+                    insert = insert.next;
+                }
+
+                ListNode next = insert.next;
+                insert.next = cur;
+                cur.next = next;
+            }
+
+            return ans.next;
+        }
+    }
+}
