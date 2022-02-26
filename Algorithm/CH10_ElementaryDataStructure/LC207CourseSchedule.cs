9addb5a18d0728b71a78b9195dbdc87bdcbcedd1
barneyliu708 <barneyliu708@gmail.com>
2022-02-25 17:47:40 -0800

    ALGORITHM-248 LC207. Course Schedule - dft + graph
    dft + graph - approach 2: https://leetcode.com/problems/course-schedule/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC207CourseSchedule.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC207CourseSchedule
+    {
+        public bool CanFinish(int numCourses, int[][] prerequisites)
+        {
+
+            Dictionary<int, List<int>> map = new Dictionary<int, List<int>>(); // prerequisite course - course
+            foreach (int[] course in prerequisites)
+            {
+                if (!map.ContainsKey(course[1]))
+                {
+                    map[course[1]] = new List<int>();
+                }
+                map[course[1]].Add(course[0]);
+            }
+
+            bool[] visited = new bool[numCourses];
+            bool[] checkes = new bool[numCourses];
+
+            for (int curCourse = 0; curCourse < numCourses; curCourse++)
+            {
+                if (IsCycle(curCourse, map, visited, checkes))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public bool IsCycle(int curCourse, Dictionary<int, List<int>> map, bool[] visited, bool[] checkes)
+        {
+
+            if (checkes[curCourse])
+            {
+                return false;
+            }
+
+            if (visited[curCourse]) // cycle detected
+            { 
+                return true;
+            }
+
+            if (!map.ContainsKey(curCourse))
+            {
+                return false;
+            }
+
+            visited[curCourse] = true;
+
+            bool ans = false;
+            foreach (int adjCourse in map[curCourse])
+            {
+                ans = IsCycle(adjCourse, map, visited, checkes);
+                if (ans)
+                {
+                    break;
+                }
+            }
+
+            visited[curCourse] = false;
+            checkes[curCourse] = true;
+            return ans;
+        }
+    }
+}
