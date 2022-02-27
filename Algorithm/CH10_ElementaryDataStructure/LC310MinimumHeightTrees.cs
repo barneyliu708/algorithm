33bbe606a9eae0bf361bd3a2da73e26841f13fc6
barneyliu708 <barneyliu708@gmail.com>
2022-02-26 21:16:57 -0800

    ALGORITHM-251 LC310. Minimum Height Trees - tropological sort
    solution: https://leetcode.com/problems/minimum-height-trees/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC310MinimumHeightTrees.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC310MinimumHeightTrees
+    {
+        public IList<int> FindMinHeightTrees(int n, int[][] edges)
+        {
+
+            if (n < 2)
+            {
+                List<int> centroids = new List<int>();
+                for (int i = 0; i < n; i++)
+                {
+                    centroids.Add(i);
+                }
+                return centroids;
+            }
+
+            List<int>[] adjacentList = new List<int>[n];
+            for (int i = 0; i < n; i++)
+            {
+                adjacentList[i] = new List<int>();
+            }
+
+            foreach (int[] edge in edges)
+            {
+                adjacentList[edge[0]].Add(edge[1]);
+                adjacentList[edge[1]].Add(edge[0]);
+            }
+
+            List<int> leaves = new List<int>();
+            for (int i = 0; i < n; i++)
+            {
+                if (adjacentList[i].Count == 1)
+                {
+                    leaves.Add(i);
+                }
+            }
+
+            int remainingNodes = n;
+            while (remainingNodes > 2)
+            {
+                remainingNodes -= leaves.Count;
+
+                List<int> newLeaves = new List<int>();
+                foreach (int leaf in leaves)
+                {
+                    int neighbor = adjacentList[leaf].First();
+                    adjacentList[neighbor].Remove(leaf);
+                    if (adjacentList[neighbor].Count == 1)
+                    {
+                        newLeaves.Add(neighbor);
+                    }
+                }
+
+                leaves = newLeaves;
+            }
+
+            return leaves;
+        }
+    }
+}
