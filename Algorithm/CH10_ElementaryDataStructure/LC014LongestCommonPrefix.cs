b6d5d816f0128145779298d73d92958a642efed8
barneyliu708 <barneyliu708@gmail.com>
2022-02-26 21:25:09 -0800

    ALGORITHM-252 LC014. Longest Common Prefix - string
    string - approach 2: https://leetcode.com/problems/longest-common-prefix/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC014LongestCommonPrefix.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC014LongestCommonPrefix
+    {
+        public string LongestCommonPrefix(string[] strs)
+        {
+            if (strs == null || strs.Length == 0)
+            {
+                return "";
+            }
+            for (int i = 0; i < strs[0].Length; i++)
+            {
+                char ch = strs[0][i];
+                for (int j = 1; j < strs.Length; j++)
+                {
+                    if (i == strs[j].Length || strs[j][i] != ch)
+                    {
+                        return strs[0].Substring(0, i);
+                    }
+                }
+            }
+            return strs[0];
+        }
+    }
+}
