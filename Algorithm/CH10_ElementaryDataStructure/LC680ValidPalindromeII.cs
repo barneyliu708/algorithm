dd7dd6bb910b7ac90fd8ca004435c3ef5e3b03f9
barneyliu708 <barneyliu708@gmail.com>
2022-02-28 15:53:49 -0800

    ALGORITHM-257 LC680. Valid Palindrome II - greedy
    greedy: https://www.cnblogs.com/grandyang/p/7618468.html

---
+++ Algorithm/CH10_ElementaryDataStructure/LC680ValidPalindromeII.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC680ValidPalindromeII
+    {
+        public bool ValidPalindrome(string s)
+        {
+
+            int left = 0;
+            int right = s.Length - 1;
+            while (left < right)
+            {
+                if (s[left] != s[right])
+                {
+                    return IsValid(s, left + 1, right) || IsValid(s, left, right - 1);
+                }
+                left++;
+                right--;
+            }
+            return true;
+        }
+
+        private bool IsValid(string s, int left, int right)
+        {
+            while (left < right)
+            {
+                if (s[left] != s[right])
+                {
+                    return false;
+                }
+                left++;
+                right--;
+            }
+            return true;
+        }
+    }
+}
