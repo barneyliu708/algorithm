8912a17699d041f8309ae4bf1a671e0669669d9f
barneyliu708 <barneyliu708@gmail.com>
2022-02-28 21:43:37 -0800

    ALGORITHM-258 LC294. Flip Game II - dp + backtracking
    solution: https://www.cnblogs.com/grandyang/p/5226206.html

---
+++ Algorithm/CH10_ElementaryDataStructure/LC294FlipGameII.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC294FlipGameII
+    {
+        public bool CanWin(string currentState)
+        {
+            for (int i = 1; i < currentState.Length; i++)
+            {
+                if (currentState[i] == '+' && currentState[i - 1] == '+' &&
+                    !CanWin(currentState.Substring(0, i - 1) + "--" + currentState.Substring(i + 1)))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
