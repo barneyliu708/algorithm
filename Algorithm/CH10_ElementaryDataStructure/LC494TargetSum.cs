27f6bce0222aead7fc3414605a9465e2a4ef0340
barneyliu708 <barneyliu708@gmail.com>
2022-03-04 20:05:19 -0800

    ALGORITHM-270 LC494. Target Sum - backtracking
    backtracking - approach 1: https://leetcode.com/problems/target-sum/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC494TargetSum.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC494TargetSum
+    {
+        class BacktrackingApproach
+        {
+            private int count;
+
+            public int FindTargetSumWays(int[] nums, int target)
+            {
+                FindTarget(nums, target, 0, 0);
+                return count;
+            }
+
+            private void FindTarget(int[] nums, int target, int i, int curSum)
+            {
+                if (i == nums.Length)
+                {
+                    if (curSum == target)
+                    {
+                        count++;
+                    }
+                }
+                else
+                {
+                    FindTarget(nums, target, i + 1, curSum + nums[i]);
+                    FindTarget(nums, target, i + 1, curSum - nums[i]);
+                }
+            }
+        }
+    }
+}
