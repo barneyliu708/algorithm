ba4e38a645e8ad1927fb8f541f9ca4a6b78343f2
barneyliu708 <barneyliu708@gmail.com>
2022-03-04 20:27:08 -0800

    ALGORITHM-271 LC1239. Maximum Length of a Concatenated String with Unique Characters - backtracking
    backtracking - approach 2 & 3: https://leetcode.com/problems/maximum-length-of-a-concatenated-string-with-unique-characters/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC1239MaximumLengthOfAConcatenatedStringWithUniqueCharacters.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC1239MaximumLengthOfAConcatenatedStringWithUniqueCharacters
+    {
+        public int MaxLength(IList<string> arr)
+        {
+            return MaxLength(arr, 0, new Dictionary<char, int>());
+        }
+
+        private int MaxLength(IList<string> arr, int start, Dictionary<char, int> counts)
+        {
+
+            foreach (char key in counts.Keys)
+            {
+                if (counts[key] > 1)
+                {
+                    return 0;
+                }
+            }
+
+            int ans = counts.Count;
+            for (int i = start; i < arr.Count; i++)
+            {
+
+                foreach (char ch in arr[i])
+                {
+                    if (!counts.ContainsKey(ch))
+                    {
+                        counts[ch] = 0;
+                    }
+                    counts[ch]++;
+                }
+
+                ans = Math.Max(ans, MaxLength(arr, i + 1, counts));
+
+                foreach (char ch in arr[i])
+                {
+                    counts[ch]--;
+                    if (counts[ch] == 0)
+                    {
+                        counts.Remove(ch);
+                    }
+                }
+            }
+
+            return ans;
+        }
+    }
+}
