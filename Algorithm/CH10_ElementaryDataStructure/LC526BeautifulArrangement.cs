0c6332e34f92de05dc666c1c2fb95ad8718b055b
barneyliu708 <barneyliu708@gmail.com>
2022-03-04 17:30:53 -0800

    ALGORITHM-267 LC526. Beautiful Arrangement - backtracking
    solution: https://leetcode.com/problems/beautiful-arrangement/solution/

--- Algorithm/CH10_ElementaryDataStructure/LC526BeautifulArrangement.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC526BeautifulArrangement.cs
@@ -8,5 +8,31 @@ namespace Algorithm.CH10_ElementaryDataStructure
 {
     internal class LC526BeautifulArrangement
     {
+        private int count;
+        public int CountArrangement(int n)
+        {
+            bool[] visited = new bool[n + 1];
+            Utility(n, 1, visited);
+            return count;
+        }
+
+        private void Utility(int N, int pos, bool[] visited)
+        {
+
+            if (pos > N)
+            {
+                count++;
+            }
+
+            for (int i = 1; i <= N; i++)
+            {
+                if (!visited[i] && (pos % i == 0 || i % pos == 0))
+                {
+                    visited[i] = true;
+                    Utility(N, pos + 1, visited);
+                    visited[i] = false;
+                }
+            }
+        }
     }
 }
