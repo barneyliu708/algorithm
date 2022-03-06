206cc82e8394c50c3ca8c5ad164d4639ed85edd5
barneyliu708 <barneyliu708@gmail.com>
2022-03-05 22:34:01 -0800

    ALGORITHM-274 LC254. Factor Combinations - backtracking
    backtracking - approach 2: https://www.cnblogs.com/grandyang/p/5332722.html

---
+++ Algorithm/CH10_ElementaryDataStructure/LC254FactorCombinations.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC254FactorCombinations
+    {
+        public IList<IList<int>> GetFactors(int n)
+        {
+            List<int> comb = new List<int>(); // store the current combination
+            List<IList<int>> ans = new List<IList<int>>(); // store the answer
+            GetFactors(n, 2, comb, ans);
+            return ans;
+        }
+
+        private void GetFactors(int n, int start, List<int> comb, List<IList<int>> ans)
+        {
+
+            for (int i = start; i <= Math.Sqrt(n); i++)
+            {
+                if (n % i != 0)
+                {
+                    continue;
+                }
+                List<int> newcomb = new List<int>(comb);
+                newcomb.Add(i);
+                GetFactors(n / i, i, newcomb, ans);
+                newcomb.Add(n / i);
+                ans.Add(newcomb);
+            }
+        }
+    }
+}
