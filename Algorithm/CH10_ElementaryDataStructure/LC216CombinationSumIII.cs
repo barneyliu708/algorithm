5ae20cc0ec06991a494b8dce09556c87b9531820
barneyliu708 <barneyliu708@gmail.com>
2022-03-05 21:57:38 -0800

    ALGORITHM-273 LC216. Combination Sum III - backtracking
    backtracking: https://leetcode.com/problems/combination-sum-iii/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC216CombinationSumIII.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC216CombinationSumIII
+    {
+        public IList<IList<int>> CombinationSum3(int k, int n)
+        {
+            List<int> comb = new List<int>();
+            List<IList<int>> ans = new List<IList<int>>();
+            Combination(k, n, 1, 0, comb, ans);
+            return ans;
+        }
+
+        public void Combination(int k, int target, int start, int cursum, List<int> comb, List<IList<int>> ans)
+        {
+            if (comb.Count == k)
+            {
+                if (cursum == target)
+                {
+                    ans.Add(new List<int>(comb));
+                }
+                return;
+            }
+
+            if (start > 9)
+            {
+                return;
+            }
+
+            for (int i = start; i <= 9; i++)
+            {
+                comb.Add(i);
+                Combination(k, target, i + 1, cursum + i, comb, ans);
+                comb.Remove(i);
+            }
+        }
+    }
+}
