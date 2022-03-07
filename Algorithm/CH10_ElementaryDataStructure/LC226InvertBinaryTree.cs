50e001ce1f608bf4f2b529a8725a7ba611c1513a
barneyliu708 <barneyliu708@gmail.com>
2022-03-06 19:38:34 -0800

    ALGORITHM-275 LC226. Invert Binary Tree - dft
    dft - approach 1: https://leetcode.com/problems/invert-binary-tree/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC226InvertBinaryTree.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC226InvertBinaryTree
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+        public TreeNode InvertTree(TreeNode root)
+        {
+            if (root == null)
+            {
+                return root;
+            }
+
+            TreeNode left = root.left;
+            root.left = root.right;
+            root.right = left;
+
+            InvertTree(root.left);
+            InvertTree(root.right);
+
+            return root;
+        }
+    }
+}
