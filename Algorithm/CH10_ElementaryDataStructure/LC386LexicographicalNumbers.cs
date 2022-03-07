3f891cb34606845ee54313bf22dd9518dd54fc2e
barneyliu708 <barneyliu708@gmail.com>
2022-03-06 21:25:44 -0800

    ALGORITHM-277 LC386. Lexicographical Numbers - backtracking
    backtracking - approach 2: https://www.cnblogs.com/grandyang/p/5798275.html

---
+++ Algorithm/CH10_ElementaryDataStructure/LC386LexicographicalNumbers.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC386LexicographicalNumbers
+    {
+        public IList<int> LexicalOrder(int n)
+        {
+            List<int> ans = new List<int>();
+            for (int i = 1; i <= 9; i++)
+            {
+                LexicalOrder(n, i, ans);
+            }
+            return ans;
+        }
+
+        private void LexicalOrder(int n, int cur, List<int> ans)
+        {
+            if (cur > n)
+            {
+                return;
+            }
+            ans.Add(cur);
+            for (int i = 0; i <= 9; i++)
+            {
+                int next = cur * 10 + i;
+                if (next > n)
+                {
+                    break;
+                }
+                LexicalOrder(n, next, ans);
+            }
+        }
+    }
+}
