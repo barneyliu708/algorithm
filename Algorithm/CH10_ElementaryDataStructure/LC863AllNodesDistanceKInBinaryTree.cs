d80ee4c47f89496e577a426628b4d58d1c2bdeaf
barneyliu708 <barneyliu708@gmail.com>
2022-03-07 21:32:02 -0800

    ALGORITHM-279 LC863. All Nodes Distance K in Binary Tree - dft
    dft - approach 1: https://leetcode.com/problems/all-nodes-distance-k-in-binary-tree/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC863AllNodesDistanceKInBinaryTree.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC863AllNodesDistanceKInBinaryTree
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+        public class ConvertToGraphApproach
+        {
+            public IList<int> DistanceK(TreeNode root, TreeNode target, int k)
+            {
+
+                Dictionary<TreeNode, TreeNode> parents = new Dictionary<TreeNode, TreeNode>();
+                MapParents(root, null, parents);
+
+                Queue<TreeNode> queue = new Queue<TreeNode>();
+                HashSet<TreeNode> visited = new HashSet<TreeNode>();
+
+                // breadth-first traversal, start with the target node, and keep track of the distance from the target node
+                queue.Enqueue(target);
+                visited.Add(target);
+                int dist = 0;
+                while (queue.Count > 0)
+                {
+                    if (dist == k)
+                    {
+                        break;
+                    }
+                    int count = queue.Count;
+                    for (int i = 0; i < count; i++)
+                    {
+                        TreeNode cur = queue.Dequeue();
+                        if (cur.left != null && !visited.Contains(cur.left))
+                        {
+                            visited.Add(cur.left);
+                            queue.Enqueue(cur.left);
+                        }
+                        if (cur.right != null && !visited.Contains(cur.right))
+                        {
+                            visited.Add(cur.right);
+                            queue.Enqueue(cur.right);
+                        }
+                        if (parents[cur] != null && !visited.Contains(parents[cur]))
+                        {
+                            visited.Add(parents[cur]);
+                            queue.Enqueue(parents[cur]);
+                        }
+                    }
+                    dist++;
+                }
+
+                List<int> ans = new List<int>();
+                foreach (TreeNode node in queue)
+                {
+                    ans.Add(node.val);
+                }
+                return ans;
+            }
+
+            private void MapParents(TreeNode cur, TreeNode parent, Dictionary<TreeNode, TreeNode> parents)
+            {
+                if (cur == null)
+                {
+                    return;
+                }
+
+                parents[cur] = parent;
+
+                MapParents(cur.left, cur, parents);
+                MapParents(cur.right, cur, parents);
+            }
+        }
+    }
+}
