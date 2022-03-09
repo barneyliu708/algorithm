8f6862da31f57be4901ece7af48c15c90cd4d128
barneyliu708 <barneyliu708@gmail.com>
2022-03-08 20:39:26 -0800

    ALGORITHM-281 LC543. Diameter of Binary Tree - dft
    dft: https://leetcode.com/problems/diameter-of-binary-tree/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC543DiameterOfBinaryTree.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC543DiameterOfBinaryTree
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        private int diameter = 0;
+        public int DiameterOfBinaryTree(TreeNode root)
+        {
+            LongestPath(root);
+            return diameter;
+        }
+
+        private int LongestPath(TreeNode root)
+        {
+
+            if (root == null)
+            {
+                return -1; // the path of the leaf is 0 rather than 1
+            }
+
+            int left = LongestPath(root.left);
+            int right = LongestPath(root.right);
+
+            diameter = Math.Max(diameter, left + right + 2);
+
+            return Math.Max(left + 1, right + 1);
+        }
+    }
+}
