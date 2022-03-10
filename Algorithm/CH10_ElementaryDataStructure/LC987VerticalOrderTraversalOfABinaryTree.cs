a6e80416999b7969a1862f3a2938f467913a56b2
barneyliu708 <barneyliu708@gmail.com>
2022-03-09 21:13:45 -0800

    ALGORITHM-285 LC987. Vertical Order Traversal of a Binary Tree - BFT
    BFT - approach 2: https://leetcode.com/problems/vertical-order-traversal-of-a-binary-tree/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC987VerticalOrderTraversalOfABinaryTree.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC987VerticalOrderTraversalOfABinaryTree
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+        public class Node
+        {
+            public int row;
+            public int col;
+            public TreeNode node;
+            public Node(int row, int col, TreeNode node)
+            {
+                this.row = row;
+                this.col = col;
+                this.node = node;
+            }
+        }
+        public IList<IList<int>> VerticalTraversal(TreeNode root)
+        {
+
+            Dictionary<int, List<(int row, int val)>> map = new Dictionary<int, List<(int row, int val)>>();
+            Queue<Node> queue = new Queue<Node>();
+
+            // BFT
+            queue.Enqueue(new Node(0, 0, root));
+            while (queue.Count > 0)
+            {
+                Node cur = queue.Dequeue();
+
+                if (!map.ContainsKey(cur.col))
+                {
+                    map[cur.col] = new List<(int row, int val)>();
+                }
+                map[cur.col].Add((cur.row, cur.node.val));
+
+                if (cur.node.left != null)
+                {
+                    queue.Enqueue(new Node(cur.row + 1, cur.col - 1, cur.node.left));
+                }
+
+                if (cur.node.right != null)
+                {
+                    queue.Enqueue(new Node(cur.row + 1, cur.col + 1, cur.node.right));
+                }
+            }
+
+            // prepare the answer
+            List<IList<int>> ans = new List<IList<int>>();
+            List<int> cols = map.Keys.ToList();
+            cols.Sort();
+            foreach (int col in cols)
+            {
+                map[col].Sort(((int row, int val) x, (int row, int val) y) => {
+                    if (x.row != y.row)
+                    {
+                        return x.row.CompareTo(y.row);
+                    }
+                    return x.val.CompareTo(y.val);
+                });
+                ans.Add(map[col].Select(x => x.val).ToList());
+            }
+
+            return ans;
+        }
+    }
+}
