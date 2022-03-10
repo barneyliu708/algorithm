c0801d620ae2b15e0b155b37f877b3534bef65ba
barneyliu708 <barneyliu708@gmail.com>
2022-03-09 20:36:02 -0800

    ALGORITHM-284 LC1650. Lowest Common Ancestor of a Binary Tree III - 2 pointers
    2 pointers: https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree-iii/discuss/1833237/Java-or-Simple

---
+++ Algorithm/CH10_ElementaryDataStructure/LC1650LowestCommonAncestorOfABinaryTreeIII.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC1650LowestCommonAncestorOfABinaryTreeIII
+    {
+        public class Node
+        {
+            public int val;
+            public Node left;
+            public Node right;
+            public Node parent;
+        }
+
+        public Node LowestCommonAncestor(Node p, Node q)
+        {
+            Node pi = p;
+            Node qi = q;
+
+            while (pi != qi)
+            {
+                pi = pi.parent == null ? q : pi.parent;
+                qi = qi.parent == null ? p : qi.parent;
+            }
+
+            return pi; // or return qi, as pi = qi at this point
+        }
+    }
+}
