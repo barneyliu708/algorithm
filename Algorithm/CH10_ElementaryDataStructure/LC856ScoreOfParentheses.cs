69c1837a3234ba067f21213dbcafe178473b519d
barneyliu708 <barneyliu708@gmail.com>
2022-03-10 21:31:10 -0800

    ALGORITHM-289 LC856. Score of Parentheses - stack
    stack - approach 2: https://leetcode.com/problems/score-of-parentheses/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC856ScoreOfParentheses.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC856ScoreOfParentheses
+    {
+        public int ScoreOfParentheses(string s)
+        {
+            Stack<int> stack = new Stack<int>();
+            stack.Push(0);
+
+            foreach (char ch in s)
+            {
+                if (ch == '(')
+                {
+                    stack.Push(0);
+                }
+                else
+                {
+                    int v = stack.Pop();
+                    int w = stack.Pop();
+                    stack.Push(w + Math.Max(2 * v, 1));
+                }
+            }
+
+            return stack.Pop();
+        }
+    }
+}
