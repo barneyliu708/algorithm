d64bfd77c9496c363cdba7e3fc2368b1eee65ad9
barneyliu708 <barneyliu708@gmail.com>
2022-03-10 19:51:36 -0800

    ALGORITHM-287 LC572. Subtree of Another Tree - dft
    dft - approach 1: https://www.cnblogs.com/grandyang/p/6828687.html

---
+++ Algorithm/CH10_ElementaryDataStructure/LC572SubtreeOfAnotherTree.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC572SubtreeOfAnotherTree
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+        public bool IsSubtree(TreeNode root, TreeNode subRoot)
+        {
+
+            if (root == null && subRoot == null)
+            {
+                return true;
+            }
+
+            if (root == null || subRoot == null)
+            {
+                return false;
+            }
+
+            if (IsSame(root, subRoot))
+            {
+                return true;
+            }
+
+            return IsSubtree(root.left, subRoot) || IsSubtree(root.right, subRoot);
+        }
+
+        private bool IsSame(TreeNode r1, TreeNode r2)
+        {
+
+            if (r1 == null && r2 == null)
+            {
+                return true;
+            }
+
+            if (r1 == null || r2 == null)
+            {
+                return false;
+            }
+
+            if (r1.val != r2.val)
+            {
+                return false;
+            }
+
+            return IsSame(r1.left, r2.left) && IsSame(r1.right, r2.right);
+        }
+    }
+}
