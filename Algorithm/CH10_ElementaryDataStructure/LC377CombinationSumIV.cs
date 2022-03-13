e83b3752d5d9a6956f8012005de2a4d391ac24c6
barneyliu708 <barneyliu708@gmail.com>
2022-03-12 21:02:27 -0800

    ALGORITHM-294 LC377. Combination Sum IV - dynamic programming
    dp: https://leetcode.com/problems/combination-sum-iv/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC377CombinationSumIV.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC377CombinationSumIV
+    {
+        public int CombinationSum4(int[] nums, int target)
+        {
+            Dictionary<int, int> map = new Dictionary<int, int>();
+            return Combination(nums, target, map);
+        }
+
+        private int Combination(int[] nums, int target, Dictionary<int, int> map)
+        {
+            if (target == 0)
+            {
+                return 1;
+            }
+
+            if (map.ContainsKey(target))
+            {
+                return map[target];
+            }
+
+            int ans = 0;
+            foreach (int num in nums)
+            {
+                if (target - num >= 0)
+                {
+                    ans += Combination(nums, target - num, map);
+                }
+            }
+
+            map[target] = ans;
+
+            return ans;
+        }
+    }
+}
