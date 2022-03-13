d30d69f5038972f2105844cfd84dabf9087c2a9e
barneyliu708 <barneyliu708@gmail.com>
2022-03-12 20:39:57 -0800

    ALGORITHM-293 LC1376. Time Needed to Inform All Employees - dft

---
+++ Algorithm/CH10_ElementaryDataStructure/LC1376TimeNeededToInformAllEmployees.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC1376TimeNeededToInformAllEmployees
+    {
+        private int maxTime = 0;
+
+        public int NumOfMinutes(int n, int headID, int[] manager, int[] informTime)
+        {
+            Dictionary<int, List<int>> subordinates = new Dictionary<int, List<int>>();
+            for (int i = 0; i < n; i++)
+            {
+                int m = manager[i];
+                if (!subordinates.ContainsKey(m))
+                {
+                    subordinates[m] = new List<int>();
+                }
+                subordinates[m].Add(i);
+            }
+
+            Dft(subordinates, headID, informTime, 0);
+
+            return maxTime;
+        }
+
+        private void Dft(Dictionary<int, List<int>> subordinates, int manager, int[] informTime, int curTime)
+        {
+
+            if (!subordinates.ContainsKey(manager))
+            {
+                maxTime = Math.Max(maxTime, curTime);
+                return;
+            }
+
+            curTime += informTime[manager];
+            foreach (int employee in subordinates[manager])
+            {
+                Dft(subordinates, employee, informTime, curTime);
+            }
+        }
+    }
+}
