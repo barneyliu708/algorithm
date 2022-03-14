9bc287f305920f7a0aa8adbc61fe220784ef5a0d
barneyliu708 <barneyliu708@gmail.com>
2022-03-13 21:42:04 -0700

    ALGORITHM-298 LC1091. Shortest Path in Binary Matrix - bft
    bft: https://leetcode.com/problems/shortest-path-in-binary-matrix/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC1091ShortestPathInBinaryMatrix.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC1091ShortestPathInBinaryMatrix
+    {
+        public int ShortestPathBinaryMatrix(int[][] grid)
+        {
+
+            bool[,] visited = new bool[grid.Length, grid[0].Length];
+            Queue<(int row, int col)> queue = new Queue<(int row, int col)>();
+
+            queue.Enqueue((0, 0));
+
+            int path = 0;
+            while (queue.Count > 0)
+            {
+                path++;
+                int count = queue.Count;
+                for (int i = 0; i < count; i++)
+                {
+                    (int row, int col) cur = queue.Dequeue();
+
+                    if (visited[cur.row, cur.col] || grid[cur.row][cur.col] == 1)
+                    {
+                        continue;
+                    }
+
+                    visited[cur.row, cur.col] = true;
+                    if (cur.row == grid.Length - 1 && cur.col == grid[0].Length - 1)
+                    {
+                        return path;
+                    }
+
+                    foreach (int r in new int[] { cur.row - 1, cur.row, cur.row + 1 })
+                    {
+                        foreach (int c in new int[] { cur.col - 1, cur.col, cur.col + 1 })
+                        {
+                            if (r >= 0 && r < grid.Length &&
+                                c >= 0 && c < grid[0].Length &&
+                                !visited[r, c] && grid[r][c] == 0)
+                            {
+                                queue.Enqueue((r, c));
+                            }
+                        }
+                    }
+                }
+            }
+            return -1;
+        }
+    }
+}
