3778c147bc3c152e6ebddc8671ee72278aa2c10a
barneyliu708 <barneyliu708@gmail.com>
2022-03-14 21:02:45 -0700

    ALGORITHM-301 LC206. Reverse Linked List - Recursion
    Recursion - approach 2: https://leetcode.com/problems/reverse-linked-list/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC206ReverseLinkedList.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC206ReverseLinkedList
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+        public ListNode ReverseList(ListNode head)
+        {
+
+            if (head == null || head.next == null)
+            {
+                return head;
+            }
+
+            // ListNode headnext = head.next;
+            // head.next = null; // break the chain to avoid infinite loop
+
+            // ListNode reversedNext = ReverseList(headnext);
+
+            // ListNode cur = reversedNext;
+            // while (cur.next != null) {
+            //     cur = cur.next;
+            // }
+            // cur.next = head;
+
+            ListNode reversedNext = ReverseList(head.next);
+            head.next.next = head;
+            head.next = null;
+
+            return reversedNext;
+        }
+    }
+}
