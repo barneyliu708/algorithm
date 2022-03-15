cd81ac74097665ac7c9847873e375cdfc7a4f769
barneyliu708 <barneyliu708@gmail.com>
2022-03-14 20:31:04 -0700

    ALGORITHM-300 LC669. Trim a Binary Search Tree - Recursion
    Recursion solution: https://leetcode.com/problems/trim-a-binary-search-tree/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC669TrimABinarySearchTree.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC669TrimABinarySearchTree
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+        public TreeNode TrimBST(TreeNode root, int low, int high)
+        {
+
+            if (root == null)
+            {
+                return null;
+            }
+
+            if (root.val >= low && root.val <= high)
+            {
+                root.left = TrimBST(root.left, low, high);
+                root.right = TrimBST(root.right, low, high);
+                return root;
+            }
+
+            if (root.val < low)
+            {
+                return TrimBST(root.right, low, high);
+            }
+
+            if (root.val > high)
+            {
+                return TrimBST(root.left, low, high);
+            }
+
+            return null;
+        }
+    }
+}
