3e37c4e63bc1a90e7b21f94e54031e2e4f3f0f12
barneyliu708 <barneyliu708@gmail.com>
2022-03-14 21:27:21 -0700

    ALGORITHM-302 LC876. Middle of the Linked List - 2 pointers
    2 pointers - approach 2: https://leetcode.com/problems/middle-of-the-linked-list/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC876MiddleOfTheLinkedList.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC876MiddleOfTheLinkedList
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+        public ListNode MiddleNode(ListNode head)
+        {
+            ListNode dummy = new ListNode();
+            dummy.next = head;
+            ListNode f = dummy;
+            ListNode s = dummy;
+
+            while (f.next != null && f.next.next != null)
+            {
+                f = f.next.next;
+                s = s.next;
+            }
+
+            return s.next;
+        }
+    }
+}
