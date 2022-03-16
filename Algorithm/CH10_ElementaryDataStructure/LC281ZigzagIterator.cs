3ce80e446e3ae8e15dfff4851430d1f20cca4735
barneyliu708 <barneyliu708@gmail.com>
2022-03-15 21:27:52 -0700

    ALGORITHM-305 LC281. Zigzag Iterator - queue
    queue: https://leetcode.com/problems/zigzag-iterator/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC281ZigzagIterator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC281ZigzagIterator
+    {
+        public class ZigzagIterator
+        {
+
+            private int i;
+            private IList<int> v;
+
+            public ZigzagIterator(IList<int> v1, IList<int> v2)
+            {
+                v = new List<int>();
+                int p1 = 0;
+                int p2 = 0;
+                while (p1 < v1.Count && p2 < v2.Count)
+                {
+                    v.Add(v1[p1++]);
+                    v.Add(v2[p2++]);
+                }
+
+                while (p1 < v1.Count)
+                {
+                    v.Add(v1[p1++]);
+                }
+
+                while (p2 < v2.Count)
+                {
+                    v.Add(v2[p2++]);
+                }
+            }
+
+            public bool HasNext()
+            {
+                return i < v.Count;
+            }
+
+            public int Next()
+            {
+                return v[i++];
+            }
+        }
+    }
+}
