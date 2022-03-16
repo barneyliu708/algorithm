651ed5544ef5073c9a8302c1328d486f0c8be8f3
barneyliu708 <barneyliu708@gmail.com>
2022-03-15 20:55:07 -0700

    ALGORITHM-304 LC346. Moving Average from Data Stream - queue
    queue - approach 2: https://leetcode.com/problems/moving-average-from-data-stream/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC346MovingAverageFromDataStream.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC346MovingAverageFromDataStream
+    {
+        public class MovingAverage
+        {
+
+            private Queue<int> queue;
+            private int size;
+            private int sum;
+
+            public MovingAverage(int size)
+            {
+                this.size = size;
+                queue = new Queue<int>();
+            }
+
+            public double Next(int val)
+            {
+
+                queue.Enqueue(val);
+                sum += val;
+                if (queue.Count > size)
+                {
+                    sum -= queue.Dequeue();
+                }
+
+                return (double)sum / queue.Count;
+            }
+        }
+    }
+}
