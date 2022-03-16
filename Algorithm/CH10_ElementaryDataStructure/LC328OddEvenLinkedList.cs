7670f71a827e927d2a95a80ce30de45e5105409e
barneyliu708 <barneyliu708@gmail.com>
2022-03-15 20:25:58 -0700

    ALGORITHM-303 LC328. Odd Even Linked List
    Solution: https://leetcode.com/problems/odd-even-linked-list/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC328OddEvenLinkedList.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC328OddEvenLinkedList
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+        public ListNode OddEvenList(ListNode head)
+        {
+            ListNode oddDummy = new ListNode();
+            ListNode evenDummy = new ListNode();
+
+            int count = 1;
+            ListNode o = oddDummy;
+            ListNode e = evenDummy;
+            while (head != null)
+            {
+                if (count % 2 == 1)
+                { // odd
+                    o.next = head;
+                    o = o.next;
+                }
+                else
+                { // even
+                    e.next = head;
+                    e = e.next;
+                }
+                head = head.next;
+                count++;
+            }
+
+            o.next = evenDummy.next;
+            e.next = null;
+
+            return oddDummy.next;
+        }
+    }
+}
