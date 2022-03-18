fe2ffa0746c8ca6b5d843e4b611aac4a0d99f860
barneyliu708 <barneyliu708@gmail.com>
2022-03-17 18:39:19 -0700

    ALGORITHM-310 LC1472. Design Browser History - stack

---
+++ Algorithm/CH10_ElementaryDataStructure/LC1472DesignBrowserHistory.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC1472DesignBrowserHistory
+    {
+        public class BrowserHistory
+        {
+
+            private List<string> histories;
+            private int curi;
+            private int length;
+
+            public BrowserHistory(string homepage)
+            {
+                histories = new List<string>();
+                histories.Add(homepage);
+                curi = 0;
+                length = histories.Count;
+            }
+
+            public void Visit(string url)
+            {
+                curi++;
+                if (curi == histories.Count)
+                {
+                    histories.Add(url);
+                    length = histories.Count;
+                }
+                else
+                {
+                    histories[curi] = url;
+                    length = curi + 1; // if there is a new visit, all forward histories need to be moved
+                }
+            }
+
+            public string Back(int steps)
+            {
+                curi = Math.Max(0, curi - steps);
+                return histories[curi];
+            }
+
+            public string Forward(int steps)
+            {
+                curi = Math.Min(length - 1, curi + steps);
+                return histories[curi];
+            }
+        }
+    }
+}
