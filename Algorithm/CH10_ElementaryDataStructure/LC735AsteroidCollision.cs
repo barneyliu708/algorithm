e4228260e4f6061095e08a7999a78d61981898a1
barneyliu708 <barneyliu708@gmail.com>
2022-03-17 19:43:01 -0700

    ALGORITHM-312 LC735. Asteroid Collision - stack
    stack: https://leetcode.com/problems/asteroid-collision/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC735AsteroidCollision.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC735AsteroidCollision
+    {
+        public int[] AsteroidCollision(int[] asteroids)
+        {
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < asteroids.Length; i++)
+            {
+                int asteroid = asteroids[i];
+
+                if (stack.Count == 0)
+                {
+                    stack.Push(asteroid);
+                    continue;
+                }
+
+                // trigger the collision
+                while (stack.Count > 0 && stack.Peek() > 0 && asteroid < 0)
+                {
+                    int left = stack.Pop();
+                    if (left > -asteroid)
+                    {
+                        asteroid = left; // this will stop the while loop
+                    }
+                    else if (left < -asteroid)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        asteroid = 0; // 0 means both asteroids are removed and will stop the while loop
+                    }
+                }
+                if (asteroid != 0)
+                {
+                    stack.Push(asteroid);
+                }
+            }
+
+            int[] ans = new int[stack.Count];
+            for (int i = ans.Length - 1; i >= 0; i--)
+            {
+                ans[i] = stack.Pop();
+            }
+
+            return ans;
+        }
+    }
+}
