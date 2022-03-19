1f573a1c3bbf60a7a3654a5c497b2178743e1276
barneyliu708 <barneyliu708@gmail.com>
2022-03-18 21:53:00 -0700

    ALGORITHM-317 LC378. Kth Smallest Element in a Sorted Matrix - priority queue
    priority queue: https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC378KthSmallestElementInASortedMatrix.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC378KthSmallestElementInASortedMatrix
+    {
+        public int KthSmallest(int[][] matrix, int k)
+        {
+            PriorityQueue<int, int> pq = new PriorityQueue<int, int>();
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                for (int j = 0; j < matrix[0].Length; j++)
+                {
+                    pq.Enqueue(matrix[i][j], -matrix[i][j]);
+                    if (pq.Count > k)
+                    {
+                        pq.Dequeue();
+                    }
+                }
+            }
+            return pq.Dequeue();
+        }
+    }
+}
