841c44e3c8090b5858aa4a24aa0498fec56788f4
barneyliu708 <barneyliu708@gmail.com>
2022-03-18 20:47:32 -0700

    ALGORITHM-314 LC973. K Closest Points to Origin - priority queue
    priority queue: https://leetcode.com/problems/k-closest-points-to-origin/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC973KClosestPointsToOrigin.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC973KClosestPointsToOrigin
+    {
+        public int[][] KClosest(int[][] points, int k)
+        {
+
+            PriorityQueue<int[], int> pq = new PriorityQueue<int[], int>();
+            foreach (int[] point in points)
+            {
+                pq.Enqueue(point, -point[0] * point[0] - point[1] * point[1]);
+                if (pq.Count > k)
+                {
+                    pq.Dequeue();
+                }
+            }
+
+            int[][] ans = new int[k][];
+            for (int i = 0; i < k; i++)
+            {
+                ans[i] = pq.Dequeue();
+            }
+
+            return ans;
+        }
+    }
+}
