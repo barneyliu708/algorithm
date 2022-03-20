4f3833d3d78180c8b87bd5bf58a7106ddcf1c305
barneyliu708 <barneyliu708@gmail.com>
2022-03-19 20:38:26 -0700

    ALGORITHM-318 LC295. Find Median from Data Stream - priority queue
    priority queue - approach 3: https://leetcode.com/problems/find-median-from-data-stream/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC295FindMedianFromDataStream.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC295FindMedianFromDataStream
+    {
+        public class MedianFinder
+        {
+
+            private PriorityQueue<int, int> maxheap;
+            private PriorityQueue<int, int> minheap;
+
+            public MedianFinder()
+            {
+                maxheap = new PriorityQueue<int, int>();
+                minheap = new PriorityQueue<int, int>();
+            }
+
+            public void AddNum(int num)
+            {
+                maxheap.Enqueue(num, -num);
+
+                // rebalancing
+                minheap.Enqueue(maxheap.Peek(), maxheap.Peek());
+                maxheap.Dequeue();
+
+                if (maxheap.Count < minheap.Count)
+                {
+                    maxheap.Enqueue(minheap.Peek(), -minheap.Peek());
+                    minheap.Dequeue();
+                }
+            }
+
+            public double FindMedian()
+            {
+                return maxheap.Count > minheap.Count ? maxheap.Peek() : ((double)maxheap.Peek() + (double)minheap.Peek()) / 2;
+            }
+        }
+    }
+}
