ccd5406103259e102e8d62fab7345a5b2ce8914b
barneyliu708 <barneyliu708@gmail.com>
2022-03-19 22:36:13 -0700

    ALGORITHM-320 LC528. Random Pick with Weight - binary search
    solution: https://leetcode.com/problems/random-pick-with-weight/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC528RandomPickWithWeight.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC528RandomPickWithWeight
+    {
+        public class Solution
+        {
+
+            private int[] presum;
+            private int total;
+            private Random rand = new Random();
+
+            public Solution(int[] w)
+            {
+                presum = new int[w.Length];
+
+                int pre = 0;
+                for (int i = 0; i < w.Length; i++)
+                {
+                    pre += w[i];
+                    presum[i] = pre;
+                }
+                total = pre;
+            }
+
+            public int PickIndex()
+            {
+                double target = total * rand.NextDouble();
+                int l = 0;
+                int r = presum.Length;
+                while (l < r)
+                {
+                    int mid = l + (r - l) / 2;
+                    if (target > presum[mid])
+                    {
+                        l = mid + 1;
+                    }
+                    else
+                    {
+                        r = mid;
+                    }
+                }
+                return l;
+            }
+        }
+    }
+}
