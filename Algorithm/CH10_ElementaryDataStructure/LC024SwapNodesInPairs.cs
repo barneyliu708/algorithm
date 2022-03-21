ecc6490f658ff33630db9685ad71d9b93da334c8
barneyliu708 <barneyliu708@gmail.com>
2022-03-20 21:26:07 -0700

    ALGORITHM-323 LC024. Swap Nodes in Pairs - Linked List
    solution: https://leetcode.com/problems/swap-nodes-in-pairs/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC024SwapNodesInPairs.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC024SwapNodesInPairs
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+        public ListNode SwapPairs(ListNode head)
+        {
+            ListNode dummy = new ListNode();
+            dummy.next = head;
+
+            ListNode pre = dummy;
+            ListNode cur = head;
+            while (cur != null && cur.next != null)
+            {
+                ListNode next = cur.next;
+
+                // swap
+                cur.next = next.next;
+                pre.next = next;
+                next.next = cur;
+
+                // move forward for the next iteration
+                pre = cur;
+                cur = cur.next;
+            }
+
+            return dummy.next;
+        }
+    }
+}
