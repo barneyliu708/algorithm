f7db4d360ccd286e89c8df7cae26e2f0c97fef83
barneyliu708 <barneyliu708@gmail.com>
2022-03-20 20:48:31 -0700

    ALGORITHM-321 LC669. Trim a Binary Search Tree - 2 pointers
    2 pointers: https://leetcode.com/problems/longest-continuous-increasing-subsequence/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC674LongestContinuousIncreasingSubsequence.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC674LongestContinuousIncreasingSubsequence
+    {
+        public int FindLengthOfLCIS(int[] nums)
+        {
+
+            if (nums.Length < 2)
+            {
+                return nums.Length;
+            }
+
+            int ans = 1;
+            int l = 0;
+            int r = 1;
+            while (r < nums.Length)
+            {
+                if (nums[r] > nums[r - 1])
+                {
+                    ans = Math.Max(ans, r - l + 1);
+                }
+                else
+                {
+                    l = r;
+                }
+                r++;
+            }
+
+            return ans;
+        }
+    }
+}
