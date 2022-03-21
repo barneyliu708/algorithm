82d35ed2d2b6c24a715ebd0c91ed9c150ff10de2
barneyliu708 <barneyliu708@gmail.com>
2022-03-20 21:12:32 -0700

    ALGORITHM-322 LC007. Reverse Integer
    Math: https://leetcode.com/problems/reverse-integer/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC007ReverseInteger.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC007ReverseInteger
+    {
+        public int Reverse(int x)
+        {
+
+            if (x < 0)
+            {
+                if (x == int.MinValue)
+                {
+                    return 0;
+                }
+                return -1 * Reverse(-x);
+            }
+
+            int ans = 0;
+            while (x > 0)
+            {
+                int lastdigit = x % 10;
+                if (ans > (int.MaxValue - lastdigit) / 10)
+                {
+                    return 0;
+                }
+                ans = ans * 10 + lastdigit;
+                x = x / 10;
+            }
+            return ans;
+        }
+    }
+}
