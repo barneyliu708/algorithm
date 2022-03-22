e046605d04580f120dc125a134ef63aa4bc234fc
barneyliu708 <barneyliu708@gmail.com>
2022-03-21 21:53:36 -0700

    ALGORITHM-188 LC033. Search in Rotated Sorted Array
    second trial

--- Algorithm/CH10_ElementaryDataStructure/LC033SearchInRotatedSortedArray.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC033SearchInRotatedSortedArray.cs
@@ -43,5 +43,46 @@ namespace Algorithm.CH10_ElementaryDataStructure
 
             return -1;
         }
+
+        class SecondTrial
+        {
+            public int Search(int[] nums, int target)
+            {
+                int l = 0;
+                int r = nums.Length - 1;
+                while (l <= r)
+                {
+                    int mid = l + (r - l) / 2;
+                    if (nums[mid] == target)
+                    {
+                        return mid;
+                    }
+                    if (nums[mid] >= nums[l])
+                    {
+                        if (target >= nums[l] && target < nums[mid])
+                        {
+                            r = mid - 1;
+                        }
+                        else
+                        {
+                            l = mid + 1;
+                        }
+                    }
+                    else
+                    {
+                        if (target <= nums[r] && target > nums[mid])
+                        {
+                            l = mid + 1;
+                        }
+                        else
+                        {
+                            r = mid - 1;
+                        }
+                    }
+                }
+
+                return -1;
+            }
+        }
     }
 }
