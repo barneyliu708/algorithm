feec5acd8e5e420b8715fe8e01a05dd16aa878f6
barneyliu708 <barneyliu708@gmail.com>
2022-03-21 22:23:53 -0700

    ALGORITHM-193 LC081. Search in Rotated Sorted Array II
    second trail:
    worst case O(n) + average case O(logn)

--- Algorithm/CH10_ElementaryDataStructure/LC081SearchInRotatedSortedArrayII.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC081SearchInRotatedSortedArrayII.cs
@@ -50,5 +50,62 @@ namespace Algorithm.CH10_ElementaryDataStructure
 
             return false;
         }
+
+        class SecondTrial
+        {
+            // the only real scenario we should look out for is when we're given an array
+            // where the first element == last element like in the following: 2, 5, 6, 0, 0, 1, 2 .
+            // Then we can just shrink our array on both sides, the left and the right, so that we could just perform the regular binary search on a rotated array algorithm.
+            // It would give us this array: 5, 6, 0, 0, 1 to work with.
+            public bool Search(int[] nums, int target)
+            {
+
+                int l = 0;
+                int r = nums.Length - 1;
+
+                while (l <= r && nums[l] == nums[r])
+                {
+                    if (nums[l] == target)
+                    {
+                        return true;
+                    }
+                    l++;
+                    r--;
+                }
+
+                while (l <= r)
+                {
+                    int mid = l + (r - l) / 2;
+                    if (nums[mid] == target)
+                    {
+                        return true;
+                    }
+                    if (nums[mid] >= nums[l])
+                    {
+                        if (target >= nums[l] && target < nums[mid])
+                        {
+                            r = mid - 1;
+                        }
+                        else
+                        {
+                            l = mid + 1;
+                        }
+                    }
+                    else
+                    {
+                        if (target <= nums[r] && target > nums[mid])
+                        {
+                            l = mid + 1;
+                        }
+                        else
+                        {
+                            r = mid - 1;
+                        }
+                    }
+                }
+
+                return false;
+            }
+        }
     }
 }
