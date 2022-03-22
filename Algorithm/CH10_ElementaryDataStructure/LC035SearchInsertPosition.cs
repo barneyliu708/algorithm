fd989f638956985b7149bf2e022c8a2eaba67bbb
barneyliu708 <barneyliu708@gmail.com>
2022-03-21 21:28:08 -0700

    ALGORITHM-190 LC035. Search Insert Position
    second trial with more standard approach

--- Algorithm/CH10_ElementaryDataStructure/LC035SearchInsertPosition.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC035SearchInsertPosition.cs
@@ -28,5 +28,32 @@ namespace Algorithm.CH10_ElementaryDataStructure
 
             return start;
         }
+
+        public class SecondTrial
+        {
+            public int SearchInsert(int[] nums, int target)
+            {
+
+                int l = 0;
+                int r = nums.Length - 1;
+                while (l <= r)
+                {
+                    int mid = l + (r - l) / 2;
+                    if (nums[mid] == target)
+                    {
+                        return mid;
+                    }
+                    if (nums[mid] > target)
+                    {
+                        r = mid - 1;
+                    }
+                    else
+                    {
+                        l = mid + 1;
+                    }
+                }
+                return l;
+            }
+        }
     }
 }
