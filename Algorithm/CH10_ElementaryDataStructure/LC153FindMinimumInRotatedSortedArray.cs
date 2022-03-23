57670e29b1471f72dfafd66a824134bb12372d61
barneyliu708 <barneyliu708@gmail.com>
2022-03-22 21:10:30 -0700

    ALGORITHM-194 LC153. Find Minimum in Rotated Sorted Array - second trail

--- Algorithm/CH10_ElementaryDataStructure/LC153FindMinimumInRotatedSortedArray.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC153FindMinimumInRotatedSortedArray.cs
@@ -32,5 +32,32 @@ namespace Algorithm.CH10_ElementaryDataStructure
 
             return nums[start % nums.Length];
         }
+
+        public class SecondTrial
+        {
+            public int FindMin(int[] nums)
+            {
+                int l = 0;
+                int r = nums.Length - 1;
+                while (l <= r)
+                {
+                    int mid = l + (r - l) / 2;
+                    if (nums[mid] > nums[(mid + 1) % nums.Length])
+                    {
+                        return nums[(mid + 1) % nums.Length];
+                    }
+                    if (nums[mid] < nums[l])
+                    {
+                        r = mid - 1;
+                    }
+                    else
+                    {
+                        l = mid + 1;
+                    }
+                }
+
+                return nums[l % nums.Length]; // this is to handle the case where nums.Length == 1
+            }
+        }
     }
 }
