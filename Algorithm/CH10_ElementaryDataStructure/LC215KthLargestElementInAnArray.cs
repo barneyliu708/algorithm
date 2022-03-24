89ed46cce7767bf09155b85888ed0843b6831d1f
barneyliu708 <barneyliu708@gmail.com>
2022-03-23 21:50:12 -0700

    ALGORITHM-23 LC215 Kth Largest Element in an Array - quick select
    quick select - divide and conquer: https://leetcode.com/problems/kth-largest-element-in-an-array/solution/

--- Algorithm/CH10_ElementaryDataStructure/LC215KthLargestElementInAnArray.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC215KthLargestElementInAnArray.cs
@@ -24,5 +24,66 @@ namespace Algorithm.CH10_ElementaryDataStructure
 
             return queue.Dequeue();
         }
+
+        public class QuickSelectApproach
+        {
+            public int FindKthLargest(int[] nums, int k)
+            {
+                return QuickSelect(nums, 0, nums.Length - 1, nums.Length - k);
+            }
+
+            private void Swap(int[] nums, int l, int r)
+            {
+                int temp = nums[l];
+                nums[l] = nums[r];
+                nums[r] = temp;
+            }
+
+            private int Partition(int[] nums, int l, int r, int pi)
+            {
+                int pivot = nums[pi];
+
+                // move pivot to the end
+                Swap(nums, pi, r);
+
+                // move all smaller elements to the left
+                int inserti = l;
+                for (int i = l; i <= r; i++)
+                {
+                    if (nums[i] < pivot)
+                    {
+                        Swap(nums, inserti, i);
+                        inserti++;
+                    }
+                }
+
+                // move pivot to its final place
+                Swap(nums, inserti, r);
+
+                return inserti;
+            }
+
+            private int QuickSelect(int[] nums, int l, int r, int kSmallest)
+            {
+                if (l == r)
+                {
+                    return nums[l];
+                }
+
+                Random rand = new Random();
+                int pi = l + rand.Next(r - l);
+                pi = Partition(nums, l, r, pi);
+
+                if (kSmallest == pi)
+                {
+                    return nums[kSmallest];
+                }
+                if (kSmallest < pi)
+                {
+                    return QuickSelect(nums, l, pi - 1, kSmallest);
+                }
+                return QuickSelect(nums, pi + 1, r, kSmallest);
+            }
+        }
     }
 }
