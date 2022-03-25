97b104c120843b2ababae7d40d6a35622c486d6c
barneyliu708 <barneyliu708@gmail.com>
2022-03-24 20:26:27 -0700

    ALGORITHM-325 LC1060. Missing Element in Sorted Array - binary search
    binary search - approach 2: https://leetcode.com/problems/missing-element-in-sorted-array/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC1060MissingElementInSortedArray.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC1060MissingElementInSortedArray
+    {
+        public int MissingElement(int[] nums, int k)
+        {
+            int l = 0;
+            int r = nums.Length - 1;
+            while (l <= r)
+            {
+                int mid = l + (r - l) / 2;
+                int count = MissingCount(nums, mid);
+
+                // need to find the leftmost potential index of target
+                if (count == k) 
+                { 
+                    r = mid - 1;
+                }
+                else if (count > k)
+                {
+                    r = mid - 1;
+                }
+                else
+                {
+                    l = mid + 1;
+                }
+            }
+            return nums[l - 1] + k - MissingCount(nums, l - 1);
+        }
+
+        private int MissingCount(int[] nums, int i)
+        {
+            return nums[i] - nums[0] - i;
+        }
+    }
+}
