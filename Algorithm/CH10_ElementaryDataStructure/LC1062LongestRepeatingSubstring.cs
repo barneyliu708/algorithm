25fe56faf6ebc0fe5e70eab3a66e8afebb991902
barneyliu708 <barneyliu708@gmail.com>
2022-03-24 21:07:03 -0700

    ALGORITHM-326 LC1062. Longest Repeating Substring - binary search + hashset scan
    binary search + hashset scan - approach 1: https://leetcode.com/problems/longest-repeating-substring/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC1062LongestRepeatingSubstring.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC1062LongestRepeatingSubstring
+    {
+        class Approach_1
+        {
+            public int LongestRepeatingSubstring(string s)
+            {
+                // if there is a duplicate substring of length k, that means there is a duplicate substring of k - 1
+                // can use binary search to find the max length that has a duplicate substring
+                int l = 1;
+                int r = s.Length - 1;
+                while (l <= r)
+                {
+                    int mid = l + (r - l) / 2;
+                    if (HasDuplicate(s, mid))
+                    {
+                        l = mid + 1;
+                    }
+                    else
+                    {
+                        r = mid - 1;
+                    }
+                }
+                return r;
+            }
+
+            private bool HasDuplicate(string s, int length)
+            {
+                HashSet<string> hashset = new HashSet<string>();
+                for (int i = 0; i < s.Length - length + 1; i++)
+                {
+                    string substr = s.Substring(i, length);
+                    if (hashset.Contains(substr))
+                    {
+                        return true;
+                    }
+                    hashset.Add(substr);
+                }
+                return false;
+            }
+        }
+    }
+}
