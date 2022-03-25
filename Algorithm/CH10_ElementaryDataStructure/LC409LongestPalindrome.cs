1bb29a83e06dfe0d97a7c100bce1cefc4154229b
barneyliu708 <barneyliu708@gmail.com>
2022-03-25 16:25:21 -0700

    ALGORITHM-328 LC409. Longest Palindrome - hash table
    hash table: https://leetcode.com/problems/longest-palindrome/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC409LongestPalindrome.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC409LongestPalindrome
+    {
+        public int LongestPalindrome(string s)
+        {
+            Dictionary<char, int> map = new Dictionary<char, int>();
+            foreach (char ch in s)
+            {
+                if (!map.ContainsKey(ch))
+                {
+                    map[ch] = 0;
+                }
+                map[ch]++;
+            }
+            int oddcount = 0;
+            foreach (char ch in map.Keys)
+            {
+                if (map[ch] % 2 == 1)
+                {
+                    oddcount++;
+                }
+            }
+            // if there is odd count, we should only allow at most one character with odd count
+            return oddcount > 0 ? s.Length - oddcount + 1 : s.Length;
+        }
+    }
+}
