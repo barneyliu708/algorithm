b6ee62ba1d77ad6fef466e9b47bc38c0ed66bf8f
barneyliu708 <barneyliu708@gmail.com>
2022-03-25 15:59:22 -0700

    ALGORITHM-327 LC1891. Cutting Ribbons - binary search
    binary search: https://leetcode.com/problems/cutting-ribbons/discuss/1263437/Java-Simple-Binary-Search-%2B-Explanation

---
+++ Algorithm/CH10_ElementaryDataStructure/LC1891CuttingRibbons.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC1891CuttingRibbons
+    {
+        public int MaxLength(int[] ribbons, int k)
+        {
+            int l = 1;
+            int r = int.MaxValue;
+            while (l <= r)
+            {
+                int mid = l + (r - l) / 2;
+                if (IsCutPossible(ribbons, mid, k))
+                {
+                    l = mid + 1;
+                }
+                else
+                {
+                    r = mid - 1;
+                }
+            }
+
+            return r;
+        }
+
+        private bool IsCutPossible(int[] ribbons, int length, int k)
+        {
+            int count = 0;
+            foreach (int ribbon in ribbons)
+            {
+                count += ribbon / length;
+            }
+
+            return count >= k;
+        }
+    }
+}
