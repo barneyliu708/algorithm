871f965cc5bb0d8f4f11d6c875608414b08d2ec0
barneyliu708 <barneyliu708@gmail.com>
2022-03-25 20:09:52 -0700

    ALGORITHM-329 LC454. 4Sum II - hash table + 2 pointers
    hash table + 2 pointers - approach 1: https://leetcode.com/problems/4sum-ii/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC454_4SumII.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC454_4SumII
+    {
+        public int FourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4)
+        {
+            int count = 0;
+            Dictionary<int, int> map = new Dictionary<int, int>();
+            foreach (int n1 in nums1)
+            {
+                foreach (int n2 in nums2)
+                {
+                    if (!map.ContainsKey(n1 + n2))
+                    {
+                        map[n1 + n2] = 0;
+                    }
+                    map[n1 + n2]++;
+                }
+            }
+            foreach (int n3 in nums3)
+            {
+                foreach (int n4 in nums4)
+                {
+                    if (map.ContainsKey(-(n3 + n4)))
+                    {
+                        count += map[-(n3 + n4)];
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}
