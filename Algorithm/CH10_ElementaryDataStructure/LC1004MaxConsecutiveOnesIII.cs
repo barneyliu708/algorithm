209c437204c80953ab9e43c81040bf06846970b6
barneyliu708 <barneyliu708@gmail.com>
2022-03-26 22:28:19 -0700

    ALGORITHM-331 LC1004. Max Consecutive Ones III - sliding window
    sliding window: https://leetcode.com/problems/max-consecutive-ones-iii/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC1004MaxConsecutiveOnesIII.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC1004MaxConsecutiveOnesIII
+    {
+        public int LongestOnes(int[] nums, int k)
+        {
+            int l = 0;
+            int r = 0;
+            int ans = 0;
+            while (r < nums.Length)
+            {
+                if (nums[r] == 0)
+                {
+                    k--;
+                }
+
+                while (k < 0)
+                {
+                    if (nums[l] == 0)
+                    {
+                        k++;
+                    }
+                    l++;
+                }
+
+                ans = Math.Max(ans, r - l + 1);
+
+                r++;
+            }
+            return ans;
+        }
+    }
+}
