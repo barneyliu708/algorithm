99623e15690b9ad085677ef14107ea4eb362536e
barneyliu708 <barneyliu708@gmail.com>
2022-03-26 22:30:03 -0700

    ALGORITHM-331 LC1004. Max Consecutive Ones III - optimized sliding window approach
    sliding window - optimized approach: https://leetcode.com/problems/max-consecutive-ones-iii/solution/

--- Algorithm/CH10_ElementaryDataStructure/LC1004MaxConsecutiveOnesIII.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC1004MaxConsecutiveOnesIII.cs
@@ -35,5 +35,34 @@ namespace Algorithm.CH10_ElementaryDataStructure
             }
             return ans;
         }
+
+        public class OptimizedApproach2
+        {
+            public int LongestOnes(int[] nums, int k)
+            {
+                int l = 0;
+                int r = 0;
+                int ans = 0;
+                while (r < nums.Length)
+                {
+                    if (nums[r] == 0)
+                    {
+                        k--;
+                    }
+
+                    if (k < 0)
+                    {
+                        if (nums[l] == 0)
+                        {
+                            k++;
+                        }
+                        l++;
+                    }
+
+                    r++;
+                }
+                return r - l;
+            }
+        }
     }
 }
