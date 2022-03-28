b114acf82a9e21e7d07e11cd63f6a58d3c415d6c
barneyliu708 <barneyliu708@gmail.com>
2022-03-27 21:19:11 -0700

    ALGORITHM-197 LC209. Minimum Size Subarray Sum
    standard sliding window apprach

--- Algorithm/CH10_ElementaryDataStructure/LC209MinimumSizeSubarraySum.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC209MinimumSizeSubarraySum.cs
@@ -38,5 +38,63 @@ namespace Algorithm.CH10_ElementaryDataStructure
 
             return minlen == int.MaxValue ? 0 : minlen;
         }
+
+        public class StandardSlidingWindowApproach
+        {
+            public int MinSubArrayLen(int target, int[] nums)
+            {
+                int[] presum = new int[nums.Length + 1];
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    presum[i + 1] = presum[i] + nums[i];
+                }
+
+                int l = 0;
+                int r = 0;
+                int ans = int.MaxValue;
+                while (r < nums.Length)
+                {
+                    // get current state of the sliding window
+                    int sum = presum[r] + nums[r] - presum[l]; // or int sum = presum[r + 1] - presum[l];
+
+                    // chech if the current substring satisfy the target condition
+                    while (sum >= target)
+                    {
+                        ans = Math.Min(ans, r - l + 1);
+                        //l++;
+                        //sum = presum[r] + nums[r] - presum[l];
+                        sum -= nums[l++];
+                    }
+
+                    r++;
+                }
+
+                return ans == int.MaxValue ? 0 : ans;
+            }
+
+            public int MinSubArrayLen2(int target, int[] nums)
+            {
+                int sum = 0;
+                int l = 0;
+                int r = 0;
+                int ans = int.MaxValue;
+                while (r < nums.Length)
+                {
+                    // get current state of the sliding window
+                    sum += nums[r];
+
+                    // chech if the current substring sati
+                    while (sum >= target)
+                    {
+                        ans = Math.Min(ans, r - l + 1);
+                        sum -= nums[l++];
+                    }
+
+                    r++;
+                }
+
+                return ans == int.MaxValue ? 0 : ans;
+            }
+        }
     }
 }
