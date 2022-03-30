b5b337b9dcdbbe626f07cede662d9ff2450c64ae
barneyliu708 <barneyliu708@gmail.com>
2022-03-29 21:40:01 -0700

    ALGORITHM-185 LC015. 3Sum - 2nd done

--- Algorithm/CH10_ElementaryDataStructure/LC015_3Sum.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC015_3Sum.cs
@@ -39,5 +39,43 @@ namespace Algorithm.CH10_ElementaryDataStructure
                 hashset.Add(nums[j]);
             }
         }
+
+        public class SecondDone
+        {
+            public IList<IList<int>> ThreeSum(int[] nums)
+            {
+                List<IList<int>> ans = new List<IList<int>>();
+                Array.Sort(nums);
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    if (i != 0 && nums[i] == nums[i - 1]) // avoid duplicate triple
+                    { // to avoid duplicate
+                        continue;
+                    }
+                    TwoSum(nums, i, ans);
+                }
+
+                return ans;
+            }
+
+            private void TwoSum(int[] nums, int i, List<IList<int>> ans)
+            {
+                Dictionary<int, int> map = new Dictionary<int, int>(); // num - indice of num
+                int target = 0 - nums[i];
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    int num = nums[j];
+                    if (map.ContainsKey(target - num))
+                    {
+                        ans.Add(new List<int> { nums[i], nums[map[target - num]], nums[j] });
+                        while (j + 1 < nums.Length && nums[j + 1] == nums[j]) // avoid duplicate pair
+                        {
+                            j++;
+                        }
+                    }
+                    map[num] = j;
+                }
+            }
+        }
     }
 }
