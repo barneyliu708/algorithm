5416ac86223ec795b8b8ecc3e604a4f088650f42
barneyliu708 <barneyliu708@gmail.com>
2022-03-30 22:59:39 -0700

    ALGORITHM-235 LC424. Longest Repeating Character Replacement - standard sliding window template

--- Algorithm/CH10_ElementaryDataStructure/LC424LongestRepeatingCharacterReplacement.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC424LongestRepeatingCharacterReplacement.cs
@@ -29,5 +29,43 @@ namespace Algorithm.CH10_ElementaryDataStructure
             }
             return ans;
         }
+
+        public class StandardSlidingWindow
+        {
+            public int CharacterReplacement(string s, int k)
+            {
+                Dictionary<char, int> map = new Dictionary<char, int>();
+                int l = 0;
+                int r = 0;
+                int mcount = 0; // the majority count of substring
+                int ans = 0;
+                while (r < s.Length)
+                {
+                    char rch = s[r];
+                    if (!map.ContainsKey(rch))
+                    {
+                        map[rch] = 0;
+                    }
+                    map[rch]++;
+
+                    // update the count of the majority character in the substring
+                    mcount = Math.Max(mcount, map[rch]);
+
+                    // shrink the window if the condition is not satisfied
+                    while (r - l + 1 - mcount > k)
+                    {
+                        char lch = s[l];
+                        map[lch]--;
+                        l++;
+                    }
+
+                    ans = Math.Max(ans, r - l + 1);
+
+                    r++;
+                }
+
+                return ans;
+            }
+        }
     }
 }
