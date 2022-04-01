e9467b7ba3e1e337595e4273381f42cc6a84f297
barneyliu708 <barneyliu708@gmail.com>
2022-04-01 11:29:39 -0700

    ALGORITHM-23 LC215 Kth Largest Element in an Array - quick select with different pick-pivot approach

--- Algorithm/CH10_ElementaryDataStructure/LC215KthLargestElementInAnArray.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC215KthLargestElementInAnArray.cs
@@ -85,5 +85,58 @@ namespace Algorithm.CH10_ElementaryDataStructure
                 return QuickSelect(nums, pi + 1, r, kSmallest);
             }
         }
+
+        public class QuickSelectApproach2ndDone // with different pick-pivot approach
+        {
+            public int FindKthLargest(int[] nums, int k)
+            {
+                return FindKthLargest(nums, 0, nums.Length - 1, k);
+            }
+
+            // pick the right most element as the pivot
+            // return the indice of the pivot element after sorted
+            private int Partition(int[] nums, int l, int r)
+            {
+                int pivot = nums[r];
+                int p = l;
+                for (int i = l; i <= r; i++)
+                {
+                    if (nums[i] > pivot)
+                    {
+                        Swap(nums, p, i);
+                        p++;
+                    }
+                }
+                Swap(nums, p, r);
+                return p;
+            }
+
+            // return the value of the kth largest element
+            private int FindKthLargest(int[] nums, int l, int r, int k)
+            {
+                if (l == r)
+                {
+                    return nums[r];
+                }
+
+                int p = Partition(nums, l, r);
+                if (p + 1 == k) // p is 0-based index
+                {
+                    return nums[p];
+                }
+                if (p + 1 < k)
+                {
+                    return FindKthLargest(nums, p + 1, r, k);
+                }
+                return FindKthLargest(nums, l, p - 1, k);
+            }
+
+            private void Swap(int[] nums, int i, int j)
+            {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+            }
+        }
     }
 }
