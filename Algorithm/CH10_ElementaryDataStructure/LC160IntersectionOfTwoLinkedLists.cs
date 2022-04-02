91f2780033cfc19bb8408cee4f0389bfc3806b35
barneyliu708 <barneyliu708@gmail.com>
2022-04-01 21:28:36 -0700

    ALGORITHM-144 LC160. Intersection of Two Linked Lists - 2 pointers

--- Algorithm/CH10_ElementaryDataStructure/LC160IntersectionOfTwoLinkedLists.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC160IntersectionOfTwoLinkedLists.cs
@@ -36,5 +36,24 @@ namespace Algorithm.CH10_ElementaryDataStructure
 
             return null;
         }
+
+        public class TwoPointerApproach
+        {
+            public ListNode GetIntersectionNode(ListNode headA, ListNode headB)
+            {
+                ListNode p1 = headA;
+                ListNode p2 = headB;
+                while (p1 != p2)
+                {
+                    p1 = p1 == null ? headB : p1.next;
+                    p2 = p2 == null ? headA : p2.next;
+                }
+                return p1;
+                // Note: In the case lists do not intersect, the pointers for A and B
+                // will still line up in the 2nd iteration, just that here won't be
+                // a common node down the list and both will reach their respective ends
+                // at the same time. So pA will be NULL in that case.
+            }
+        }
     }
 }
