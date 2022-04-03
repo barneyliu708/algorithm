e76a2ffa3525410b4eebd6897366bd9d55996284
barneyliu708 <barneyliu708@gmail.com>
2022-04-02 21:33:07 -0700

    ALGORITHM-309 LC227. Basic Calculator II - 2nd done

--- Algorithm/CH10_ElementaryDataStructure/LC227BasicCalculatorII.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC227BasicCalculatorII.cs
@@ -61,5 +61,57 @@ namespace Algorithm.CH10_ElementaryDataStructure
         {
             return ch == '+' || ch == '-' || ch == '*' || ch == '/';
         }
+
+        public class SecondDone
+        {
+            public int Calculate(string s)
+            {
+                Stack<int> stack = new Stack<int>();
+                char sign = '+';
+                int operand = 0;
+                s = s.Trim();
+                for (int i = 0; i < s.Length; i++)
+                {
+                    char ch = s[i];
+                    if (ch == ' ')
+                    {
+                        continue;
+                    }
+                    if (char.IsDigit(ch))
+                    {
+                        operand = operand * 10 + (ch - '0');
+                    }
+                    if (i == s.Length - 1 || !char.IsDigit(ch))
+                    {
+                        if (sign == '+')
+                        {
+                            stack.Push(operand);
+                        }
+                        else if (sign == '-')
+                        {
+                            stack.Push(-operand);
+                        }
+                        else if (sign == '*')
+                        {
+                            stack.Push(stack.Pop() * operand);
+                        }
+                        else
+                        {
+                            stack.Push(stack.Pop() / operand);
+                        }
+                        sign = ch;
+                        operand = 0;
+                    }
+                }
+
+                int ans = 0;
+                while (stack.Count > 0)
+                {
+                    ans += stack.Pop();
+                }
+
+                return ans;
+            }
+        }
     }
 }
