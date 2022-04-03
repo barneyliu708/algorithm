141475dd43445409b76fba78bd3164b037262c09
barneyliu708 <barneyliu708@gmail.com>
2022-04-02 19:22:10 -0700

    ALGORITHM-334 LC224. Basic Calculator - Stack (Recursion)
    Approach 2-2 (recursion): https://leetcode.com/problems/basic-calculator/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC224BasicCalculator.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC224BasicCalculator
+    {
+        public int Calculate(string s)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (char ch in s)
+            {
+                if (ch != ' ') // remove space
+                {
+                    sb.Append(ch);
+                }
+            }
+            return Calculate(sb.ToString(), 0);
+        }
+
+        public int Calculate(string s, int i)
+        {
+            int operand = 0;
+            int result = 0;
+            int sign = 1;
+            while (i < s.Length)
+            {
+                char ch = s[i];
+                i++;
+
+                if (char.IsDigit(ch))
+                {
+                    operand = operand * 10 + (ch - '0');
+                }
+                else if (ch == '(')
+                {
+                    operand = Calculate(s, i);
+                }
+                else if (ch == ')')
+                {
+                    break; // the sub-expression is ended, exit now
+                }
+                else // ch == operator
+                {
+                    // store the current calculation to the result
+                    result += sign * operand;
+
+                    // reset the sign the operand
+                    sign = ch == '+' ? 1 : -1;
+                    operand = 0;
+                }
+            }
+            return result + sign * operand;
+        }
+    }
+}
