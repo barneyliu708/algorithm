54e0ccda46d72f0918dbc86538d4572cdac42d37
barneyliu708 <barneyliu708@gmail.com>
2022-04-04 21:13:19 -0700

    ALGORITHM-91 LC103. Binary Tree Zigzag Level Order Traversal - 2nd done

--- Algorithm/CH10_ElementaryDataStructure/LC103BinaryTreeZigzagLevelOrderTraversal.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC103BinaryTreeZigzagLevelOrderTraversal.cs
@@ -64,5 +64,93 @@ namespace Algorithm.CH10_ElementaryDataStructure
 
             return ans;
         }
+
+        public class SecondDone
+        {
+            public IList<IList<int>> ZigzagLevelOrder(TreeNode root)
+            {
+                if (root == null)
+                {
+                    return new List<IList<int>>();
+                }
+                Queue<TreeNode> queue = new Queue<TreeNode>();
+                queue.Enqueue(root);
+                IList<IList<int>> ans = new List<IList<int>>();
+                int level = 0;
+                while (queue.Count > 0)
+                {
+                    int count = queue.Count;
+                    List<int> subAns = new List<int>();
+                    for (int i = 0; i < count; i++)
+                    {
+                        TreeNode node = queue.Dequeue();
+                        subAns.Add(node.val);
+                        if (node.left != null)
+                        {
+                            queue.Enqueue(node.left);
+                        }
+                        if (node.right != null)
+                        {
+                            queue.Enqueue(node.right);
+                        }
+                    }
+                    if (level % 2 == 1)
+                    {
+                        subAns.Reverse();
+                    }
+                    ans.Add(subAns);
+                    level++;
+                }
+                return ans;
+            }
+        }
+
+        public class SecondDone_QueueWithDelimiter
+        {
+            public IList<IList<int>> ZigzagLevelOrder(TreeNode root)
+            {
+                if (root == null)
+                {
+                    return new List<IList<int>>();
+                }
+                Queue<TreeNode> queue = new Queue<TreeNode>();
+                queue.Enqueue(root);
+                queue.Enqueue(null);
+                IList<IList<int>> ans = new List<IList<int>>();
+                List<int> subAns = new List<int>();
+                bool isLeftDirection = true;
+                while (queue.Count > 0)
+                {
+                    TreeNode node = queue.Dequeue();
+                    if (node != null)
+                    {
+                        subAns.Add(node.val);
+                        if (node.left != null)
+                        {
+                            queue.Enqueue(node.left);
+                        }
+                        if (node.right != null)
+                        {
+                            queue.Enqueue(node.right);
+                        }
+                    }
+                    else
+                    {
+                        if (!isLeftDirection)
+                        {
+                            subAns.Reverse();
+                        }
+                        ans.Add(subAns);
+                        subAns = new List<int>();
+                        isLeftDirection = !isLeftDirection;
+                        if (queue.Count > 0)
+                        {
+                            queue.Enqueue(null);
+                        }
+                    }
+                }
+                return ans;
+            }
+        }
     }
 }
