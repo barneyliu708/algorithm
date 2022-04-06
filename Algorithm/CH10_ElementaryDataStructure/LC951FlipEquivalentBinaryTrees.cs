1e742bbbff2546c3724349873c30e6fbda86710b
barneyliu708 <barneyliu708@gmail.com>
2022-04-05 22:34:46 -0700

    ALGORITHM-282 LC951. Flip Equivalent Binary Trees - 2nd done

--- Algorithm/CH10_ElementaryDataStructure/LC951FlipEquivalentBinaryTrees.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC951FlipEquivalentBinaryTrees.cs
@@ -38,5 +38,25 @@ namespace Algorithm.CH10_ElementaryDataStructure
             return (FlipEquiv(root1.left, root2.left) && FlipEquiv(root1.right, root2.right)) ||
                    (FlipEquiv(root1.left, root2.right) && FlipEquiv(root1.right, root2.left));
         }
+
+        public class SecondDone
+        {
+            public bool FlipEquiv(TreeNode root1, TreeNode root2)
+            {
+                if (root1 == null && root2 == null)
+                {
+                    return true;
+                }
+
+                if (root1 == null || root2 == null)
+                {
+                    return false;
+                }
+
+                return root1.val == root2.val &&
+                    ((FlipEquiv(root1.left, root2.right) && FlipEquiv(root1.right, root2.left)) ||
+                    (FlipEquiv(root1.left, root2.left) && FlipEquiv(root1.right, root2.right)));
+            }
+        }
     }
 }
