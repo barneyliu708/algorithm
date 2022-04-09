d6976c685b0637fa86e8c8aa5fdcc798c20cadfd
barneyliu708 <barneyliu708@gmail.com>
2022-04-09 16:16:33 -0700

    ALGORITHM-134 LC017. Letter Combinations of a Phone Number - 2nd done
    Time Complexity: O(N*4^N)

--- Algorithm/CH10_ElementaryDataStructure/LC017LetterCombinationsOfAPhoneNumber.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC017LetterCombinationsOfAPhoneNumber.cs
@@ -51,5 +51,52 @@ namespace Algorithm.CH10_ElementaryDataStructure
             map[substr] = ans;
             return ans;
         }
+
+        public class SecondDone
+        {
+            public IList<string> LetterCombinations(string digits)
+            {
+                Dictionary<char, List<string>> map = new Dictionary<char, List<string>>() {
+                    { '2', new List<string> { "a", "b", "c" } },
+                    { '3', new List<string> { "d", "e", "f" } },
+                    { '4', new List<string> { "g", "h", "i" } },
+                    { '5', new List<string> { "j", "k", "l" } },
+                    { '6', new List<string> { "m", "n", "o" } },
+                    { '7', new List<string> { "p", "q", "r", "s" } },
+                    { '8', new List<string> { "t", "u", "v" } },
+                    { '9', new List<string> { "w", "x", "y", "z" } },
+                 };
+
+                return Combination(digits, 0, digits.Length - 1, map);
+            }
+
+            private IList<string> Combination(string digits, int l, int r, Dictionary<char, List<string>> map)
+            {
+                if (l > r)
+                {
+                    return new List<string>();
+                }
+
+                if (l == r)
+                {
+                    return map[digits[l]];
+                }
+
+                int mid = (l + r) / 2;
+                IList<string> left = Combination(digits, l, mid, map);
+                IList<string> right = Combination(digits, mid + 1, r, map);
+
+                List<string> ans = new List<string>();
+                foreach (string le in left)
+                {
+                    foreach (string ri in right)
+                    {
+                        ans.Add(le + ri);
+                    }
+                }
+
+                return ans;
+            }
+        }
     }
 }
