f1209fad45c8dd870b2fa7d2197d6aee360fe2e2
barneyliu708 <barneyliu708@gmail.com>
2022-04-10 15:13:36 -0700

    ALGORITHM-336 LC072. Edit Distance - dp
    dp: https://leetcode.com/problems/edit-distance/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC072EditDistance.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC072EditDistance
+    {
+        // should not count each characters as the sequence also matters
+        public int MinDistance(string word1, string word2)
+        {
+            int m = word1.Length;
+            int n = word2.Length;
+            int[,] dp = new int[m + 1, n + 1];
+            // i, j represent the length of substring, not the index
+            for (int i = 0; i < m + 1; i++)
+            {
+                dp[i, 0] = i;
+            }
+            for (int j = 0; j < n + 1; j++)
+            {
+                dp[0, j] = j;
+            }
+            for (int i = 1; i < m + 1; i++)
+            {
+                for (int j = 1; j < n + 1; j++)
+                {
+                    if (word1[i - 1] == word2[j - 1])
+                    {
+                        dp[i, j] = 1 + Math.Min(Math.Min(dp[i - 1, j], dp[i, j - 1]), dp[i - 1, j - 1] - 1);
+                    }
+                    else
+                    {
+                        dp[i, j] = 1 + Math.Min(Math.Min(dp[i - 1, j], dp[i, j - 1]), dp[i - 1, j - 1]);
+                    }
+                }
+            }
+            return dp[m, n];
+        }
+    }
+}
