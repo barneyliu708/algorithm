0a39d12562f68d087b1163674bee978926c4afb9
barneyliu708 <barneyliu708@gmail.com>
2022-04-10 20:02:34 -0700

    ALGORITHM-337 LC1235. Maximum Profit in Job Scheduling - dft + memorization (dp)
    dft + memorization (approach 1 top-down dp): https://leetcode.com/problems/maximum-profit-in-job-scheduling/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC1235MaximumProfitInJobScheduling.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC1235MaximumProfitInJobScheduling
+    {
+        public class DftMemorization
+        {
+            public int JobScheduling(int[] startTime, int[] endTime, int[] profit)
+            {
+                // prepare the jobs list
+                (int start, int end, int profit)[] jobs = new (int start, int end, int profit)[startTime.Length];
+                for (int i = 0; i < startTime.Length; i++)
+                {
+                    jobs[i] = (startTime[i], endTime[i], profit[i]);
+                }
+                Array.Sort(jobs, ((int start, int end, int profit) x, (int start, int end, int profit) y) => {
+                    return x.start.CompareTo(y.start);
+                });
+
+                // prepare the memorization
+                int[] memo = new int[startTime.Length];
+                return JobScheduling(jobs, 0, memo);
+            }
+
+            private int JobScheduling((int start, int end, int profit)[] jobs, int i, int[] memo)
+            {
+                if (i == jobs.Length)
+                {
+                    return 0;
+                }
+                if (memo[i] > 0)
+                {
+                    return memo[i];
+                }
+
+                int nexti = FindNextJob(jobs, i);
+
+                // take current ith job, or skip
+                memo[i] = Math.Max(jobs[i].profit + JobScheduling(jobs, nexti, memo), JobScheduling(jobs, i + 1, memo));
+                return memo[i];
+            }
+
+            // find the next available job after the ith job by the binary search
+            private int FindNextJob((int start, int end, int profit)[] jobs, int i)
+            {
+                int lastEnd = jobs[i].end;
+                int l = i;
+                int r = jobs.Length - 1;
+                while (l <= r)
+                {
+                    int mid = l + (r - l) / 2;
+                    if (jobs[mid].start == lastEnd)
+                    { // need to find the leftmost index that satisfy the conditions
+                        r = mid - 1;
+                    }
+                    if (jobs[mid].start < lastEnd)
+                    {
+                        l = mid + 1;
+                    }
+                    else
+                    {
+                        r = mid - 1;
+                    }
+                }
+                return l;
+            }
+        }
+    }
+}
