475d05fb55f598ac53fe53aa97603eeea04ce1a9
barneyliu708 <barneyliu708@gmail.com>
2022-04-12 21:47:53 -0700

    ALGORITHM-129 LC368. Largest Divisible Subset - 2nd done (Top-down dp)

--- Algorithm/CH10_ElementaryDataStructure/LC368LargestDivisibleSubset.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC368LargestDivisibleSubset.cs
@@ -48,5 +48,50 @@ namespace Algorithm.CH10_ElementaryDataStructure
 
             return ans;
         }
+
+        public class TopDowm_DP
+        {
+            public IList<int> LargestDivisibleSubset(int[] nums)
+            {
+                Array.Sort(nums);
+                Dictionary<int, List<int>> memo = new Dictionary<int, List<int>>();
+                List<int> maxSubset = new List<int>();
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    List<int> curSubset = Backtracking(nums, i, memo);
+                    if (curSubset.Count > maxSubset.Count)
+                    {
+                        maxSubset = curSubset;
+                    }
+                }
+                return maxSubset;
+            }
+
+            private List<int> Backtracking(int[] nums, int i, Dictionary<int, List<int>> memo)
+            {
+
+                if (memo.ContainsKey(i))
+                {
+                    return memo[i];
+                }
+
+                List<int> maxSubset = new List<int>();
+                for (int j = 0; j < i; j++)
+                {
+                    if (nums[i] % nums[j] == 0)
+                    {
+                        List<int> curSubset = Backtracking(nums, j, memo);
+                        if (curSubset.Count > maxSubset.Count)
+                        {
+                            maxSubset = curSubset;
+                        }
+                    }
+                }
+
+                memo[i] = new List<int>(maxSubset);
+                memo[i].Add(nums[i]);
+                return memo[i];
+            }
+        }
     }
 }
