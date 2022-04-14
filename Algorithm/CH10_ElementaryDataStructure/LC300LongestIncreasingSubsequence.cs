5dddf874afebdf0e1fa29f7fe44eea96c67d55fc
barneyliu708 <barneyliu708@gmail.com>
2022-04-13 20:44:37 -0700

    ALGORITHM-121 LC300. Longest Increasing Subsequence - dp (top-down)

--- Algorithm/CH10_ElementaryDataStructure/LC300LongestIncreasingSubsequence.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC300LongestIncreasingSubsequence.cs
@@ -30,5 +30,44 @@ namespace Algorithm.CH10_ElementaryDataStructure
 
             return ans;
         }
+
+        public class TopDown_DP
+        {
+            public int LengthOfLIS(int[] nums)
+            {
+                Dictionary<int, int> memo = new Dictionary<int, int>();
+                int ans = 0;
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    ans = Math.Max(ans, LengthOfLIS(nums, i, memo));
+                }
+                return ans;
+            }
+
+            private int LengthOfLIS(int[] nums, int i, Dictionary<int, int> memo)
+            {
+                if (i == 0)
+                {
+                    memo[0] = 1;
+                    return 1;
+                }
+                if (memo.ContainsKey(i))
+                {
+                    return memo[i];
+                }
+
+                int ans = 1;
+                for (int j = 0; j < i; j++)
+                {
+                    if (nums[j] < nums[i])
+                    {
+                        ans = Math.Max(ans, LengthOfLIS(nums, j, memo) + 1);
+                    }
+                }
+
+                memo[i] = ans;
+                return memo[i];
+            }
+        }
     }
 }
