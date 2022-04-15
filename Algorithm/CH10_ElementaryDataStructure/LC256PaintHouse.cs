ca847202a14a8211bb74f0c8c838abef185c8ee4
barneyliu708 <barneyliu708@gmail.com>
2022-04-14 19:27:55 -0700

    ALGORITHM-117 LC256. Paint House - 2nd done (top-down dp)

--- Algorithm/CH10_ElementaryDataStructure/LC256PaintHouse.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC256PaintHouse.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Algorithm.CH10_ElementaryDataStructure
 {
+    [TestFixture]
     class LC256PaintHouse
     {
         public int MinCost(int[][] costs)
@@ -23,6 +25,60 @@ namespace Algorithm.CH10_ElementaryDataStructure
             }
 
             return Math.Min(Math.Min(dp[n - 1, 0], dp[n - 1, 1]), dp[n - 1, 2]);
+        }
+
+        public class TopDown_DP
+        {
+            public int MinCost(int[][] costs)
+            {
+                int[,] memo = new int[costs.Length, 3];
+                int ans = int.MaxValue;
+                for (int icolor = 0; icolor < 3; icolor++)
+                {
+                    ans = Math.Min(ans, MinCost(costs, 0, icolor, memo));
+                }
+                return ans;
+            }
+
+            private int MinCost(int[][] costs, int ihouse, int icolor, int[,] memo)
+            {
+                if (ihouse == costs.Length)
+                {
+                    return 0;
+                }
+
+                if (memo[ihouse, icolor] != 0)
+                {
+                    return memo[ihouse, icolor];
+                }
+
+                int ans = int.MaxValue;
+                for (int jcolor = 0; jcolor < 3; jcolor++)
+                {
+                    if (jcolor != icolor)
+                    {
+                        ans = Math.Min(ans, MinCost(costs, ihouse + 1, jcolor, memo));
+                    }
+                }
+                ans += costs[ihouse][icolor];
+
+                memo[ihouse, icolor] = ans;
+                return ans;
+            }
+        }
+
+        [Test] 
+        public void TestCase1()
+        {
+            int[][] costs = new int[][]
+            {
+                new int[] {3, 5, 3},
+                new int[] {6, 17, 6},
+                new int[] {7, 13, 18},
+                new int[] {9, 10, 18},
+            };
+
+            int result = new TopDown_DP().MinCost(costs);
         }
     }
 }
