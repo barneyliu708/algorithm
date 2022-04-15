871651d9ed42675f7d56371e0e7acae9a81488fd
barneyliu708 <barneyliu708@gmail.com>
2022-04-15 16:40:37 -0700

    ALGORITHM-102 LC091. Decode Ways - 2nd done (bottom-up dp)

--- Algorithm/CH10_ElementaryDataStructure/LC091DecodeWays.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC091DecodeWays.cs
@@ -46,5 +46,32 @@ namespace Algorithm.CH10_ElementaryDataStructure
 
             return dp[s.Length - 1];
         }
+
+        public class BottomUp_DP
+        {
+            public int NumDecodings(string s)
+            {
+                int[] dp = new int[s.Length + 1];
+                if (s[0] == '0')
+                {
+                    return 0;
+                }
+                dp[0] = 1;
+                dp[1] = 1;
+                for (int i = 2; i < s.Length + 1; i++)
+                {
+                    if (s[i - 1] != '0')
+                    {
+                        dp[i] += dp[i - 1];
+                    }
+                    int twoDigit = int.Parse(s.Substring(i - 2, 2));
+                    if (twoDigit >= 10 && twoDigit <= 26)
+                    {
+                        dp[i] += dp[i - 2];
+                    }
+                }
+                return dp[s.Length];
+            }
+        }
     }
 }
