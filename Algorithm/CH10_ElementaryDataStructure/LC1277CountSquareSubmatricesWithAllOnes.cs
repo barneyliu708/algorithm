3f5abd94647b9e9b184d3fa62878f32165081c33
barneyliu708 <barneyliu708@gmail.com>
2022-04-15 20:27:14 -0700

    ALGORITHM-341 LC1277. Count Square Submatrices with All Ones - dp (bottom-up)
    solution is similar to the LC221

---
+++ Algorithm/CH10_ElementaryDataStructure/LC1277CountSquareSubmatricesWithAllOnes.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC1277CountSquareSubmatricesWithAllOnes
+    {
+        public int CountSquares(int[][] matrix)
+        {
+            int m = matrix.Length;
+            int n = matrix[0].Length;
+            int[,] dp = new int[m + 1, n + 1];
+            int ans = 0;
+            for (int i = 1; i < m + 1; i++)
+            {
+                for (int j = 1; j < n + 1; j++)
+                {
+                    if (matrix[i - 1][j - 1] == 1)
+                    {
+                        dp[i, j] = 1 + Math.Min(Math.Min(dp[i - 1, j], dp[i, j - 1]), dp[i - 1, j - 1]);
+                        ans += dp[i, j];
+                    }
+                }
+            }
+            return ans;
+        }
+    }
+}
