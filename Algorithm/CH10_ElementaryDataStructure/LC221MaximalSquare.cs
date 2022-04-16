f003a1d02b818fe153abeded1bad4416c1622514
barneyliu708 <barneyliu708@gmail.com>
2022-04-15 19:54:29 -0700

    ALGORITHM-115 LC221. Maximal Square - 2nd done

--- Algorithm/CH10_ElementaryDataStructure/LC221MaximalSquare.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC221MaximalSquare.cs
@@ -32,5 +32,28 @@ namespace Algorithm.CH10_ElementaryDataStructure
 
             return ans * ans;
         }
+
+        public class SecondDone
+        {
+            public int MaximalSquare(char[][] matrix)
+            {
+                int m = matrix.Length;
+                int n = matrix[0].Length;
+                int[,] dp = new int[m + 1, n + 1];
+                int ans = 0;
+                for (int i = 1; i < m + 1; i++)
+                {
+                    for (int j = 1; j < n + 1; j++)
+                    {
+                        if (matrix[i - 1][j - 1] == '1')
+                        {
+                            dp[i, j] = 1 + Math.Min(Math.Min(dp[i - 1, j - 1], dp[i, j - 1]), dp[i - 1, j]);
+                            ans = Math.Max(ans, dp[i, j]);
+                        }
+                    }
+                }
+                return ans * ans;
+            }
+        }
     }
 }
