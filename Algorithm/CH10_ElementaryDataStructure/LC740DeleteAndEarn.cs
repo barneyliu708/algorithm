c440cc7e086ecafb7f9b531e9e6d5e6c38c0198d
barneyliu708 <barneyliu708@gmail.com>
2022-04-16 21:38:51 -0700

    ALGORITHM-343 LC740. Delete and Earn - dynamic programming

---
+++ Algorithm/CH10_ElementaryDataStructure/LC740DeleteAndEarn.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC740DeleteAndEarn
+    {
+        public int DeleteAndEarn(int[] nums)
+        {
+            Dictionary<int, int> points = new Dictionary<int, int>(); // num - total points
+            int maxNum = 0;
+            foreach (int num in nums)
+            {
+                if (!points.ContainsKey(num))
+                {
+                    points[num] = 0;
+                }
+                points[num] += num;
+                maxNum = Math.Max(maxNum, num);
+            }
+
+            // create dp based on the value of num
+            int[] dp = new int[maxNum + 1];
+            dp[1] = points.ContainsKey(1) ? points[1] : 0;
+            for (int num = 2; num <= maxNum; num++)
+            {
+                int newPoints = points.ContainsKey(num) ? points[num] : 0;
+                dp[num] = Math.Max(dp[num - 1], dp[num - 2] + newPoints);
+            }
+
+            return dp[maxNum];
+        }
+    }
+}
