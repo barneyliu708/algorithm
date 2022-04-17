83d50f26032101876811f4b393f2d8a3bc984c4a
barneyliu708 <barneyliu708@gmail.com>
2022-04-16 20:48:08 -0700

    ALGORITHM-113 LC198. House Robber - 2nd done (dp)

--- Algorithm/CH10_ElementaryDataStructure/LC198HouseRobber.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC198HouseRobber.cs
@@ -30,5 +30,19 @@ namespace Algorithm.CH10_ElementaryDataStructure
 
             return dp[nums.Length - 1];
         }
+
+        public class SecondDone
+        {
+            public int Rob(int[] nums)
+            {
+                int[] dp = new int[nums.Length + 1];
+                dp[1] = nums[0];
+                for (int i = 2; i < nums.Length + 1; i++)
+                {
+                    dp[i] = Math.Max(dp[i - 1], dp[i - 2] + nums[i - 1]);
+                }
+                return dp[nums.Length];
+            }
+        }
     }
 }
