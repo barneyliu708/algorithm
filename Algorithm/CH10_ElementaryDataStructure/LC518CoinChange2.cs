a0e988e6a767f7efa6df333eff015dcba78472d5
barneyliu708 <barneyliu708@gmail.com>
2022-04-17 22:07:22 -0700

    ALGORITHM-345 LC518. Coin Change 2 - dp
    dp: https://leetcode.com/problems/coin-change-2/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC518CoinChange2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC518CoinChange2
+    {
+        public int Change(int amount, int[] coins)
+        {
+            int[] dp = new int[amount + 1];
+            dp[0] = 1;
+            foreach (int coin in coins)
+            {
+                for (int amt = coin; amt <= amount; amt++)
+                {
+                    if (amt - coin >= 0)
+                    {
+                        dp[amt] += dp[amt - coin];
+                    }
+                }
+            }
+            return dp[amount];
+        }
+    }
+}
