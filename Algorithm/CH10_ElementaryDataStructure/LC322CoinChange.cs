7afd5bebb6b44578d434c77260d3a3804a3de9e7
barneyliu708 <barneyliu708@gmail.com>
2022-04-17 21:29:10 -0700

    ALGORITHM-125 LC322. Coin Change - 2nd done

--- Algorithm/CH10_ElementaryDataStructure/LC322CoinChange.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC322CoinChange.cs
@@ -29,5 +29,35 @@ namespace Algorithm.CH10_ElementaryDataStructure
 
             return dp[amount] > amount ? -1 : dp[amount];
         }
+
+        public class SecondDone
+        {
+            public int CoinChange(int[] coins, int amount)
+            {
+                if (amount == 0)
+                {
+                    return 0;
+                }
+                int[] dp = new int[amount + 1];
+                for (int amt = 1; amt <= amount; amt++)
+                {
+                    // initial with Max value since we are looking for the minimum value
+                    dp[amt] = int.MaxValue;
+                    foreach (int coin in coins)
+                    {
+                        if (amt - coin >= 0 && dp[amt - coin] != -1)
+                        {
+                            dp[amt] = Math.Min(dp[amt], dp[amt - coin] + 1);
+                        }
+                    }
+                    if (dp[amt] == int.MaxValue)
+                    {
+                        dp[amt] = -1;
+                    }
+                }
+
+                return dp[amount];
+            }
+        }
     }
 }
