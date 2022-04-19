82f27155bd89d251f4e0c1a86ebdadbe2a8ab29a
barneyliu708 <barneyliu708@gmail.com>
2022-04-18 22:27:56 -0700

    ALGORITHM-347 LC1043. Partition Array for Maximum Sum - dp
    dp: https://www.cnblogs.com/grandyang/p/14461730.html

---
+++ Algorithm/CH10_ElementaryDataStructure/LC1043PartitionArrayForMaximumSum.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC1043PartitionArrayForMaximumSum
+    {
+        public int MaxSumAfterPartitioning(int[] arr, int k)
+        {
+            int n = arr.Length;
+
+            int[] dp = new int[n + 1];
+            for (int i = 1; i < n + 1; i++)
+            {
+                int max = arr[i - 1];
+                for (int l = 1; l <= k && i - l + 1 > 0; l++)
+                {
+                    max = Math.Max(max, arr[i - l]);
+                    dp[i] = Math.Max(dp[i], dp[i - l] + max * l);
+                }
+            }
+
+            return dp[n];
+        }
+    }
+}
