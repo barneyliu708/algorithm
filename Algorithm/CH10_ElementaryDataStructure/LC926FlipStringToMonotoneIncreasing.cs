de26224bcecbd0a4a1bbd895cbe911ce5c928a16
barneyliu708 <barneyliu708@gmail.com>
2022-04-18 22:54:22 -0700

    ALGORITHM-348 LC926. Flip String to Monotone Increasing - dp
    dp: https://leetcode.com/problems/flip-string-to-monotone-increasing/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC926FlipStringToMonotoneIncreasing.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC926FlipStringToMonotoneIncreasing
+    {
+        public int MinFlipsMonoIncr(string s)
+        {
+            int n = s.Length;
+
+            int[] presum = new int[n + 1];
+            for (int i = 0; i < n; i++)
+            {
+                presum[i + 1] = presum[i] + (s[i] - '0');
+            }
+
+            int ans = int.MaxValue;
+            for (int i = 0; i <= n; i++)
+            {
+                ans = Math.Min(ans, presum[i] + n - i - (presum[n] - presum[i]));
+            }
+
+            return ans;
+        }
+    }
+}
