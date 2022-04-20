0005a83e520cfec8cbf49bfc36cbc8e86a94417e
barneyliu708 <barneyliu708@gmail.com>
2022-04-19 21:08:48 -0700

    ALGORITHM-89 LC200. Number of Islands - bft
    bft - approach 2: https://leetcode.com/problems/number-of-islands/solution/

--- Algorithm/CH10_ElementaryDataStructure/LC200NumberOfIslands.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC200NumberOfIslands.cs
@@ -49,5 +49,62 @@ namespace Algorithm.CH10_ElementaryDataStructure
             Dft(grid, i, j + 1);
             Dft(grid, i, j - 1);
         }
+
+        public class BFTApproach
+        {
+            public int NumIslands(char[][] grid)
+            {
+                int numIsland = 0;
+                int nrow = grid.Length;
+                int ncol = grid[0].Length;
+                for (int r = 0; r < nrow; r++)
+                {
+                    for (int c = 0; c < ncol; c++)
+                    {
+                        if (grid[r][c] == '1')
+                        {
+                            numIsland++;
+
+                            // bread-first traversal to mark the whole island
+                            Queue<int> queue = new Queue<int>();
+                            queue.Enqueue(r * ncol + c);
+                            while (queue.Count > 0)
+                            {
+                                int id = queue.Dequeue();
+                                int col = id % ncol;
+                                int row = id / ncol;
+                                grid[row][col] = '0';
+
+                                if (row - 1 >= 0 && grid[row - 1][col] == '1')
+                                {
+                                    grid[row - 1][col] = '0';
+                                    queue.Enqueue((row - 1) * ncol + col);
+                                }
+
+                                if (row + 1 < nrow && grid[row + 1][col] == '1')
+                                {
+                                    grid[row + 1][col] = '0';
+                                    queue.Enqueue((row + 1) * ncol + col);
+                                }
+
+                                if (col - 1 >= 0 && grid[row][col - 1] == '1')
+                                {
+                                    grid[row][col - 1] = '0';
+                                    queue.Enqueue(row * ncol + col - 1);
+                                }
+
+                                if (col + 1 < ncol && grid[row][col + 1] == '1')
+                                {
+                                    grid[row][col + 1] = '0';
+                                    queue.Enqueue(row * ncol + col + 1);
+                                }
+                            }
+                        }
+                    }
+                }
+
+                return numIsland;
+            }
+        }
     }
 }
