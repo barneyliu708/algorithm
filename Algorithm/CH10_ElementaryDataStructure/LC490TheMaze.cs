3d82eeded2362cffb15baee727d6956795312853
barneyliu708 <barneyliu708@gmail.com>
2022-04-20 21:52:29 -0700

    ALGORITHM-296 LC490. The Maze - bft
    bft - approach 2: https://leetcode.com/problems/the-maze/solution/

--- Algorithm/CH10_ElementaryDataStructure/LC490TheMaze.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC490TheMaze.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace Algorithm.CH10_ElementaryDataStructure
 {
+    [TestFixture]
     internal class LC490TheMaze
     {
         public bool HasPath(int[][] maze, int[] start, int[] destination)
@@ -70,6 +72,68 @@ namespace Algorithm.CH10_ElementaryDataStructure
             }
 
             return false;
+        }
+
+
+        public class SecondDone
+        {
+            public bool HasPath(int[][] maze, int[] start, int[] destination)
+            {
+                bool[,] visited = new bool[maze.Length, maze[0].Length];
+                int[][] directions = new int[][] {
+            new int[] { 0, 1 }, new int[] { 0, -1 }, new int[] { 1, 0 }, new int[] { -1, 0 }
+        };
+                Queue<int[]> queue = new Queue<int[]>();
+                queue.Enqueue(start);
+                while (queue.Count > 0)
+                {
+                    int[] s = queue.Dequeue();
+                    visited[s[0], s[1]] = true;
+                    if (s[0] == destination[0] && s[1] == destination[1])
+                    {
+                        return true;
+                    }
+                    foreach (int[] dir in directions)
+                    {
+                        int[] p = new int[] { s[0], s[1] };
+                        while (CanMoveForward(maze, p, dir))
+                        {
+                            p[0] += dir[0];
+                            p[1] += dir[1];
+                        }
+                        if (!visited[p[0], p[1]])
+                        {
+                            queue.Enqueue(p);
+                        }
+                    }
+                }
+                return false;
+            }
+
+            private bool CanMoveForward(int[][] maze, int[] position, int[] direction)
+            {
+                int[] next = new int[] { position[0] + direction[0], position[1] + direction[1] };
+                return next[0] >= 0 && next[0] < maze.Length &&
+                       next[1] >= 0 && next[1] < maze[0].Length &&
+                       maze[next[0]][next[1]] != 1;
+            }
+
+            [Test] 
+            public void TestCase1()
+            {
+                int[][] maze = new int[][]
+                {
+                    new int[] {0, 0, 1, 0, 0 },
+                    new int[] {0, 0, 0, 0, 0 },
+                    new int[] {0, 0, 0, 1, 0 },
+                    new int[] {1, 1, 0, 1, 1 },
+                    new int[] {0, 0, 0, 0, 0 }
+                };
+                int[] start = new int[] { 0, 4 };
+                int[] destination = new[] { 4, 4 };
+
+                var test = HasPath(maze, start, destination);
+            }
         }
     }
 }
