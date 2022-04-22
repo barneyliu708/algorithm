062749a778340d7a0c34143d98efe9e5c08debd9
barneyliu708 <barneyliu708@gmail.com>
2022-04-22 14:30:01 -0700

    ALGORITHM-298 LC1091. Shortest Path in Binary Matrix - 2nd done

--- Algorithm/CH10_ElementaryDataStructure/LC1091ShortestPathInBinaryMatrix.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC1091ShortestPathInBinaryMatrix.cs
@@ -52,5 +52,60 @@ namespace Algorithm.CH10_ElementaryDataStructure
             }
             return -1;
         }
+
+        public class SecondDone
+        {
+            public int ShortestPathBinaryMatrix(int[][] grid)
+            {
+                int m = grid.Length;
+                int n = grid[0].Length;
+                bool[,] visited = new bool[m, n];
+
+                // corner case
+                if (grid[0][0] != 0 || grid[m - 1][n - 1] != 0)
+                {
+                    return -1;
+                }
+
+                // breadth-first traversal
+                Queue<(int r, int c)> queue = new Queue<(int r, int c)>();
+                queue.Enqueue((0, 0));
+                int steps = 0;
+                while (queue.Count > 0)
+                {
+                    int count = queue.Count;
+                    steps++;
+                    // scan the whole nodes of current level
+                    for (int cnt = 0; cnt < count; cnt++)
+                    {
+                        (int r, int c) index = queue.Dequeue();
+                        if (visited[index.r, index.c])
+                        {
+                            continue;
+                        }
+                        visited[index.r, index.c] = true;
+                        if (index.r == m - 1 && index.c == n - 1)
+                        {
+                            return steps;
+                        }
+                        // get all neighbors
+                        foreach (int i in new[] { -1, 0, 1 })
+                        {
+                            foreach (int j in new[] { -1, 0, 1 })
+                            {
+                                int row = index.r + i;
+                                int col = index.c + j;
+                                if (row >= 0 && row < m && col >= 0 && col < n &&
+                                    !visited[row, col] && grid[row][col] == 0)
+                                {
+                                    queue.Enqueue((index.r + i, index.c + j));
+                                }
+                            }
+                        }
+                    }
+                }
+                return -1;
+            }
+        }
     }
 }
