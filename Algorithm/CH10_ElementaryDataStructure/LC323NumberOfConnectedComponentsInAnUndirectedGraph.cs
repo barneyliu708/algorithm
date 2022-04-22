ecf418da0d87edc389843a2d773d70f533fc5f25
barneyliu708 <barneyliu708@gmail.com>
2022-04-21 20:35:50 -0700

    ALGORITHM-253 LC323. Number of Connected Components in an Undirected Graph - bft
    solution: https://leetcode.com/problems/number-of-connected-components-in-an-undirected-graph/solution/

--- Algorithm/CH10_ElementaryDataStructure/LC323NumberOfConnectedComponentsInAnUndirectedGraph.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC323NumberOfConnectedComponentsInAnUndirectedGraph.cs
@@ -48,5 +48,56 @@ namespace Algorithm.CH10_ElementaryDataStructure
                 }
             }
         }
+
+        public class BFTApproach
+        {
+            public int CountComponents(int n, int[][] edges)
+            {
+                // pre-process
+                List<int>[] neighbors = new List<int>[n];
+                for (int i = 0; i < n; i++)
+                {
+                    neighbors[i] = new List<int>();
+                }
+                foreach (int[] edge in edges)
+                {
+                    neighbors[edge[0]].Add(edge[1]);
+                    neighbors[edge[1]].Add(edge[0]);
+                }
+
+                // scan the whole nodes
+                bool[] visited = new bool[n];
+                int total = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    if (visited[i])
+                    {
+                        continue;
+                    }
+
+                    total++;
+
+                    // breadth-first traversal
+                    Queue<int> queue = new Queue<int>();
+                    queue.Enqueue(i);
+                    while (queue.Count > 0)
+                    {
+                        int cur = queue.Dequeue();
+                        if (visited[cur])
+                        {
+                            continue;
+                        }
+                        visited[cur] = true;
+
+                        foreach (int neighbor in neighbors[cur])
+                        {
+                            queue.Enqueue(neighbor);
+                        }
+                    }
+                }
+
+                return total;
+            }
+        }
     }
 }
