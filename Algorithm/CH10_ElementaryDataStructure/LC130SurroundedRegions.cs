5ee48be8941f729cfb9bea5acb38be1d09475363
barneyliu708 <barneyliu708@gmail.com>
2022-04-21 21:01:44 -0700

    ALGORITHM-87 LC130. Surrounded Regions - bft
    bft - approach 2: https://leetcode.com/problems/surrounded-regions/solution/

--- Algorithm/CH10_ElementaryDataStructure/LC130SurroundedRegions.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC130SurroundedRegions.cs
@@ -66,5 +66,67 @@ namespace Algorithm.CH10_ElementaryDataStructure
             Dft(board, row, col + 1);
 
         }
+
+        public class BFTApproach
+        {
+            public void Solve(char[][] board)
+            {
+                int m = board.Length;
+                int n = board[0].Length;
+
+                // generate all borders node
+                List<int[]> borders = new List<int[]>();
+                for (int i = 0; i < m; i++)
+                {
+                    borders.Add(new int[] { i, 0 });
+                    borders.Add(new int[] { i, n - 1 });
+                }
+                for (int j = 0; j < n; j++)
+                {
+                    borders.Add(new int[] { 0, j });
+                    borders.Add(new int[] { m - 1, j });
+                }
+
+                // scan the border nodes
+                foreach (int[] b in borders)
+                {
+                    if (board[b[0]][b[1]] == 'O')
+                    {
+                        // breadth-first traversal to mark the node
+                        Queue<int[]> queue = new Queue<int[]>();
+                        queue.Enqueue(b);
+
+                        while (queue.Count > 0)
+                        {
+                            int[] cur = queue.Dequeue();
+                            if (cur[0] < 0 || cur[0] >= m || cur[1] < 0 || cur[1] >= n || board[cur[0]][cur[1]] != 'O')
+                            {
+                                continue;
+                            }
+                            board[cur[0]][cur[1]] = 'E';
+                            queue.Enqueue(new int[] { cur[0] - 1, cur[1] });
+                            queue.Enqueue(new int[] { cur[0] + 1, cur[1] });
+                            queue.Enqueue(new int[] { cur[0], cur[1] - 1 });
+                            queue.Enqueue(new int[] { cur[0], cur[1] + 1 });
+                        }
+                    }
+                }
+
+                for (int i = 0; i < m; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        if (board[i][j] == 'O')
+                        {
+                            board[i][j] = 'X';
+                        }
+                        if (board[i][j] == 'E')
+                        {
+                            board[i][j] = 'O';
+                        }
+                    }
+                }
+            }
+        }
     }
 }
