f88ec5971910923ee7f218197954bb7ca8006a7e
barneyliu708 <barneyliu708@gmail.com>
2022-04-23 23:41:53 -0700

    ALGORITHM-248 LC207. Course Schedule - 2nd done

--- Algorithm/CH10_ElementaryDataStructure/LC207CourseSchedule.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC207CourseSchedule.cs
@@ -69,5 +69,68 @@ namespace Algorithm.CH10_ElementaryDataStructure
             checkes[curCourse] = true;
             return ans;
         }
+
+        public class SecondDone_DFT
+        {
+            public bool CanFinish(int numCourses, int[][] prerequisites)
+            {
+
+                Dictionary<int, List<int>> map = new Dictionary<int, List<int>>(); // prerequisite course - course
+                foreach (int[] course in prerequisites)
+                {
+                    if (!map.ContainsKey(course[1]))
+                    {
+                        map[course[1]] = new List<int>();
+                    }
+                    map[course[1]].Add(course[0]);
+                }
+
+                bool[] path = new bool[numCourses];
+                bool[] checkes = new bool[numCourses];
+
+                for (int curCourse = 0; curCourse < numCourses; curCourse++)
+                {
+                    if (IsCycle(curCourse, map, checkes, path))
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+
+            public bool IsCycle(int curCourse, Dictionary<int, List<int>> map, bool[] checkes, bool[] path)
+            {
+
+                if (checkes[curCourse])
+                {
+                    return false;
+                }
+
+                if (path[curCourse])
+                { // cycle detected
+                    return true;
+                }
+
+                if (!map.ContainsKey(curCourse))
+                {
+                    return false;
+                }
+
+                path[curCourse] = true; // mark in current path
+
+                foreach (int adjCourse in map[curCourse])
+                {
+                    if (IsCycle(adjCourse, map, checkes, path))
+                    {
+                        return true;
+                    }
+                }
+
+                path[curCourse] = false;
+                checkes[curCourse] = true;
+                return false;
+            }
+        }
     }
 }
