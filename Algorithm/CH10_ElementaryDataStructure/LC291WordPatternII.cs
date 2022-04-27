f7a613217b41654de80ad89b8d357fdb390e79f0
barneyliu708 <barneyliu708@gmail.com>
2022-04-26 20:24:18 -0700

    ALGORITHM-163 LC291. Word Pattern II - 2nd done

--- Algorithm/CH10_ElementaryDataStructure/LC291WordPatternII.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC291WordPatternII.cs
@@ -59,6 +59,56 @@ namespace Algorithm.CH10_ElementaryDataStructure
             return false;
         }
 
+        public class SecondDone
+        {
+            public bool WordPatternMatch(string pattern, string str)
+            {
+                Dictionary<char, string> ptos = new Dictionary<char, string>();
+                Dictionary<string, char> stop = new Dictionary<string, char>();
+                return Backtracking(pattern, 0, str, 0, ptos, stop);
+            }
+
+            private bool Backtracking(string pattern, int pi, string str, int si, Dictionary<char, string> ptos, Dictionary<string, char> stop)
+            {
+
+                if (pi == pattern.Length && si == str.Length)
+                {
+                    return true;
+                }
+
+                if (pi == pattern.Length || si == str.Length)
+                {
+                    return false;
+                }
+
+                for (int i = si; i < str.Length; i++)
+                {
+                    char pch = pattern[pi];
+                    string substr = str.Substring(si, i - si + 1);
+                    if (ptos.ContainsKey(pch) && ptos[pch] == substr)
+                    {
+                        if (Backtracking(pattern, pi + 1, str, i + 1, ptos, stop))
+                        {
+                            return true;
+                        }
+                    }
+                    else if (!ptos.ContainsKey(pch) && !stop.ContainsKey(substr))
+                    {
+                        ptos[pch] = substr;
+                        stop[substr] = pch;
+                        if (Backtracking(pattern, pi + 1, str, i + 1, ptos, stop))
+                        {
+                            return true;
+                        }
+                        ptos.Remove(pch);
+                        stop.Remove(substr);
+                    }
+                }
+
+                return false;
+            }
+        }
+
         [Test]
         public void TestCase1()
         {
