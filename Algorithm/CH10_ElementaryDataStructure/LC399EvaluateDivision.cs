026f1e10922c6aee578b8147834c89d77b73dd7d
barneyliu708 <barneyliu708@gmail.com>
2022-04-29 20:36:02 -0700

    ALGORITHM-254 LC399. Evaluate Division - 2nd done

--- Algorithm/CH10_ElementaryDataStructure/LC399EvaluateDivision.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC399EvaluateDivision.cs
@@ -82,5 +82,78 @@ namespace Algorithm.CH10_ElementaryDataStructure
 
             return ans;
         }
+
+        public class SecondDone
+        {
+            public double[] CalcEquation(IList<IList<string>> equations, double[] values, IList<IList<string>> queries)
+            {
+                // pre-process to build graph
+                Dictionary<string, Dictionary<string, double>> graph = new Dictionary<string, Dictionary<string, double>>();
+                for (int i = 0; i < equations.Count; i++)
+                {
+                    string var1 = equations[i][0];
+                    string var2 = equations[i][1];
+                    if (!graph.ContainsKey(var1))
+                    {
+                        graph[var1] = new Dictionary<string, double>();
+                    }
+                    if (!graph.ContainsKey(var2))
+                    {
+                        graph[var2] = new Dictionary<string, double>();
+                    }
+                    graph[var1][var2] = values[i];
+                    graph[var2][var1] = (double)(1 / values[i]);
+                }
+
+                // backtracking
+                double[] ans = new double[queries.Count];
+                for (int i = 0; i < queries.Count; i++)
+                {
+                    string var1 = queries[i][0];
+                    string var2 = queries[i][1];
+                    ans[i] = Backtracking(var1, var2, graph, new HashSet<string>());
+                }
+
+                return ans;
+            }
+
+            private double Backtracking(string start, string end, Dictionary<string, Dictionary<string, double>> graph, HashSet<string> path)
+            {
+                if (!graph.ContainsKey(start) || !graph.ContainsKey(end))
+                {
+                    return -1.0;
+                }
+
+                if (start == end)
+                {
+                    return 1.0;
+                }
+
+                if (graph.ContainsKey(start) && graph[start].ContainsKey(end))
+                {
+                    return graph[start][end];
+                }
+
+                path.Add(start);
+
+                foreach (string neighbor in graph[start].Keys)
+                {
+                    if (path.Contains(neighbor))
+                    {
+                        continue;
+                    }
+                    double subans = Backtracking(neighbor, end, graph, path);
+                    if (subans != -1)
+                    {
+                        graph[start][end] = graph[start][neighbor] * subans;
+                        return graph[start][end];
+                    }
+                }
+
+                path.Remove(start);
+
+                return -1.0;
+            }
+        }
     }
 }
