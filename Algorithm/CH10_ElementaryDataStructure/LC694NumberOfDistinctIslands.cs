0037d92d11357c15cabbfd8485673308ccf8af1d
barneyliu708 <barneyliu708@gmail.com>
2022-04-29 21:29:18 -0700

    ALGORITHM-290 LC694. Number of Distinct Islands - 2nd done

--- Algorithm/CH10_ElementaryDataStructure/LC694NumberOfDistinctIslands.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC694NumberOfDistinctIslands.cs
@@ -45,5 +45,49 @@ namespace Algorithm.CH10_ElementaryDataStructure
             DftIsland(grid, si, sj, i, j + 1, curIsland);
             DftIsland(grid, si, sj, i, j - 1, curIsland);
         }
+
+        public class SecondDone
+        {
+            public int NumDistinctIslands(int[][] grid)
+            {
+
+                HashSet<string> islands = new HashSet<string>();
+                for (int i = 0; i < grid.Length; i++)
+                {
+                    for (int j = 0; j < grid[0].Length; j++)
+                    {
+                        if (grid[i][j] == 1)
+                        {
+                            StringBuilder sb = new StringBuilder();
+                            DFT(i, j, i, j, grid, sb);
+                            islands.Add(sb.ToString());
+                        }
+                    }
+                }
+
+                return islands.Count;
+            }
+
+            private void DFT(int i, int j, int starti, int startj, int[][] grid, StringBuilder sb)
+            {
+                if (i < 0 || i >= grid.Length ||
+                    j < 0 || j >= grid[0].Length ||
+                   grid[i][j] != 1)
+                {
+                    return;
+                }
+
+                grid[i][j] = 2;
+
+                // build the hash
+                sb.Append(i - starti);
+                sb.Append(j - startj);
+
+                DFT(i + 1, j, starti, startj, grid, sb);
+                DFT(i - 1, j, starti, startj, grid, sb);
+                DFT(i, j + 1, starti, startj, grid, sb);
+                DFT(i, j - 1, starti, startj, grid, sb);
+            }
+        }
     }
 }
