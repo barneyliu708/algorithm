ff2eb0424eccd11dca601f09c8fa43de0093c096
barneyliu708 <barneyliu708@gmail.com>
2022-04-30 22:29:43 -0700

    ALGORITHM-259 LC039. Combination Sum - 2nd done

--- Algorithm/CH10_ElementaryDataStructure/LC039CombinationSum.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC039CombinationSum.cs
@@ -37,5 +37,37 @@ namespace Algorithm.CH10_ElementaryDataStructure
                 comb.Remove(candidates[i]);
             }
         }
+
+        public class SecondDone
+        {
+            public IList<IList<int>> CombinationSum(int[] candidates, int target)
+            {
+                Array.Sort(candidates);
+                List<int> path = new List<int>();
+                List<IList<int>> ans = new List<IList<int>>();
+                Combination(candidates, 0, target, path, ans);
+                return ans;
+            }
+
+            private void Combination(int[] candidates, int i, int target, List<int> path, List<IList<int>> ans)
+            {
+                if (target < 0)
+                {
+                    return;
+                }
+                if (target == 0)
+                {
+                    ans.Add(new List<int>(path));
+                    return;
+                }
+
+                for (int j = i; j < candidates.Length; j++)
+                {
+                    path.Add(candidates[j]);
+                    Combination(candidates, j, target - candidates[j], path, ans);
+                    path.RemoveAt(path.Count - 1);
+                }
+            }
+        }
     }
 }
