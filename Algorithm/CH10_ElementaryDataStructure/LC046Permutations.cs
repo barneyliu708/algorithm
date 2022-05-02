cf35af8d5fb8a12b1de161a2d723483a09c1b164
barneyliu708 <barneyliu708@gmail.com>
2022-05-01 18:18:37 -0700

    ALGORITHM-261 LC046. Permutations - 2nd done

--- Algorithm/CH10_ElementaryDataStructure/LC046Permutations.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC046Permutations.cs
@@ -36,5 +36,37 @@ namespace Algorithm.CH10_ElementaryDataStructure
             nums[i] = nums[j];
             nums[j] = temp;
         }
+
+        public class SecondDone
+        {
+            public IList<IList<int>> Permute(int[] nums)
+            {
+                List<IList<int>> ans = new List<IList<int>>();
+                Permute(nums, 0, ans);
+                return ans;
+            }
+
+            private void Permute(int[] nums, int i, List<IList<int>> ans)
+            {
+                if (i == nums.Length)
+                {
+                    ans.Add(new List<int>(nums));
+                }
+
+                for (int j = i; j < nums.Length; j++)
+                {
+                    Swap(nums, i, j);
+                    Permute(nums, i + 1, ans); // use i + 1 to the next level, rather than the j + 1
+                    Swap(nums, i, j);
+                }
+            }
+
+            private void Swap(int[] nums, int i, int j)
+            {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+            }
+        }
     }
 }
