dd1bbcf9f71b8dc2549d986d2893f1fd9c7085a5
barneyliu708 <barneyliu708@gmail.com>
2022-05-02 21:04:12 -0700

    ALGORITHM-55 LC144 Binary Tree Preorder Traversal - 2nd done

--- Algorithm/CH10_ElementaryDataStructure/LC144BinaryTreePreorderTraversal.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC144BinaryTreePreorderTraversal.cs
@@ -39,5 +39,37 @@ namespace Algorithm.CH10_ElementaryDataStructure
             }
             return results;
         }
+
+        public class SecondDone
+        {
+            public IList<int> PreorderTraversal(TreeNode root)
+            {
+
+                List<int> ans = new List<int>();
+                Stack<TreeNode> stack = new Stack<TreeNode>();
+                if (root != null)
+                {
+                    stack.Push(root);
+                }
+
+                while (stack.Count > 0)
+                {
+                    TreeNode cur = stack.Pop();
+                    ans.Add(cur.val);
+
+                    if (cur.right != null)
+                    {
+                        stack.Push(cur.right);
+                    }
+
+                    if (cur.left != null)
+                    {
+                        stack.Push(cur.left);
+                    }
+                }
+
+                return ans;
+            }
+        }
     }
 }
