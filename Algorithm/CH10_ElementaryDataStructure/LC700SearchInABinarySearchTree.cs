60cb38efcc3d7f75766bee6d7a2c1ed984aca385
barneyliu708 <barneyliu708@gmail.com>
2022-05-03 20:54:42 -0700

    ALGORITHM-180 LC700. Search in a Binary Search Tree - 2nd done

--- Algorithm/CH10_ElementaryDataStructure/LC700SearchInABinarySearchTree.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC700SearchInABinarySearchTree.cs
@@ -40,5 +40,29 @@ namespace Algorithm.CH10_ElementaryDataStructure
             }
             return null;
         }
+
+        public class SecondDone
+        {
+            public TreeNode SearchBST(TreeNode root, int val)
+            {
+                if (root == null)
+                {
+                    return null;
+                }
+
+                if (root.val == val)
+                {
+                    return root;
+                }
+                else if (root.val > val)
+                {
+                    return SearchBST(root.left, val);
+                }
+                else
+                {
+                    return SearchBST(root.right, val);
+                }
+            }
+        }
     }
 }
