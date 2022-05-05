76826cc8ce12b74722e0f1a0150118abcd9f9e6e
barneyliu708 <barneyliu708@gmail.com>
2022-05-04 20:48:51 -0700

    ALGORITHM-183 LC333. Largest BST Subtree - 2nd done

--- Algorithm/CH10_ElementaryDataStructure/LC333LargestBSTSubtree.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC333LargestBSTSubtree.cs
@@ -55,5 +55,45 @@ namespace Algorithm.CH10_ElementaryDataStructure
 
             return new NodeValue(int.MinValue, int.MaxValue, Math.Max(left.MaxSize, right.MaxSize));
         }
+
+        public class SecondDone
+        {
+            public class NodeValue
+            {
+                public int MaxValue;
+                public int MinValue;
+                public int MaxSize;
+                public NodeValue(int minValue, int maxValue, int maxSize)
+                {
+                    MinValue = minValue;
+                    MaxValue = maxValue;
+                    MaxSize = maxSize;
+                }
+            }
+
+            public int LargestBSTSubtree(TreeNode root)
+            {
+                return Dft(root).MaxSize;
+            }
+
+            public NodeValue Dft(TreeNode root)
+            {
+
+                if (root == null)
+                {
+                    return new NodeValue(int.MaxValue, int.MinValue, 0);
+                }
+
+                NodeValue left = Dft(root.left);
+                NodeValue right = Dft(root.right);
+
+                if (left.MaxValue < root.val && root.val < right.MinValue)
+                {
+                    return new NodeValue(Math.Min(left.MinValue, root.val), Math.Max(root.val, right.MaxValue), left.MaxSize + 1 + right.MaxSize);
+                }
+
+                return new NodeValue(int.MinValue, int.MaxValue, Math.Max(left.MaxSize, right.MaxSize));
+            }
+        }
     }
 }
