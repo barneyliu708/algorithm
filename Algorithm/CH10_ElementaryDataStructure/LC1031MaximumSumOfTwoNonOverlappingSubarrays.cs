8c64fd4ebdb4918f4b9f8c7de6bf9b90f31be860
barneyliu708 <barneyliu708@gmail.com>
2022-05-06 22:27:13 -0700

    ALGORITHM-356 LC1031. Maximum Sum of Two Non-Overlapping Subarrays - dp
    dp: https://leetcode.com/problems/maximum-sum-of-two-non-overlapping-subarrays/discuss/278727/C%2B%2B-O(N)-buysell-stock-2-times
    
    solution is similar to buysell-stock-2-times

---
+++ Algorithm/CH10_ElementaryDataStructure/LC1031MaximumSumOfTwoNonOverlappingSubarrays.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC1031MaximumSumOfTwoNonOverlappingSubarrays
+    {
+        public int MaxSumTwoNoOverlap(int[] nums, int firstLen, int secondLen)
+        {
+            return Math.Max(MaxSum(nums, firstLen, secondLen), MaxSum(nums, secondLen, firstLen));
+        }
+
+        private int MaxSum(int[] nums, int firstLen, int secondLen)
+        {
+            int n = nums.Length;
+
+            int[] leftmax = new int[n];
+            int subsum = 0;
+            int maxsum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                subsum += nums[i];
+                if (i > firstLen - 1)
+                {
+                    subsum -= nums[i - firstLen];
+                }
+                maxsum = Math.Max(maxsum, subsum);
+                leftmax[i] = maxsum;
+            }
+
+            subsum = 0;
+            maxsum = 0;
+            int[] rightmax = new int[n];
+            for (int i = n - 1; i >= 0; i--)
+            {
+                subsum += nums[i];
+                if (i < n - secondLen)
+                {
+                    subsum -= nums[i + secondLen];
+                }
+                maxsum = Math.Max(maxsum, subsum);
+                rightmax[i] = maxsum;
+            }
+
+            int max = 0;
+            for (int i = firstLen - 1; i < n - secondLen; i++)
+            {
+                max = Math.Max(max, leftmax[i] + rightmax[i + 1]);
+            }
+
+            return max;
+        }
+    }
+}
