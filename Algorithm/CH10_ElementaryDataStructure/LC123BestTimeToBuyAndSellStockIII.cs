7f3fe94b072421a9371b4b3d0ada9513a16f1864
barneyliu708 <barneyliu708@gmail.com>
2022-05-06 21:50:08 -0700

    ALGORITHM-355 LC123. Best Time to Buy and Sell Stock III - dynamic programming
    dp - approach 1: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC123BestTimeToBuyAndSellStockIII.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC123BestTimeToBuyAndSellStockIII
+    {
+        public int MaxProfit(int[] prices)
+        {
+            int n = prices.Length;
+
+            int minleft = prices[0];
+            int[] leftprofits = new int[n];
+            for (int l = 1; l < n; l++)
+            {
+                leftprofits[l] = Math.Max(leftprofits[l - 1], prices[l] - minleft);
+                minleft = Math.Min(minleft, prices[l]);
+            }
+
+            int maxright = prices[n - 1];
+            int[] rightprofits = new int[n];
+            for (int r = n - 2; r >= 0; r--)
+            {
+                rightprofits[r] = Math.Max(rightprofits[r + 1], maxright - prices[r]);
+                maxright = Math.Max(maxright, prices[r]);
+            }
+
+            int max = 0;
+            for (int i = 0; i < n - 1; i++)
+            {
+                max = Math.Max(max, leftprofits[i] + rightprofits[i]);
+            }
+
+            return max;
+        }
+    }
+}
