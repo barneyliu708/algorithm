74d1414b611a5c31141bebc1db8e0bc4352b4f12
barneyliu708 <barneyliu708@gmail.com>
2022-05-07 20:31:32 -0700

    ALGORITHM-357 LC532. K-diff Pairs in an Array - hash table
    hash table - approach 3: https://leetcode.com/problems/k-diff-pairs-in-an-array/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC532KdiffPairsInAnArray.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC532KdiffPairsInAnArray
+    {
+        public int FindPairs(int[] nums, int k)
+        {
+
+            int total = 0;
+            if (k == 0)
+            {
+                Dictionary<int, int> count = new Dictionary<int, int>();
+                foreach (int num in nums)
+                {
+                    if (!count.ContainsKey(num))
+                    {
+                        count[num] = 0;
+                    }
+                    count[num]++;
+                    // Console.WriteLine(num + " " + count[num]);
+                }
+                foreach (int num in count.Keys)
+                {
+                    if (count[num] > 1)
+                    {
+                        total++;
+                    }
+                }
+                return total;
+            }
+
+            Dictionary<int, HashSet<int>> map = new Dictionary<int, HashSet<int>>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int num = nums[i];
+                if (!map.ContainsKey(num))
+                {
+                    map[num] = new HashSet<int>();
+                }
+                if (map.ContainsKey(num + k))
+                {
+                    map[num + k].Add(num);
+                    map[num].Add(num + k);
+                }
+                if (map.ContainsKey(num - k))
+                {
+                    map[num - k].Add(num);
+                    map[num].Add(num - k);
+                }
+            }
+
+            foreach (int num in map.Keys)
+            {
+                total += map[num].Count;
+            }
+            return total / 2;
+        }
+    }
+}
