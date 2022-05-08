25c5651215e44dcecbb7e1cb7fa05e054c92516d
barneyliu708 <barneyliu708@gmail.com>
2022-05-07 21:18:45 -0700

    ALGORITHM-358 LC523. Continuous Subarray Sum - prefix sum
    prefix sum: https://leetcode.com/problems/continuous-subarray-sum/discuss/99499/Java-O(n)-time-O(k)-space

---
+++ Algorithm/CH10_ElementaryDataStructure/LC523ContinuousSubarraySum.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC523ContinuousSubarraySum
+    {
+        public bool CheckSubarraySum(int[] nums, int k)
+        {
+            int n = nums.Length;
+            if (n <= 1)
+            {
+                return false;
+            }
+            int[] presum = new int[n + 1];
+            for (int i = 1; i < n + 1; i++)
+            {
+                presum[i] = presum[i - 1] + nums[i - 1];
+            }
+
+            Dictionary<int, int> map = new Dictionary<int, int>(); // remainder - index
+            for (int i = 1; i < n + 1; i++)
+            {
+                int remainder = presum[i] % k;
+                if (remainder == 0 && i > 1)
+                {
+                    return true;
+                }
+                if (map.ContainsKey(remainder))
+                {
+                    if (i - map[remainder] >= 2)
+                    {
+                        return true;
+                    }
+                }
+                else
+                {
+                    map[remainder] = i;
+                }
+            }
+
+            return false;
+        }
+    }
+}
