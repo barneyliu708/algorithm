b49575db802788aa0cd0711f738d1d99de913cd8
barneyliu708 <barneyliu708@gmail.com>
2022-05-09 20:40:45 -0700

    ALGORITHM-61 LC225 Implement Stack using Queues - 2nd done

--- Algorithm/CH10_ElementaryDataStructure/LC225ImplementStackUsingQueues.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC225ImplementStackUsingQueues.cs
@@ -42,5 +42,45 @@ namespace Algorithm.CH10_ElementaryDataStructure
                 return queue.Count == 0;
             }
         }
+
+        public class SecondDone
+        {
+            public class MyStack
+            {
+
+                private Queue<int> queue;
+
+                public MyStack()
+                {
+                    queue = new Queue<int>();
+                }
+
+                public void Push(int x)
+                {
+                    queue.Enqueue(x);
+                    int size = queue.Count;
+                    while (size > 1)
+                    {
+                        queue.Enqueue(queue.Dequeue());
+                        size--;
+                    }
+                }
+
+                public int Pop()
+                {
+                    return queue.Dequeue();
+                }
+
+                public int Top()
+                {
+                    return queue.Peek();
+                }
+
+                public bool Empty()
+                {
+                    return queue.Count == 0;
+                }
+            }
+        }
     }
 }
