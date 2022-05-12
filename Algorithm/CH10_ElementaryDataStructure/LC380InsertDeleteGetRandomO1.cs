88cf9183743d3761cb03268ea6fe63bad2eacac9
barneyliu708 <barneyliu708@gmail.com>
2022-05-12 08:26:17 -0700

    ALGORITHM-313 LC380. Insert Delete GetRandom O(1) - 2nd done

--- Algorithm/CH10_ElementaryDataStructure/LC380InsertDeleteGetRandomO1.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC380InsertDeleteGetRandomO1.cs
@@ -60,5 +60,62 @@ namespace Algorithm.CH10_ElementaryDataStructure
                 return list[rand.Next(list.Count)]; // return random element between 0 and list.count - 1
             }
         }
+
+        public class SecondDone
+        {
+            public class RandomizedSet
+            {
+
+                private List<int> list;
+                private Dictionary<int, int> map; // val - index of val in the list
+                private Random rand;
+
+                public RandomizedSet()
+                {
+                    list = new List<int>();
+                    map = new Dictionary<int, int>();
+                    rand = new Random();
+                }
+
+                public bool Insert(int val)
+                {
+                    if (map.ContainsKey(val))
+                    {
+                        return false;
+                    }
+                    list.Add(val); // O(1);
+                    map[val] = list.Count - 1; // O(1)
+
+                    return true;
+                }
+
+                public bool Remove(int val)
+                {
+                    if (!map.ContainsKey(val))
+                    {
+                        return false;
+                    }
+
+                    // swap the val with the last element in the list
+                    int ival = map[val];
+                    int lastVal = list[list.Count - 1];
+                    list[ival] = lastVal;
+                    list[list.Count - 1] = val;
+                    map[lastVal] = ival;
+
+                    // remove the last element in the list
+                    list.RemoveAt(list.Count - 1);
+                    map.Remove(val);
+
+                    return true;
+                }
+
+                public int GetRandom()
+                {
+                    return list[rand.Next(list.Count)];
+                }
+            }
+
+        }
     }
 }
