b29b4ddcb867dba25b66af23967447804d23425a
barneyliu708 <barneyliu708@gmail.com>
2022-05-12 08:48:08 -0700

    ALGORITHM-136 LC049. Group Anagrams - 2nd done

--- Algorithm/CH10_ElementaryDataStructure/LC049GroupAnagrams.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC049GroupAnagrams.cs
@@ -45,5 +45,39 @@ namespace Algorithm.CH10_ElementaryDataStructure
 
             return sb.ToString();
         }
+
+        public class SecondDone
+        {
+            public IList<IList<string>> GroupAnagrams(string[] strs)
+            {
+                Dictionary<string, IList<string>> map = new Dictionary<string, IList<string>>();
+                foreach (string str in strs)
+                {
+                    string hash = HashStr(str);
+                    if (!map.ContainsKey(hash))
+                    {
+                        map[hash] = new List<string>();
+                    }
+                    map[hash].Add(str);
+                }
+
+                return map.Values.ToList();
+            }
+
+            private string HashStr(string str)
+            {
+                int[] count = new int[26];
+                foreach (char ch in str)
+                {
+                    count[ch - 'a']++;
+                }
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < count.Length; i++)
+                {
+                    sb.Append('#' + count[i]);
+                }
+                return sb.ToString();
+            }
+        }
     }
 }
