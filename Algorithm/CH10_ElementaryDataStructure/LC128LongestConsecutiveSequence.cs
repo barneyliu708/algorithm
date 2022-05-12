3d0524e599dc033f862a0830a1a393969930cc77
barneyliu708 <barneyliu708@gmail.com>
2022-05-11 20:24:28 -0700

    ALGORITHM-138 LC128. Longest Consecutive Sequence - 2nd done

--- Algorithm/CH10_ElementaryDataStructure/LC128LongestConsecutiveSequence.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC128LongestConsecutiveSequence.cs
@@ -35,5 +35,35 @@ namespace Algorithm.CH10_ElementaryDataStructure
 
             return ans;
         }
+
+        public class SecondDone
+        {
+            public int LongestConsecutive(int[] nums)
+            {
+                HashSet<int> hashset = new HashSet<int>();
+                foreach (int num in nums)
+                {
+                    hashset.Add(num);
+                }
+
+                int ans = 0;
+                foreach (int num in nums)
+                {
+                    if (!hashset.Contains(num - 1))
+                    {
+                        int count = 0;
+                        int n = num;
+                        while (hashset.Contains(n))
+                        {
+                            count++;
+                            n++;
+                        }
+                        ans = Math.Max(ans, count);
+                    }
+                }
+
+                return ans;
+            }
+        }
     }
 }
