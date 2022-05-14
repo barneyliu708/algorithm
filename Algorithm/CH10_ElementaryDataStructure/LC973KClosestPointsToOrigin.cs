85c4b53f859d1249777f3d08d0e6e6b9a7a9d608
barneyliu708 <barneyliu708@gmail.com>
2022-05-13 21:37:17 -0700

    ALGORITHM-314 LC973. K Closest Points to Origin - 2nd done

--- Algorithm/CH10_ElementaryDataStructure/LC973KClosestPointsToOrigin.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC973KClosestPointsToOrigin.cs
@@ -29,5 +29,34 @@ namespace Algorithm.CH10_ElementaryDataStructure
 
             return ans;
         }
+
+        public class SecondDone
+        {
+            public int[][] KClosest(int[][] points, int k)
+            {
+                PriorityQueue<int[], int[]> pq = new PriorityQueue<int[], int[]>(Comparer<int[]>.Create((x, y) => {
+                    int xdist = x[0] * x[0] + x[1] * x[1];
+                    int ydist = y[0] * y[0] + y[1] * y[1];
+                    return ydist.CompareTo(xdist); // max-heap
+                }));
+
+                foreach (int[] point in points)
+                {
+                    pq.Enqueue(point, point);
+                    if (pq.Count > k)
+                    {
+                        pq.Dequeue();
+                    }
+                }
+
+                int[][] ans = new int[k][];
+                for (int i = 0; i < k; i++)
+                {
+                    ans[i] = pq.Dequeue();
+                }
+
+                return ans;
+            }
+        }
     }
 }
