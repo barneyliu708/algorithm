d58bf20f2d8a63209985298621f64cb491acdee6
barneyliu708 <barneyliu708@gmail.com>
2022-05-14 10:34:33 -0700

    ALGORITHM-217 LC088. Merge Sorted Array - 2nd done

--- Algorithm/CH10_ElementaryDataStructure/LC088MergeSortedArray.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC088MergeSortedArray.cs
@@ -29,5 +29,39 @@ namespace Algorithm.CH10_ElementaryDataStructure
                 nums1[p--] = nums2[j--];
             }
         }
+
+        public class SecondDone
+        {
+            public void Merge(int[] nums1, int m, int[] nums2, int n)
+            {
+                int p = m + n - 1;
+                int p1 = m - 1;
+                int p2 = n - 1;
+                while (p1 >= 0 && p2 >= 0)
+                {
+                    if (nums1[p1] > nums2[p2])
+                    {
+                        nums1[p] = nums1[p1];
+                        p1--;
+                    }
+                    else
+                    {
+                        nums1[p] = nums2[p2];
+                        p2--;
+                    }
+                    p--;
+                }
+
+                while (p1 >= 0)
+                {
+                    nums1[p--] = nums1[p1--];
+                }
+
+                while (p2 >= 0)
+                {
+                    nums1[p--] = nums2[p2--];
+                }
+            }
+        }
     }
 }
