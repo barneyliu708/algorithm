23722f3ab1d2fcd715476b0a26fae9c06c4a7798
barneyliu708 <barneyliu708@gmail.com>
2022-05-14 10:27:41 -0700

    ALGORITHM-119 LC264. Ugly Number II - 2nd done

--- Algorithm/CH10_ElementaryDataStructure/LC264UglyNumberII.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC264UglyNumberII.cs
@@ -42,6 +42,41 @@ namespace Algorithm.CH10_ElementaryDataStructure
             return ans[ans.Count - 1];
         }
 
+        public class SecondDone
+        {
+            public int NthUglyNumber(int n)
+            {
+                int i2 = 0;
+                int i3 = 0;
+                int i5 = 0;
+                List<int> list = new List<int>();
+                list.Add(1);
+                while (list.Count < n)
+                {
+                    int n2 = list[i2] * 2;
+                    int n3 = list[i3] * 3;
+                    int n5 = list[i5] * 5;
+
+                    int candidate = Math.Min(n2, Math.Min(n3, n5));
+                    if (candidate == n2)
+                    {
+                        i2++;
+                    }
+                    if (candidate == n3)
+                    {
+                        i3++;
+                    }
+                    if (candidate == n5)
+                    {
+                        i5++;
+                    }
+                    list.Add(candidate);
+                }
+
+                return list[list.Count - 1];
+            }
+        }
+
         [Test]
         public void TestCase1()
         {
