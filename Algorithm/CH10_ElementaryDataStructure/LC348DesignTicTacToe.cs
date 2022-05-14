83aac240eadd7976d8af358ea9bfafe952003559
barneyliu708 <barneyliu708@gmail.com>
2022-05-13 21:05:46 -0700

    ALGORITHM-169 LC348. Design Tic-Tac-Toe - 2nd done

--- Algorithm/CH10_ElementaryDataStructure/LC348DesignTicTacToe.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC348DesignTicTacToe.cs
@@ -52,5 +52,48 @@ namespace Algorithm.CH10_ElementaryDataStructure
                 return 0;
             }
         }
+
+        public class SecondDone
+        {
+            public class TicTacToe
+            {
+
+                private int[] rows;
+                private int[] cols;
+                private int diagonal;
+                private int antidiagonal;
+                private int n;
+
+                public TicTacToe(int n)
+                {
+                    rows = new int[n];
+                    cols = new int[n];
+                    this.n = n;
+                }
+
+                public int Move(int row, int col, int player)
+                {
+                    int val = player == 1 ? 1 : -1;
+                    rows[row] += val;
+                    cols[col] += val;
+                    if (row - col == 0)
+                    { // diagonal
+                        diagonal += val;
+                    }
+                    if (row + col == n - 1)
+                    { // antidiagonal 
+                        antidiagonal += val;
+                    }
+
+                    if (rows[row] == n || cols[col] == n || diagonal == n || antidiagonal == n ||
+                        rows[row] == -n || cols[col] == -n || diagonal == -n || antidiagonal == -n)
+                    {
+                        return player;
+                    }
+
+                    return 0;
+                }
+            }
+        }
     }
 }
