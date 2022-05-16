cfc497b9e326bf6615e091e2ce14f56119251b41
barneyliu708 <barneyliu708@gmail.com>
2022-05-15 20:40:08 -0700

    ALGORITHM-317 LC378. Kth Smallest Element in a Sorted Matrix - 2nd done

--- Algorithm/CH10_ElementaryDataStructure/LC378KthSmallestElementInASortedMatrix.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC378KthSmallestElementInASortedMatrix.cs
@@ -24,5 +24,27 @@ namespace Algorithm.CH10_ElementaryDataStructure
             }
             return pq.Dequeue();
         }
+
+        public class SecondDone
+        {
+            public int KthSmallest(int[][] matrix, int k)
+            {
+                PriorityQueue<int, int> pq = new PriorityQueue<int, int>(Comparer<int>.Create((int x, int y) => {
+                    return y.CompareTo(x); // max-heap
+                }));
+                for (int i = 0; i < matrix.Length; i++)
+                {
+                    for (int j = 0; j < matrix[0].Length; j++)
+                    {
+                        pq.Enqueue(matrix[i][j], matrix[i][j]);
+                        if (pq.Count > k)
+                        {
+                            pq.Dequeue();
+                        }
+                    }
+                }
+                return pq.Dequeue();
+            }
+        }
     }
 }
