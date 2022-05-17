782ed9a30b0792740ce506b3921c6b69f140803d
barneyliu708 <barneyliu708@gmail.com>
2022-05-16 21:35:26 -0700

    ALGORITHM-319 LC767. Reorganize String - 2nd done

--- Algorithm/CH10_ElementaryDataStructure/LC767ReorganizeString.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC767ReorganizeString.cs
@@ -63,5 +63,61 @@ namespace Algorithm.CH10_ElementaryDataStructure
 
             return sb.ToString();
         }
+
+        public class SecondDone
+        {
+            public string ReorganizeString(string s)
+            {
+                Dictionary<char, int> map = new Dictionary<char, int>();
+                foreach (char ch in s)
+                {
+                    if (!map.ContainsKey(ch))
+                    {
+                        map[ch] = 0;
+                    }
+                    map[ch]++;
+                }
+
+                PriorityQueue<char, int> pq = new PriorityQueue<char, int>(Comparer<int>.Create((int x, int y) => {
+                    return y.CompareTo(x); // max-heap
+                }));
+                foreach (char ch in map.Keys)
+                {
+                    if (map[ch] > (s.Length + 1) / 2)
+                    {
+                        return "";
+                    }
+                    pq.Enqueue(ch, map[ch]);
+                }
+
+                StringBuilder sb = new StringBuilder();
+                while (pq.Count >= 2)
+                {
+                    char ch1, ch2;
+                    int cnt1, cnt2;
+                    pq.TryDequeue(out ch1, out cnt1);
+                    pq.TryDequeue(out ch2, out cnt2);
+
+                    sb.Append(ch1);
+                    sb.Append(ch2);
+
+                    if (--cnt1 > 0)
+                    {
+                        pq.Enqueue(ch1, cnt1);
+                    }
+                    if (--cnt2 > 0)
+                    {
+                        pq.Enqueue(ch2, cnt2);
+                    }
+                }
+
+                if (pq.Count == 1)
+                {
+                    sb.Append(pq.Dequeue());
+                }
+
+                return sb.ToString();
+            }
+        }
     }
 }
