a8b4a310e62bd330668ea28a37f0794cd72ed9e0
barneyliu708 <barneyliu708@gmail.com>
2022-05-16 22:01:04 -0700

    ALGORITHM-362 LC895. Maximum Frequency Stack - stack / priority queue
    solution: https://leetcode.com/problems/maximum-frequency-stack/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC895MaximumFrequencyStack.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC895MaximumFrequencyStack
+    {
+        public class FreqStack
+        {
+
+            private Dictionary<int, int> freq;
+            private Dictionary<int, Stack<int>> grp;
+            private int maxfreq;
+
+            public FreqStack()
+            {
+                freq = new Dictionary<int, int>();
+                grp = new Dictionary<int, Stack<int>>();
+                maxfreq = 0;
+            }
+
+            public void Push(int val)
+            {
+                if (!freq.ContainsKey(val))
+                {
+                    freq[val] = 0;
+                }
+                freq[val]++;
+
+                if (freq[val] > maxfreq)
+                {
+                    maxfreq = freq[val];
+                }
+
+                if (!grp.ContainsKey(freq[val]))
+                {
+                    grp[freq[val]] = new Stack<int>();
+                }
+                grp[freq[val]].Push(val);
+            }
+
+            public int Pop()
+            {
+                int val = grp[maxfreq].Pop();
+                freq[val]--;
+                if (grp[maxfreq].Count == 0)
+                {
+                    maxfreq--;
+                }
+                return val;
+            }
+        }
+
+    }
+}
