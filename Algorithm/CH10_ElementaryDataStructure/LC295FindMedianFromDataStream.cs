5a8255fe7b1220b556a1e607ebb4e8670291f50c
barneyliu708 <barneyliu708@gmail.com>
2022-05-16 20:18:56 -0700

    ALGORITHM-318 LC295. Find Median from Data Stream - 2nd done

--- Algorithm/CH10_ElementaryDataStructure/LC295FindMedianFromDataStream.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC295FindMedianFromDataStream.cs
@@ -40,5 +40,43 @@ namespace Algorithm.CH10_ElementaryDataStructure
                 return maxheap.Count > minheap.Count ? maxheap.Peek() : ((double)maxheap.Peek() + (double)minheap.Peek()) / 2;
             }
         }
+
+        public class SecondDone
+        {
+            public class MedianFinder
+            {
+
+                private PriorityQueue<int, int> lo;
+                private PriorityQueue<int, int> hi;
+                public MedianFinder()
+                {
+                    lo = new PriorityQueue<int, int>(Comparer<int>.Create((int x, int y) => {
+                        return y.CompareTo(x);
+                    })); // max-heap
+                    hi = new PriorityQueue<int, int>(); // min-heap
+                }
+
+                public void AddNum(int num)
+                {
+                    lo.Enqueue(num, num);
+
+                    // rebalancing
+                    hi.Enqueue(lo.Peek(), lo.Peek());
+                    lo.Dequeue();
+
+                    if (lo.Count < hi.Count)
+                    {
+                        lo.Enqueue(hi.Peek(), hi.Peek());
+                        hi.Dequeue();
+                    }
+                }
+
+                public double FindMedian()
+                {
+                    return lo.Count > hi.Count ? lo.Peek() : (double)(lo.Peek() + hi.Peek()) / 2;
+                }
+            }
+
+        }
     }
 }
