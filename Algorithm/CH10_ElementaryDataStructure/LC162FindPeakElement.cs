282273ede1923b9730d64b6383a5c320c9267bd2
barneyliu708 <barneyliu708@gmail.com>
2022-05-17 22:10:05 -0700

    ALGORITHM-195 LC162. Find Peak Element - 2nd done

--- Algorithm/CH10_ElementaryDataStructure/LC162FindPeakElement.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC162FindPeakElement.cs
@@ -27,5 +27,28 @@ namespace Algorithm.CH10_ElementaryDataStructure
 
             return end;
         }
+
+        public class SecondDone
+        {
+            public int FindPeakElement(int[] nums)
+            {
+                int l = 0;
+                int r = nums.Length - 1;
+                while (l <= r)
+                {
+                    int mid = l + (r - l) / 2;
+                    if (mid == (nums.Length - 1) || nums[mid] < nums[mid + 1])
+                    {
+                        l = mid + 1;
+                    }
+                    else
+                    {
+                        r = mid - 1;
+                    }
+                }
+
+                return l == nums.Length ? l - 1 : l;
+            }
+        }
     }
 }
