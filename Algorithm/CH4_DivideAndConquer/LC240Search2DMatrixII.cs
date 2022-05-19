e3de6f237a435fa7b0e87d30de7dce7fd275fcff
barneyliu708 <barneyliu708@gmail.com>
2022-05-18 21:19:58 -0700

    ALGORITHM-25 LC240 Search a 2D Matrix II - 2nd done

--- Algorithm/CH4_DivideAndConquer/LC240Search2DMatrixII.cs
+++ Algorithm/CH4_DivideAndConquer/LC240Search2DMatrixII.cs
@@ -42,5 +42,38 @@ namespace Algorithm.CH4_DivideAndConque
                    SearchMatrix(matrix, target, l, (l + r) / 2, (u + d) / 2 + 1, d) ||
                    SearchMatrix(matrix, target, (l + r) / 2 + 1, r, (u + d) / 2 + 1, d);
         }
+
+        public class SecondDone
+        {
+            public bool SearchMatrix(int[][] matrix, int target)
+            {
+                int m = matrix.Length;
+                int n = matrix[0].Length;
+                return SearchMatrix(matrix, target, 0, n - 1, 0, m - 1);
+            }
+
+            private bool SearchMatrix(int[][] matrix, int target, int l, int r, int u, int d)
+            {
+                if (l > r || u > d)
+                {
+                    return false;
+                }
+
+                if (target < matrix[u][l] || target > matrix[d][r])
+                {
+                    return false;
+                }
+
+                if (l == r && u == d)
+                {
+                    return target == matrix[u][l];
+                }
+
+                return SearchMatrix(matrix, target, l, (l + r) / 2, u, (u + d) / 2) ||
+                       SearchMatrix(matrix, target, (l + r) / 2 + 1, r, u, (u + d) / 2) ||
+                       SearchMatrix(matrix, target, l, (l + r) / 2, (u + d) / 2 + 1, d) ||
+                       SearchMatrix(matrix, target, (l + r) / 2 + 1, r, (u + d) / 2 + 1, d);
+            }
+        }
     }
 }
