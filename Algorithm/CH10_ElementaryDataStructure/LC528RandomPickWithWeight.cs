9ddf694a900b9c7ecc5513ecc571f74a87ed90c3
barneyliu708 <barneyliu708@gmail.com>
2022-05-19 21:05:28 -0700

    ALGORITHM-320 LC528. Random Pick with Weight - 2nd done

--- Algorithm/CH10_ElementaryDataStructure/LC528RandomPickWithWeight.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC528RandomPickWithWeight.cs
@@ -48,5 +48,52 @@ namespace Algorithm.CH10_ElementaryDataStructure
                 return l;
             }
         }
+
+        public class SecondDone
+        {
+            public class Solution
+            {
+
+                private int[] presum;
+                private int total;
+                private Random rand;
+                public Solution(int[] w)
+                {
+                    rand = new Random();
+                    presum = new int[w.Length];
+                    int pre = 0;
+                    for (int i = 0; i < w.Length; i++)
+                    {
+                        pre += w[i];
+                        presum[i] = pre;
+                    }
+                    total = pre;
+                }
+
+                public int PickIndex()
+                {
+                    int target = rand.Next(total) + 1; // return [1, total]
+                    int l = 0;
+                    int r = presum.Length - 1;
+                    while (l <= r)
+                    {
+                        int mid = l + (r - l) / 2;
+                        if (presum[mid] == target)
+                        {
+                            return mid;
+                        }
+                        if (presum[mid] < target)
+                        {
+                            l = mid + 1;
+                        }
+                        else
+                        {
+                            r = mid - 1;
+                        }
+                    }
+                    return l;
+                }
+            }
+        }
     }
 }
