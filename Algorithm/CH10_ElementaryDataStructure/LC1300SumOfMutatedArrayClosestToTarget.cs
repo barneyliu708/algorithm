3adca08823f6e4ba3ec8875193d0cbf21a125773
barneyliu708 <barneyliu708@gmail.com>
2022-05-19 21:54:43 -0700

    ALGORITHM-324 LC1300. Sum of Mutated Array Closest to Target - 2nd done

--- Algorithm/CH10_ElementaryDataStructure/LC1300SumOfMutatedArrayClosestToTarget.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC1300SumOfMutatedArrayClosestToTarget.cs
@@ -81,5 +81,76 @@ namespace Algorithm.CH10_ElementaryDataStructure
             }
             return l;
         }
+
+        public class SecondDone
+        {
+            public int FindBestValue(int[] arr, int target)
+            {
+                Array.Sort(arr);
+                int n = arr.Length;
+
+                // calculate the prefix sum
+                int[] presum = new int[n + 1];
+                for (int i = 0; i < n; i++)
+                {
+                    presum[i + 1] = presum[i] + arr[i]; // presum[i] = arr[0] + arr[1] + ... + arr[i - 1] 
+                }
+
+                // find the best value
+                int lv = 0;
+                int rv = arr[n - 1];
+                int ansSum = presum[n];
+                int ansVal = arr[n - 1];
+                while (lv <= rv)
+                {
+                    int mv = lv + (rv - lv) / 2;
+                    int i = FindIndex(arr, mv); // i = [0, n];
+                    int sum = presum[i] + mv * (n - i);
+
+                    if (sum == target)
+                    {
+                        return mv;
+                    }
+                    if (sum < target)
+                    {
+                        lv = mv + 1;
+                    }
+                    else
+                    {
+                        rv = mv - 1;
+                    }
+
+                    // update the ans
+                    if (Math.Abs(sum - target) < Math.Abs(ansSum - target) ||
+                        (Math.Abs(sum - target) == Math.Abs(ansSum - target) && mv < ansVal))
+                    {
+                        ansSum = sum;
+                        ansVal = mv;
+                    }
+                }
+
+                return ansVal;
+            }
+
+            // find the first element which is greater or equal to the element
+            private int FindIndex(int[] arr, int target)
+            {
+                int l = 0;
+                int r = arr.Length - 1;
+                while (l <= r)
+                {
+                    int mid = (l + r) / 2;
+                    if (arr[mid] <= target)
+                    {
+                        l = mid + 1;
+                    }
+                    else
+                    {
+                        r = mid - 1;
+                    }
+                }
+                return l;
+            }
+        }
     }
 }
