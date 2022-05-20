f298c32227aa173dca80223347cca6439cf24d0b
barneyliu708 <barneyliu708@gmail.com>
2022-05-19 22:02:24 -0700

    ALGORITHM-325 LC1060. Missing Element in Sorted Array - 2nd done

--- Algorithm/CH10_ElementaryDataStructure/LC1060MissingElementInSortedArray.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC1060MissingElementInSortedArray.cs
@@ -38,5 +38,37 @@ namespace Algorithm.CH10_ElementaryDataStructure
         {
             return nums[i] - nums[0] - i;
         }
+
+        public class SecondDone
+        {
+            public int MissingElement(int[] nums, int k)
+            {
+                int l = 0;
+                int r = nums.Length - 1;
+                while (l <= r)
+                {
+                    int mid = l + (r - l) / 2;
+                    int count = MissingCount(nums, mid);
+                    if (count == k)
+                    { // need to find the leftmost potential index of target
+                        r = mid - 1;
+                    }
+                    else if (count > k)
+                    {
+                        r = mid - 1;
+                    }
+                    else
+                    {
+                        l = mid + 1;
+                    }
+                }
+                return nums[r] + k - MissingCount(nums, r);
+            }
+
+            private int MissingCount(int[] nums, int i)
+            {
+                return nums[i] - nums[0] - i;
+            }
+        }
     }
 }
