113a845ce897875ef7fe7db1820523bd4cd9233d
barneyliu708 <barneyliu708@gmail.com>
2022-05-23 18:48:53 -0700

    ALGORITHM-120 LC279. Perfect Squares - 2nd done

--- Algorithm/CH10_ElementaryDataStructure/LC279PerfectSquares.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC279PerfectSquares.cs
@@ -38,5 +38,37 @@ namespace Algorithm.CH10_ElementaryDataStructure
 
             return dp[n];
         }
+
+        public class SecondDone
+        {
+            public int NumSquares(int n)
+            {
+                int[] dp = new int[n + 1];
+                for (int i = 0; i <= n; i++)
+                {
+                    dp[i] = int.MaxValue;
+                }
+                int[] squars = new int[(int)Math.Sqrt(n) + 1];
+                for (int i = 0; i < squars.Length; i++)
+                {
+                    squars[i] = i * i;
+                }
+
+                dp[0] = 0;
+                for (int i = 1; i <= n; i++)
+                {
+                    for (int s = 1; s < squars.Length; s++)
+                    {
+                        if (i < squars[s])
+                        {
+                            break;
+                        }
+                        dp[i] = Math.Min(dp[i], dp[i - squars[s]] + 1);
+                    }
+                }
+
+                return dp[n];
+            }
+        }
     }
 }
