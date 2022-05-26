0e44e1f6978b8332c2feb6298fb2a34d73dcfcde
barneyliu708 <barneyliu708@gmail.com>
2022-05-25 17:19:46 -0700

    ALGORITHM-146 LC166. Fraction to Recurring Decimal - 2nd done

--- Algorithm/CH10_ElementaryDataStructure/LC166FractionToRecurringDecimal.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC166FractionToRecurringDecimal.cs
@@ -49,5 +49,50 @@ namespace Algorithm.CH10_ElementaryDataStructure
 
             return sb.ToString();
         }
+
+        public string FractionToDecimal(int numerator, int denominator)
+        {
+
+            if (numerator == 0)
+            {
+                return "0";
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            if (numerator < 0 ^ denominator < 0)
+            {
+                sb.Append('-');
+            }
+
+            long dividend = Math.Abs((long)numerator);
+            long dividor = Math.Abs((long)denominator);
+
+            sb.Append((dividend / dividor).ToString());
+            dividend = dividend % dividor;
+
+            if (dividend == 0)
+            {
+                return sb.ToString();
+            }
+
+            sb.Append('.');
+            Dictionary<long, int> map = new Dictionary<long, int>(); // remainder - sb length
+            while (dividend != 0)
+            {
+                if (map.ContainsKey(dividend))
+                {
+                    sb.Insert(map[dividend], '(');
+                    sb.Append(')');
+                    break;
+                }
+                map[dividend] = sb.Length;
+                dividend *= 10;
+                sb.Append((dividend / dividor).ToString());
+                dividend = dividend % dividor;
+            }
+
+            return sb.ToString();
+        }
     }
 }
