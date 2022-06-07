d1810bcfa6b55b91315b527fb948d36763c19b47
barneyliu708 <barneyliu708@gmail.com>
2022-06-07 15:44:35 -0700

    ALGORITHM-278 LC938. Range Sum of BST - 2nd done

--- Algorithm/CH10_ElementaryDataStructure/LC938RangeSumOfBST.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC938RangeSumOfBST.cs
@@ -47,5 +47,27 @@ namespace Algorithm.CH10_ElementaryDataStructure
 
             return sum;
         }
+
+        public class SecondDone
+        {
+            public int RangeSumBST(TreeNode root, int low, int high)
+            {
+                if (root == null)
+                {
+                    return 0;
+                }
+
+                int sum = 0;
+                if (root.val >= low && root.val <= high)
+                {
+                    sum += root.val;
+                }
+
+                sum += RangeSumBST(root.left, low, high);
+                sum += RangeSumBST(root.right, low, high);
+
+                return sum;
+            }
+        }
     }
 }
