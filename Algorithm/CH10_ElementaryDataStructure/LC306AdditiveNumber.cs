336a4d10b68d8bc354af0cbdeccdb5e9a7ad7a68
barneyliu708 <barneyliu708@gmail.com>
2022-06-12 21:09:04 -0700

    ALGORITHM-276 LC306. Additive Number - 2nd done

--- Algorithm/CH10_ElementaryDataStructure/LC306AdditiveNumber.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC306AdditiveNumber.cs
@@ -49,5 +49,64 @@ namespace Algorithm.CH10_ElementaryDataStructure
 
             return false;
         }
+
+        public class SecondDone
+        {
+            public bool IsAdditiveNumber(string num)
+            {
+                List<long> cur = new List<long>();
+                return Backtracking(num, 0, cur);
+            }
+            // return -1 if is not additive num, otherwise, return the next num 
+            private bool Backtracking(string num, int istart, List<long> cur)
+            {
+
+                if (istart == num.Length)
+                {
+                    return cur.Count >= 3;
+                }
+
+                if (cur.Count < 2)
+                {
+                    bool isAdditive = false;
+                    for (int i = istart; i < num.Length; i++)
+                    {
+                        if (num[istart] == '0' && istart != i)
+                        { // num with leading 0
+                            break;
+                        }
+                        long nextNum;
+                        if (!long.TryParse(num.Substring(istart, i - istart + 1), out nextNum))
+                        {
+                            continue;
+                        }
+                        // Console.WriteLine(nextNum);
+                        cur.Add(nextNum);
+                        isAdditive = isAdditive || Backtracking(num, i + 1, cur);
+                        cur.RemoveAt(cur.Count - 1);
+                    }
+                    return isAdditive;
+                }
+                else
+                {
+                    long expectedNextNum = cur[cur.Count - 1] + cur[cur.Count - 2];
+                    int expectedNextNumLen = expectedNextNum.ToString().Length;
+                    if (istart + expectedNextNumLen - 1 >= num.Length)
+                    { // num[istart : num.Length - 1] is not long enought
+                        return false;
+                    }
+                    long nextNum = long.Parse(num.Substring(istart, expectedNextNumLen));
+                    if (nextNum != expectedNextNum)
+                    {
+                        return false;
+                    }
+                    // Console.WriteLine(cur[cur.Count - 2] + " " + cur[cur.Count - 1]);
+                    cur.Add(nextNum);
+                    bool ans = Backtracking(num, istart + expectedNextNumLen, cur);
+                    cur.RemoveAt(cur.Count - 1);
+                    return ans;
+                }
+            }
+        }
     }
 }
