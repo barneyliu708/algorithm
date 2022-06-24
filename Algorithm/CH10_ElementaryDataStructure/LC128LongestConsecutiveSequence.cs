ff6b9cbabb47c0b503642bf6b99f5f23f5f8f78a
barneyliu708 <barneyliu708@gmail.com>
2022-06-23 21:03:35 -0700

    ALGORITHM-138 LC128. Longest Consecutive Sequence - 3rd done

--- Algorithm/CH10_ElementaryDataStructure/LC128LongestConsecutiveSequence.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC128LongestConsecutiveSequence.cs
@@ -65,5 +65,93 @@ namespace Algorithm.CH10_ElementaryDataStructure
                 return ans;
             }
         }
+
+        public class ThirdDone_UnionFind
+        {
+            public class Solution
+            {
+                public int LongestConsecutive(int[] nums)
+                {
+                    Dictionary<int, int> map = new Dictionary<int, int>(); // num value - index
+                    DSU dsu = new DSU(nums.Length);
+                    for (int i = 0; i < nums.Length; i++)
+                    {
+                        int num = nums[i];
+                        if (map.ContainsKey(num))
+                        {
+                            continue;
+                        }
+                        if (map.ContainsKey(num - 1))
+                        {
+                            dsu.Union(i, map[num - 1]);
+                        }
+                        if (map.ContainsKey(num + 1))
+                        {
+                            dsu.Union(i, map[num + 1]);
+                        }
+                        map[num] = i;
+                    }
+                    return dsu.GetMaxSize();
+                }
+            }
+
+            public class DSU
+            {
+                private int[] representatives; // the index of elements
+                private int[] size;
+
+                public DSU(int sz)
+                {
+                    representatives = new int[sz];
+                    size = new int[sz];
+                    for (int i = 0; i < sz; i++)
+                    {
+                        representatives[i] = i;
+                        size[i] = 1;
+                    }
+                }
+
+                public int GetRepresentative(int i)
+                {
+                    if (representatives[i] == i)
+                    {
+                        return i;
+                    }
+                    return representatives[i] = GetRepresentative(representatives[i]);
+                }
+
+                public void Union(int ia, int ib)
+                {
+                    int iarep = GetRepresentative(ia);
+                    int ibrep = GetRepresentative(ib);
+
+                    if (iarep == ibrep)
+                    {
+                        return;
+                    }
+
+                    if (size[iarep] >= size[ibrep])
+                    {
+                        size[iarep] += size[ibrep];
+                        representatives[ibrep] = iarep;
+                    }
+                    else
+                    {
+                        size[ibrep] += size[iarep];
+                        representatives[iarep] = ibrep;
+                    }
+                }
+
+                public int GetMaxSize()
+                {
+                    int max = 0;
+                    foreach (int sz in size)
+                    {
+                        max = Math.Max(max, sz);
+                    }
+                    return max;
+                }
+            }
+        }
     }
 }
