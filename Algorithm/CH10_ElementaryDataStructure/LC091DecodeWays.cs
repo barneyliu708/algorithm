4127aa85d21b319fe5e14dc1eceb2311e8724e4b
barneyliu708 <barneyliu708@gmail.com>
2022-07-07 20:59:17 -0700

    ALGORITHM-102 LC091. Decode Ways - 3rd done

--- Algorithm/CH10_ElementaryDataStructure/LC091DecodeWays.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC091DecodeWays.cs
@@ -73,5 +73,44 @@ namespace Algorithm.CH10_ElementaryDataStructure
                 return dp[s.Length];
             }
         }
+
+        public class ThirdDone
+        {
+            public int NumDecodings(string s)
+            {
+                int[] dp = new int[s.Length];
+                if (s[0] == '0')
+                {
+                    return 0;
+                }
+                if (s.Length == 1)
+                {
+                    return 1;
+                }
+                dp[0] = 1;
+                if (s[1] != '0')
+                {
+                    dp[1] = dp[0];
+                }
+                if (int.Parse(s.Substring(0, 2)) <= 26)
+                {
+                    dp[1] += 1;
+                }
+
+                for (int i = 2; i < s.Length; i++)
+                {
+                    if (s[i] != '0')
+                    {
+                        dp[i] = dp[i - 1];
+                    }
+                    if (s[i - 1] != '0' && int.Parse(s.Substring(i - 1, 2)) <= 26)
+                    {
+                        dp[i] += dp[i - 2];
+                    }
+                }
+
+                return dp[s.Length - 1];
+            }
+        }
     }
 }
