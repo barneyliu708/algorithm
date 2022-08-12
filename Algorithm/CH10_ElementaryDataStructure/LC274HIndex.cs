1b76ad9dbb013b7e6fb3d9673ba9802d44e210bc
barneyliu708 <barneyliu708@gmail.com>
2022-08-11 20:48:09 -0700

    ALGORITHM-247 LC274. H-Index - 2nd done - binary search

--- Algorithm/CH10_ElementaryDataStructure/LC274HIndex.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC274HIndex.cs
@@ -23,5 +23,32 @@ namespace Algorithm.CH10_ElementaryDataStructure
 
             return h;
         }
+
+        public class SecondDone_BinarySearch
+        {
+            public int HIndex(int[] citations)
+            {
+
+                Array.Sort(citations, (int x, int y) => {
+                    return y.CompareTo(x);
+                });
+                int l = 0;
+                int r = citations.Length - 1;
+                while (l <= r)
+                {
+                    int h = l + (r - l) / 2;
+                    if (citations[h] > h)
+                    {
+                        l = h + 1;
+                    }
+                    else
+                    {
+                        r = h - 1;
+                    }
+                }
+
+                return l;
+            }
+        }
     }
 }
