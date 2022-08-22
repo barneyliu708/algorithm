6f51d28c0c18fe403755f34212606a945ee206a3
barneyliu708 <barneyliu708@gmail.com>
2022-08-21 17:48:10 -0700

    ALGORITHM-26 LC315 Count of Smaller Numbers After Self - merge sort
    solution 3 merge sort: https://leetcode.com/problems/count-of-smaller-numbers-after-self/solution/

---
+++ Algorithm/CH10_ElementaryDataStructure/LC315CountOfSmallerNumbersAfterSelf.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC315CountOfSmallerNumbersAfterSelf
+    {
+        public IList<int> CountSmaller(int[] nums)
+        {
+            int n = nums.Length;
+            int[] results = new int[n];
+            int[] indices = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                indices[i] = i;
+            }
+            MergeSort(nums, indices, 0, n - 1, results);
+            // Console.WriteLine(string.Join(", ", indices));
+
+            return results.ToList();
+        }
+
+        private void MergeSort(int[] nums, int[] indices, int l, int r, int[] results)
+        {
+            if (l >= r)
+            {
+                return;
+            }
+
+            int mid = l + (r - l) / 2;
+            MergeSort(nums, indices, l, mid, results);
+            MergeSort(nums, indices, mid + 1, r, results);
+
+            Merge(nums, indices, l, mid, r, results);
+        }
+
+        private void Merge(int[] nums, int[] indices, int l, int mid, int r, int[] results)
+        {
+            int i = l;
+            int j = mid + 1;
+            List<int> temp = new List<int>();
+            while (i <= mid && j <= r)
+            {
+                if (nums[indices[i]] <= nums[indices[j]])
+                { // must include the equal as well
+                    results[indices[i]] += j - mid - 1;
+                    temp.Add(indices[i]);
+                    i++;
+                }
+                else
+                {
+                    temp.Add(indices[j]);
+                    j++;
+                }
+            }
+
+            while (i <= mid)
+            {
+                results[indices[i]] += j - mid - 1;
+                temp.Add(indices[i]);
+                i++;
+            }
+
+            while (j <= r)
+            {
+                temp.Add(indices[j]);
+                j++;
+            }
+
+            for (int k = l; k <= r; k++)
+            {
+                indices[k] = temp[k - l];
+            }
+        }
+    }
+}
