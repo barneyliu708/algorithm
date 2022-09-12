5ab69f2a7dae01fd27527b803dec7180b9191f85
barneyliu708 <barneyliu708@gmail.com>
2022-09-11 22:14:32 -0700

    ALGORITHM-335 LC772. Basic Calculator III - stack

--- Algorithm/CH10_ElementaryDataStructure/LC772BasicCalculatorIII.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC772BasicCalculatorIII.cs
@@ -62,5 +62,71 @@ namespace Algorithm.CH10_ElementaryDataStructure
 
             return ans;
         }
+
+        public class SecondDone
+        {
+            private Dictionary<char, Action<Stack<int>, int>> operations = new Dictionary<char, Action<Stack<int>, int>>() {
+                { '+', (Stack<int> s, int operand) => { s.Push(operand); }},
+                { '-', (Stack<int> s, int operand) => { s.Push(-operand); }},
+                { '*', (Stack<int> s, int operand) => { s.Push(s.Pop() * operand); }},
+                { '/', (Stack<int> s, int operand) => { s.Push(s.Pop() / operand); }}
+            };
+            public int Calculate(string s)
+            {
+
+
+                Stack<Stack<int>> levels = new Stack<Stack<int>>(); // stack of each layer's score
+                levels.Push(new Stack<int>());
+
+                int operand = 0;
+                char sign = '+'; // store the sign before the operand
+                for (int i = 0; i < s.Length; i++)
+                {
+
+                    char ch = s[i];
+                    if (ch == '(')
+                    {
+                        levels.Push(new Stack<int>()); // start a new level, store the sign from curent level into the new level
+                        levels.Peek().Push(sign);
+                        sign = '+';
+                    }
+                    else if (ch == ')')
+                    {
+                        // push the last operand and its sign in the expression to the current stack
+                        operations[sign](levels.Peek(), operand);
+                        int subtotal = 0;
+                        while (levels.Peek().Count > 1)
+                        { // the last element is the sign from the parent level
+                            subtotal += levels.Peek().Pop();
+                        }
+                        sign = (char)levels.Peek().Pop();
+                        // Console.WriteLine(sign + " " + subtotal);
+                        levels.Pop(); // pop the current child level and return the subtotal to the parent level
+                        operand = subtotal;
+                    }
+                    else if (char.IsDigit(ch))
+                    {
+                        operand = operand * 10 + (ch - '0');
+                    }
+                    else
+                    { // ch is a sign 
+                        operations[sign](levels.Peek(), operand);
+                        sign = ch;
+                        operand = 0;
+                    }
+                }
+
+                // push the last operand and its sign in the expression to the stack
+                operations[sign](levels.Peek(), operand);
+
+                int ans = 0;
+                while (levels.Peek().Count > 0)
+                {
+                    ans += levels.Peek().Pop();
+                }
+
+                return ans;
+            }
+        }
     }
 }
