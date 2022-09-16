b338c1c8b9658eba44ccbf788a41415ff257f4bb
barneyliu708 <barneyliu708@gmail.com>
2022-09-15 19:42:06 -0700

    ALGORITHM-47 LC032 Longest Valid Parentheses - 2nd done

--- Algorithm/CH10_ElementaryDataStructure/LC032LongestValidParentheses.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC032LongestValidParentheses.cs
@@ -35,5 +35,36 @@ namespace Algorithm.CH10_ElementaryDataStructure
             }
             return maxlen;
         }
+
+        public class SecondDone
+        {
+            public int LongestValidParentheses(string s)
+            {
+                int ans = 0;
+                Stack<int> stack = new Stack<int>();
+                stack.Push(-1);
+                for (int i = 0; i < s.Length; i++)
+                {
+                    if (s[i] == '(')
+                    {
+                        stack.Push(i);
+                    }
+                    else
+                    {
+                        stack.Pop();
+                        if (stack.Count == 0)
+                        {
+                            stack.Push(i);
+                        }
+                        else
+                        {
+                            ans = Math.Max(ans, i - stack.Peek());
+                        }
+                    }
+                }
+
+                return ans;
+            }
+        }
     }
 }
