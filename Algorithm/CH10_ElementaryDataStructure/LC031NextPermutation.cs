5b0dc91b145617499c67aee52bceb0d173eb512f
barneyliu708 <barneyliu708@gmail.com>
2022-09-15 19:01:56 -0700

    ALGORITHM-209 LC031. Next Permutation - 2nd done

--- Algorithm/CH10_ElementaryDataStructure/LC031NextPermutation.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC031NextPermutation.cs
@@ -43,5 +43,55 @@ namespace Algorithm.CH10_ElementaryDataStructure
                 j--;
             }
         }
+
+        public class SecondDone
+        {
+            public void NextPermutation(int[] nums)
+            {
+                if (nums.Length <= 1)
+                {
+                    return;
+                }
+
+                int i = nums.Length - 2;
+                while (i >= 0 && nums[i] >= nums[i + 1])
+                {
+                    i--;
+                }
+
+                if (i == -1)
+                {
+                    Reverse(nums, 0, nums.Length - 1);
+                    return;
+                }
+
+                int j = i + 1;
+                while (j < nums.Length && nums[j] > nums[i])
+                {
+                    j++;
+                }
+                j--;
+
+                Swap(nums, i, j);
+                Reverse(nums, i + 1, nums.Length - 1);
+            }
+
+            private void Swap(int[] nums, int i, int j)
+            {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+            }
+
+            private void Reverse(int[] nums, int i, int j)
+            {
+                while (i < j)
+                {
+                    Swap(nums, i, j);
+                    i++;
+                    j--;
+                }
+            }
+        }
     }
 }
