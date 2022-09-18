14051fc662746b137b563cc545ec1ea05c5842b4
barneyliu708 <barneyliu708@gmail.com>
2022-09-17 21:07:14 -0700

    ALGORITHM-260 LC040. Combination Sum II - 2nd done

--- Algorithm/CH10_ElementaryDataStructure/LC040CombinationSumII.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC040CombinationSumII.cs
@@ -39,5 +39,45 @@ namespace Algorithm.CH10_ElementaryDataStructure
                 comb.Remove(candidates[i]);
             }
         }
+
+
+        public class SecondDone
+        {
+            public IList<IList<int>> CombinationSum2(int[] candidates, int target)
+            {
+                Array.Sort(candidates);
+
+                List<int> comb = new List<int>();
+                List<IList<int>> ans = new List<IList<int>>();
+                Utility(candidates, target, 0, comb, ans);
+
+                return ans;
+            }
+
+            private void Utility(int[] candidates, int target, int istart, List<int> comb, List<IList<int>> ans)
+            {
+                // check target first. the result could be added at the last element which may meet istart == candidates.Length
+                if (target == 0)
+                {
+                    ans.Add(new List<int>(comb));
+                    return;
+                }
+
+                if (istart == candidates.Length || target < 0)
+                {
+                    return;
+                }
+
+                for (int i = istart; i < candidates.Length; i++)
+                {
+                    if (i == istart || candidates[i] != candidates[i - 1])
+                    {
+                        comb.Add(candidates[i]);
+                        Utility(candidates, target - candidates[i], i + 1, comb, ans);
+                        comb.RemoveAt(comb.Count - 1);
+                    }
+                }
+            }
+        }
     }
 }
