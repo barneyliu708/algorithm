52d6175336222aff07922131080f0a3d17748fe7
barneyliu708 <barneyliu708@gmail.com>
2022-09-22 20:18:11 -0700

    ALGORITHM-193 LC081. Search in Rotated Sorted Array II - 3rd done

--- Algorithm/CH10_ElementaryDataStructure/LC081SearchInRotatedSortedArrayII.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC081SearchInRotatedSortedArrayII.cs
@@ -107,5 +107,57 @@ namespace Algorithm.CH10_ElementaryDataStructure
                 return false;
             }
         }
+
+        public class ThirdDone
+        {
+            public bool Search(int[] nums, int target)
+            {
+                int l = 0;
+                int r = nums.Length - 1;
+                while (l <= r && nums[l] == nums[r])
+                {
+                    if (nums[l] == target)
+                    {
+                        return true;
+                    }
+                    l++;
+                    r--;
+                }
+
+                while (l <= r)
+                {
+                    int mid = l + (r - l) / 2;
+                    if (nums[mid] == target)
+                    {
+                        return true;
+                    }
+
+                    if (nums[mid] >= nums[l])
+                    {
+                        if (target >= nums[l] && target < nums[mid])
+                        {
+                            r = mid - 1;
+                        }
+                        else
+                        {
+                            l = mid + 1;
+                        }
+                    }
+                    else
+                    {
+                        if (target > nums[mid] && target <= nums[r])
+                        {
+                            l = mid + 1;
+                        }
+                        else
+                        {
+                            r = mid - 1;
+                        }
+                    }
+                }
+
+                return false;
+            }
+        }
     }
 }
