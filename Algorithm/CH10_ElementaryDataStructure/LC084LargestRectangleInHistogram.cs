9acfb675f6b0eca50dcb05ba4d21d0a7f6fa70f7
barneyliu708 <barneyliu708@gmail.com>
2022-09-24 14:59:30 -0700

    ALGORITHM-50 LC084 Largest Rectangle in Histogram - second done

--- Algorithm/CH10_ElementaryDataStructure/LC084LargestRectangleInHistogram.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC084LargestRectangleInHistogram.cs
@@ -32,5 +32,35 @@ namespace Algorithm.CH10_ElementaryDataStructure
 
             return maxArea;
         }
+
+        public class SecondDone
+        {
+            public int LargestRectangleArea(int[] heights)
+            {
+                Stack<int> stack = new Stack<int>(); // stack of index
+                stack.Push(-1);
+
+                int ans = 0;
+                for (int i = 0; i < heights.Length; i++)
+                {
+                    while (stack.Peek() != -1 && heights[stack.Peek()] >= heights[i])
+                    {
+                        int curHeight = heights[stack.Pop()];
+                        int curWidth = i - stack.Peek() - 1; // exclude both left and right boundary
+                        ans = Math.Max(ans, curHeight * curWidth);
+                    }
+                    stack.Push(i);
+                }
+
+                while (stack.Peek() != -1)
+                { // exclude the last element -1
+                    int curHeight = heights[stack.Pop()];
+                    int curWidth = heights.Length - stack.Peek() - 1;
+                    ans = Math.Max(ans, curHeight * curWidth);
+                }
+
+                return ans;
+            }
+        }
     }
 }
