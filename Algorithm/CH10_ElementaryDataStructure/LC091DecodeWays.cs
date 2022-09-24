f76ac17a4f1c8eca7b5b48c1b0738ac15d06be1f
barneyliu708 <barneyliu708@gmail.com>
2022-09-24 16:55:10 -0700

    ALGORITHM-102 LC091. Decode Ways - 4th done

--- Algorithm/CH10_ElementaryDataStructure/LC091DecodeWays.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC091DecodeWays.cs
@@ -112,5 +112,32 @@ namespace Algorithm.CH10_ElementaryDataStructure
                 return dp[s.Length - 1];
             }
         }
+
+        public class ForthDone
+        {
+            public int NumDecodings(string s)
+            {
+                int[] dp = new int[s.Length + 1];
+                dp[0] = 1;
+                if (s[0] == '0')
+                {
+                    return 0;
+                }
+                dp[1] += dp[0];
+                for (int i = 1; i < s.Length; i++)
+                {
+                    if (s[i] != '0')
+                    {
+                        dp[i + 1] += dp[i];
+                    }
+                    if (s[i - 1] != '0' && int.Parse(s.Substring(i - 1, 2)) <= 26)
+                    {
+                        dp[i + 1] += dp[i - 1];
+                    }
+                }
+
+                return dp[s.Length];
+            }
+        }
     }
 }
