45c8296b4bc2ee3d7799b7fc26a1d01b4ae2c32f
barneyliu708 <barneyliu708@gmail.com>
2022-09-25 22:13:05 -0700

    ALGORITHM-356 LC1031. Maximum Sum of Two Non-Overlapping Subarrays - 2nd done

--- Algorithm/CH10_ElementaryDataStructure/LC1031MaximumSumOfTwoNonOverlappingSubarrays.cs
+++ Algorithm/CH10_ElementaryDataStructure/LC1031MaximumSumOfTwoNonOverlappingSubarrays.cs
@@ -53,5 +53,65 @@ namespace Algorithm.CH10_ElementaryDataStructure
 
             return max;
         }
+
+        public class SecondDone
+        {
+            public int MaxSumTwoNoOverlap(int[] nums, int firstLen, int secondLen)
+            {
+                int n = nums.Length;
+                int[] sum1 = new int[n];
+                int[] sum2 = new int[n];
+                for (int i = 0; i < n; i++)
+                {
+                    if (i == 0)
+                    {
+                        sum1[i] = nums[i];
+                        sum2[i] = nums[i];
+                    }
+                    else
+                    {
+                        sum1[i] = sum1[i - 1] + nums[i];
+                        sum2[i] = sum2[i - 1] + nums[i];
+
+                        if (i >= firstLen)
+                        {
+                            sum1[i] -= nums[i - firstLen];
+                        }
+                        if (i >= secondLen)
+                        {
+                            sum2[i] -= nums[i - secondLen];
+                        }
+                    }
+                }
+
+                int[] rmax1 = new int[n];
+                int[] rmax2 = new int[n];
+                for (int i = n - 1; i >= 0; i--)
+                {
+                    if (i == n - 1)
+                    {
+                        rmax1[i] = sum1[i];
+                        rmax2[i] = sum2[i];
+                    }
+                    else
+                    {
+                        rmax1[i] = Math.Max(rmax1[i + 1], sum1[i]);
+                        rmax2[i] = Math.Max(rmax2[i + 1], sum2[i]);
+                    }
+                }
+
+                int ans = 0;
+                for (int i = firstLen - 1; i + secondLen < n; i++)
+                {
+                    ans = Math.Max(ans, sum1[i] + rmax2[i + secondLen]);
+                }
+                for (int i = secondLen - 1; i + firstLen < n; i++)
+                {
+                    ans = Math.Max(ans, sum2[i] + rmax1[i + firstLen]);
+                }
+
+                return ans;
+            }
+        }
     }
 }
