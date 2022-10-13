d1a9301b633643eddd20268070f113adde709871
barneyliu708 <barneyliu708@gmail.com>
2022-10-12 20:26:46 -0700

    ALGORITHM-25 LC240 Search a 2D Matrix II - 3rd done

--- Algorithm/CH4_DivideAndConquer/LC240Search2DMatrixII.cs
+++ Algorithm/CH4_DivideAndConquer/LC240Search2DMatrixII.cs
@@ -75,5 +75,36 @@ namespace Algorithm.CH4_DivideAndConque
                        SearchMatrix(matrix, target, (l + r) / 2 + 1, r, (u + d) / 2 + 1, d);
             }
         }
+
+        public class ThirdDone
+        {
+            public bool SearchMatrix(int[][] matrix, int target)
+            {
+                return SearchMatrix(matrix, target, (0, 0), (matrix.Length - 1, matrix[0].Length - 1));
+            }
+
+            public bool SearchMatrix(int[][] matrix, int target, (int i, int j) bottom, (int i, int j) top)
+            {
+                if (bottom.i > top.i || bottom.j > top.j)
+                {
+                    return false;
+                }
+                if (bottom.i == top.i && bottom.j == top.j)
+                {
+                    return matrix[bottom.i][bottom.j] == target;
+                }
+                if (matrix[bottom.i][bottom.j] > target || matrix[top.i][top.j] < target)
+                {
+                    return false;
+                }
+                int midi = bottom.i + (top.i - bottom.i) / 2;
+                int midj = bottom.j + (top.j - bottom.j) / 2;
+
+                return SearchMatrix(matrix, target, (bottom.i, bottom.j), (midi, midj)) ||
+                       SearchMatrix(matrix, target, (bottom.i, midj + 1), (midi, top.j)) ||
+                       SearchMatrix(matrix, target, (midi + 1, bottom.j), (top.i, midj)) ||
+                       SearchMatrix(matrix, target, (midi + 1, midj + 1), (top.i, top.j));
+            }
+        }
     }
 }
