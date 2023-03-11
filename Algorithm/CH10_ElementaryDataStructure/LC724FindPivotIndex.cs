319192374a9e2ee2e7c30ca379b42d7d8c8cfdb9
barneyliu708 <barneyliu708@gmail.com>
2023-03-11 11:46:09 -0800

    ALGORITHM-366 LC724. Find Pivot Index

---
+++ Algorithm/CH10_ElementaryDataStructure/LC724FindPivotIndex.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CH10_ElementaryDataStructure
+{
+    internal class LC724FindPivotIndex
+    {
+        public int PivotIndex(int[] nums)
+        {
+            int n = nums.Length;
+            int postSum = 0;
+            int[] postSums = new int[n];
+            for (int i = n - 1; i >= 0; i--)
+            {
+                postSums[i] = postSum;
+                postSum += nums[i];
+            }
+
+            int preSum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (preSum == postSums[i])
+                {
+                    return i;
+                }
+                preSum += nums[i];
+            }
+
+            return -1;
+        }
+    }
+}
